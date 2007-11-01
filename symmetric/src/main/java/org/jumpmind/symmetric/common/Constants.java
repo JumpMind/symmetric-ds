2e8bcf662dbba0cd1d5a3b3c3690f7cbddb98dd9
erilong <erilong@users.noreply.github.com>
2007-11-01 15:06:09 +0000

    add reload-node option

--- symmetric/src/main/java/org/jumpmind/symmetric/common/Constants.java
+++ symmetric/src/main/java/org/jumpmind/symmetric/common/Constants.java
@@ -59,6 +59,8 @@ public class Constants {
     
     public static final String REGISTRATION_SERVICE = "registrationService";
     
+    public static final String DATA_SERVICE = "dataService";
+    
     public static final String PUSH_SERVICE = "pushService";
     
     public static final String PULL_SERVICE = "pullService";
