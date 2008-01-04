97af293dba5273384c1a3178bb5e0d89cce06d61
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-01-04 18:32:26 +0000

    Change the scope of the transaction for a purge.

--- branches/1.2.0.a1/symmetric/src/main/java/org/jumpmind/symmetric/service/impl/PurgeService.java
+++ branches/1.2.0.a1/symmetric/src/main/java/org/jumpmind/symmetric/service/impl/PurgeService.java
@@ -28,6 +28,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jumpmind.symmetric.db.IDbDialect;
@@ -41,11 +42,12 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
-public class PurgeService extends AbstractService implements IPurgeService {
+public class PurgeService extends AbstractService implements IPurgeService
+{
 
     private final static Log logger = LogFactory.getLog(PurgeService.class);
 
-    private static int NUMBER_DATA_IDS_TO_PURGE_IN_BATCH = 5000;
+    private int maxNumOfDataIdsToPurgeInTx = 5000;
 
     private IDbDialect dbDialect;
 
@@ -72,85 +74,110 @@ public class PurgeService extends AbstractService implements IPurgeService {
     private IClusterService clusterService;
 
     @SuppressWarnings("unchecked")
-    public void purge() {
-        if (clusterService.lock(LockAction.PURGE)) {
-            try {
+    public void purge()
+    {
+        if (clusterService.lock(LockAction.PURGE))
+        {
+            try
+            {
                 logger.info("The purge process is about to run.");
-                Calendar calendar = Calendar.getInstance();
+                final Calendar calendar = Calendar.getInstance();
                 calendar.add(Calendar.MINUTE, -retentionInMinutes);
 
                 purgeBatchesOlderThan(calendar);
                 purgeDataRows();
 
-                for (String sql : otherPurgeSql) {
-                    int count = jdbcTemplate.update(sql, new Object[] { calendar.getTime() });
-                    if (count > 0) {
+                for (final String sql : otherPurgeSql)
+                {
+                    final int count = jdbcTemplate.update(sql, new Object[] {calendar.getTime()});
+                    if (count > 0)
+                    {
                         logger.info("Purged " + count + " rows after running: " + cleanSql(sql));
                     }
                 }
-            } finally {
+            }
+            finally
+            {
                 clusterService.unlock(LockAction.PURGE);
                 logger.info("The purge process has completed.");
             }
-            
-        } else {
+
+        }
+        else
+        {
             logger.info("Could not get a lock to run a purge.");
         }
     }
 
-    private void purgeDataRows() {
-        transactionTemplate.execute(new TransactionCallback() {
-            public Object doInTransaction(TransactionStatus s) {
-                int count = 0;
-                List<Integer> dataIds = null;
-                do {
-                    dataIds = getNextDataIds(selectDataIdToPurgeSql, null, NUMBER_DATA_IDS_TO_PURGE_IN_BATCH);
-                    for (Integer dataId : dataIds) {
-                        count += jdbcTemplate.update(deleteDataSql, new Object[] { dataId });
+    private void purgeDataRows()
+    {
+        int dataIdCount = 0;
+        do
+        {
+            dataIdCount = (Integer) transactionTemplate.execute(new TransactionCallback()
+            {
+                public Object doInTransaction(final TransactionStatus s)
+                {
+                    int count = 0;
+                    List<Integer> dataIds = null;
+                    dataIds = getNextDataIds(selectDataIdToPurgeSql, null, maxNumOfDataIdsToPurgeInTx);
+                    for (final Integer dataId : dataIds)
+                    {
+                        count += jdbcTemplate.update(deleteDataSql, new Object[] {dataId});
                     }
-                } while (dataIds != null && dataIds.size() > 0);
-
-                if (count > 0) {
-                    logger.info("Purged " + count + " data rows.");
+                    if (count > 0)
+                    {
+                        logger.info("Purged " + count + " data rows.");
+                    }
+                    return dataIds.size();
                 }
-                return null;
-            }
-        });
+            });
+        }
+        while (dataIdCount > 0);
 
     }
 
     @SuppressWarnings("unchecked")
-    private void purgeBatchesOlderThan(Calendar time) {
+    private void purgeBatchesOlderThan(final Calendar time)
+    {
         // Iterate over batch ids and data events to access by primary key so we prevent lock escalation
-        List<Integer> batchIds = jdbcTemplate.queryForList(selectOutgoingBatchIdsToPurgeSql, new Object[] { time
-                .getTime() }, Integer.class);
-        int batchCount = 0;
+        final List<Integer> batchIds = jdbcTemplate.queryForList(selectOutgoingBatchIdsToPurgeSql, new Object[] {time
+            .getTime()}, Integer.class);
         int eventRowCount = 0;
-        for (final Integer batchId : batchIds) {
-            batchCount++;
-            eventRowCount += (Integer) transactionTemplate.execute(new TransactionCallback() {
-                public Object doInTransaction(TransactionStatus s) {
-                    jdbcTemplate.update(deleteFromOutgoingBatchHistSql, new Object[] { batchId });
+        int dataIdCount = 0;
+        for (final Integer batchId : batchIds)
+        {
+            do
+            {
+                dataIdCount = (Integer) transactionTemplate.execute(new TransactionCallback()
+                {
+                    public Object doInTransaction(final TransactionStatus s)
+                    {
+                        jdbcTemplate.update(deleteFromOutgoingBatchHistSql, new Object[] {batchId});
 
-                    int eventCount = 0;
-                    List<Integer> dataIds = null;
-                    do {
-                        dataIds = getNextDataIds(selectEventDataIdToPurgeSql, new Object[] { batchId },
-                                NUMBER_DATA_IDS_TO_PURGE_IN_BATCH);
+                        int eventCount = 0;
+                        List<Integer> dataIds = null;
+                        dataIds = getNextDataIds(selectEventDataIdToPurgeSql, new Object[] {batchId},
+                            maxNumOfDataIdsToPurgeInTx);
 
-                        for (Integer dataId : dataIds) {
-                            eventCount += jdbcTemplate.update(deleteDataEventSql, new Object[] { dataId, batchId });
+                        for (final Integer dataId : dataIds)
+                        {
+                            eventCount += jdbcTemplate.update(deleteDataEventSql, new Object[] {dataId, batchId});
                         }
-                    } while (dataIds != null && dataIds.size() > 0);
 
-                    jdbcTemplate.update(deleteFromOutgoingBatchSql, new Object[] { batchId });
-                    return eventCount;
-                }
-            });
+                        jdbcTemplate.update(deleteFromOutgoingBatchSql, new Object[] {batchId});
+                        return eventCount;
+                    }
+                });
+                eventRowCount += dataIdCount;
+            }
+            while (dataIdCount > 0);
+
         }
 
-        if (batchCount > 0) {
-            logger.info("Purged " + batchCount + " batches and " + eventRowCount + " data_events.");
+        if (batchIds.size() > 0)
+        {
+            logger.info("Purged " + batchIds.size() + " batches and " + eventRowCount + " data_events.");
         }
     }
 
@@ -158,20 +185,26 @@ public class PurgeService extends AbstractService implements IPurgeService {
      * Select data ids using a streaming results set so we don't pull too much data into memory.
      */
     @SuppressWarnings("unchecked")
-    private List<Integer> getNextDataIds(final String sql, final Object[] args, final int maxNumberToReturn) {
-        return (List<Integer>) jdbcTemplate.execute(new ConnectionCallback() {
-            public Object doInConnection(Connection conn) throws SQLException, DataAccessException {
-                List<Integer> dataIds = new ArrayList<Integer>();
-                PreparedStatement st = conn.prepareStatement(sql, java.sql.ResultSet.TYPE_FORWARD_ONLY,
-                        java.sql.ResultSet.CONCUR_READ_ONLY);
-                if (args != null) {
-                    for (int i = 1; i <= args.length; i++) {
+    private List<Integer> getNextDataIds(final String sql, final Object[] args, final int maxNumberToReturn)
+    {
+        return (List<Integer>) jdbcTemplate.execute(new ConnectionCallback()
+        {
+            public Object doInConnection(final Connection conn) throws SQLException, DataAccessException
+            {
+                final List<Integer> dataIds = new ArrayList<Integer>();
+                final PreparedStatement st = conn.prepareStatement(sql, java.sql.ResultSet.TYPE_FORWARD_ONLY,
+                    java.sql.ResultSet.CONCUR_READ_ONLY);
+                if (args != null)
+                {
+                    for (int i = 1; i <= args.length; i++)
+                    {
                         st.setObject(i, args[i - 1]);
                     }
                 }
                 st.setFetchSize(dbDialect.getStreamingResultsFetchSize());
-                ResultSet rs = st.executeQuery();
-                for (int i = 0; i < 10000 && rs.next(); i++) {
+                final ResultSet rs = st.executeQuery();
+                for (int i = 0; i < 10000 && rs.next(); i++)
+                {
                     dataIds.add(rs.getInt(1));
                 }
                 JdbcUtils.closeResultSet(rs);
@@ -182,56 +215,74 @@ public class PurgeService extends AbstractService implements IPurgeService {
         });
     }
 
-    private String cleanSql(String sql) {
-        return sql.replace('\r', ' ').replace('\n', ' ').replace("\\w+", " ").trim();
+    private String cleanSql(final String sql)
+    {
+        return StringUtils.replace(StringUtils.replace(StringUtils.replace(sql, "\r", " "), "\n", " "), "  ", "");
     }
 
-    public void setOtherPurgeSql(String[] purgeSql) {
+    public void setOtherPurgeSql(final String[] purgeSql)
+    {
         this.otherPurgeSql = purgeSql;
     }
 
-    public void setRetentionInMinutes(int retentionInMinutes) {
+    public void setRetentionInMinutes(final int retentionInMinutes)
+    {
         this.retentionInMinutes = retentionInMinutes;
     }
 
-    public void setDbDialect(IDbDialect dbDialect) {
+    public void setDbDialect(final IDbDialect dbDialect)
+    {
         this.dbDialect = dbDialect;
     }
 
-    public void setSelectOutgoingBatchIdsToPurgeSql(String selectOutgoingBatchIdsToPurgeSql) {
+    public void setSelectOutgoingBatchIdsToPurgeSql(final String selectOutgoingBatchIdsToPurgeSql)
+    {
         this.selectOutgoingBatchIdsToPurgeSql = selectOutgoingBatchIdsToPurgeSql;
     }
 
-    public void setDeleteFromOutgoingBatchHistSql(String deleteFromOutgoingBatchHistSql) {
+    public void setDeleteFromOutgoingBatchHistSql(final String deleteFromOutgoingBatchHistSql)
+    {
         this.deleteFromOutgoingBatchHistSql = deleteFromOutgoingBatchHistSql;
     }
 
-    public void setDeleteFromOutgoingBatchSql(String deleteFromOutgoingBatchSql) {
+    public void setDeleteFromOutgoingBatchSql(final String deleteFromOutgoingBatchSql)
+    {
         this.deleteFromOutgoingBatchSql = deleteFromOutgoingBatchSql;
     }
 
-    public void setSelectEventDataIdToPurgeSql(String selectDataIdToPurgeSql) {
+    public void setSelectEventDataIdToPurgeSql(final String selectDataIdToPurgeSql)
+    {
         this.selectEventDataIdToPurgeSql = selectDataIdToPurgeSql;
     }
 
-    public void setDeleteDataEventSql(String deleteDataEventSql) {
+    public void setDeleteDataEventSql(final String deleteDataEventSql)
+    {
         this.deleteDataEventSql = deleteDataEventSql;
     }
 
-    public void setDeleteDataSql(String deleteDataSql) {
+    public void setDeleteDataSql(final String deleteDataSql)
+    {
         this.deleteDataSql = deleteDataSql;
     }
 
-    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+    public void setTransactionTemplate(final TransactionTemplate transactionTemplate)
+    {
         this.transactionTemplate = transactionTemplate;
     }
 
-    public void setSelectDataIdToPurgeSql(String selectDataIdToDeleteSql) {
+    public void setSelectDataIdToPurgeSql(final String selectDataIdToDeleteSql)
+    {
         this.selectDataIdToPurgeSql = selectDataIdToDeleteSql;
     }
 
-    public void setClusterService(IClusterService clusterService) {
+    public void setClusterService(final IClusterService clusterService)
+    {
         this.clusterService = clusterService;
+    }
+
+    public void setMaxNumOfDataIdsToPurgeInTx(final int maxNumOfDataIdsToPurgeInTx)
+    {
+        this.maxNumOfDataIdsToPurgeInTx = maxNumOfDataIdsToPurgeInTx;
     }
 
 }
