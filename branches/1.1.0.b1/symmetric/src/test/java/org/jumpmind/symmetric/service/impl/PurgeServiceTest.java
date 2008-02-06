0e798bfa031997e854b589068036d79b41721080
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-02-06 19:37:39 +0000

    NGPHASE-3178

--- branches/1.1.0.b1/symmetric/src/test/java/org/jumpmind/symmetric/service/impl/PurgeServiceTest.java
+++ branches/1.1.0.b1/symmetric/src/test/java/org/jumpmind/symmetric/service/impl/PurgeServiceTest.java
@@ -23,6 +23,7 @@ package org.jumpmind.symmetric.service.impl;
 
 import org.jumpmind.symmetric.AbstractTest;
 import org.jumpmind.symmetric.common.Constants;
+import org.jumpmind.symmetric.service.IPurgeService;
 import org.testng.annotations.Test;
 
 public class PurgeServiceTest extends AbstractTest
@@ -30,7 +31,7 @@ public class PurgeServiceTest extends AbstractTest
     @Test(groups="continuous")
     public void testThatPurgeExecutes()
     {
-        PurgeService service = (PurgeService) getBeanFactory().getBean(Constants.PURGE_SERVICE);        
+        IPurgeService service = (IPurgeService) getBeanFactory().getBean(Constants.PURGE_SERVICE);        
         service.purge();
     }
 }
