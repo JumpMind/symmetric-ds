a7abc316c6038e324e50191f745fff04d3f43b0e
henglinwang <henglinwang@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-02-12 13:19:48 +0000

    Add ThrottleFilter

--- branches/1.1.0.b1/symmetric/src/main/java/org/jumpmind/symmetric/util/MeteredOutputStream.java
+++ branches/1.1.0.b1/symmetric/src/main/java/org/jumpmind/symmetric/util/MeteredOutputStream.java
@@ -1,93 +1,115 @@
-/*
- * SymmetricDS is an open source database synchronization solution.
- *   
- * Copyright (C) Chris Henson <chenson42@users.sourceforge.net>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see
- * <http://www.gnu.org/licenses/>.
- */
 
 package org.jumpmind.symmetric.util;
 
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * An OutputStream that easily allows the rate of output to be limited.
+ * Throttle output stream to write at a specified rate. the rate will be an average
  * 
- * @author awilcox
- * 
+ * @author hwang
  */
-public class MeteredOutputStream extends OutputStream {
+public class MeteredOutputStream extends FilterOutputStream
+{
 
-    public static final long KB = 1024;
+    // max allowed, this will be an average over time. for small packages, it will be consume the whole bandwidth
+    private long maxBps;
 
-    public static final long MB = 1048576;
+    // total bytes send through
+    private long bytesSent;
 
-    private final long rate;
+    // start time when stream started output
+    private long startTime;
 
-    private final long pauseTime;
+    // flag to determine whether stream started output
+    private boolean started = false;
 
-    private long currentTime = 0;
+    // threshold before throttling in number of bytes
+    private long threshold = DEFFAULT_THRESHOLD;
 
-    private long bytesOutput = 0;
+    // frequency to recalculation rate in number of bytes
+    private long checkPoint = DEFAULT_CHECK_POINT;
 
-    private OutputStream out;
+    // default threshold before throttling in number of bytes
+    private static final long DEFFAULT_THRESHOLD = 8192L;
 
-    public MeteredOutputStream(OutputStream out, long ratePerSecond) {
-        this.out = out;
-        rate = ratePerSecond;
-        pauseTime = 10;
+    // default frequency to recalculation rate in number of bytes
+    private static final long DEFAULT_CHECK_POINT = 1024L;
+
+    /**
+     * @param out stream written to
+     * @param maxBps max number of bytes per second
+     * @param threshold the number in bytes before the throttle output stream
+     */
+    public MeteredOutputStream(OutputStream out, long maxBps, long threshold)
+    {
+        super(out);
+        this.maxBps = maxBps;
+        bytesSent = 0;
+        this.threshold = threshold <= DEFFAULT_THRESHOLD ? DEFFAULT_THRESHOLD : threshold;
+
+        checkPoint = DEFAULT_CHECK_POINT;
     }
 
-    public MeteredOutputStream(OutputStream out, long ratePerSecond, long pauseTime) {
-        this.out = out;
-        rate = ratePerSecond;
-        this.pauseTime = pauseTime;
+    /**
+     * @param out out stream written to
+     * @param maxBps max number of bytes per second
+     * @param threshold the number in bytes before throttling output stream
+     * @param checkPoint check the average rate when total byts%checkPoint == 0. <br>
+     *        the throttled output stream will write checkPoing number of bytes at full speed, and then sleep for a
+     *        certain to obtain an average close to maxBps. If set it to 1, it will check every bytes written and the rate will be the most accurate.
+     */
+    public MeteredOutputStream(OutputStream out, long maxBps, long threshold, long checkPoint)
+    {
+        super(out);
+        this.maxBps = maxBps;
+        bytesSent = 0;
+        this.threshold = threshold <= DEFFAULT_THRESHOLD ? DEFFAULT_THRESHOLD : threshold;
+        this.checkPoint = checkPoint;
+    }
+
+    /**
+     * @param out
+     * @param maxBps
+     */
+    public MeteredOutputStream(OutputStream out, long maxBps)
+    {
+        super(out);
+        this.maxBps = maxBps;
+        bytesSent = 0;
+        this.threshold = DEFFAULT_THRESHOLD;
+        checkPoint = DEFAULT_CHECK_POINT;
     }
 
     @Override
-    public void write(int b) throws IOException {
-        while (true) {
-            long time = System.currentTimeMillis() / 1000;
+    public void write(int b) throws IOException
+    {
+        // check if stream start output, if not, set started to true and set the start time
+        bytesSent += 1;
+        if (!started)
+        {
+            started = true;
+            startTime = System.currentTimeMillis();
+        }
+        // only check when total bytes greater than limit and adjust at checkPoint
+        if (bytesSent >= threshold && (bytesSent % checkPoint == 0))
+        {
+            long elapsed = System.currentTimeMillis() - startTime + 1;
+            long currentBps = bytesSent * 1000L / elapsed;
+            if (currentBps > maxBps)
+            {
 
-            if (time != currentTime) {
-                currentTime = time;
-                bytesOutput = 0;
-            }
-
-            if (bytesOutput < rate) {
-                out.write(b);
-                bytesOutput++;
-                return;
-            }
-
-            try {
-                Thread.sleep(pauseTime);
-            } catch (InterruptedException ie) {
-                // eat;
+                long expected = bytesSent * 1000L / maxBps;
+                try
+                {
+                    Thread.sleep(expected - elapsed);
+                }
+                catch (InterruptedException e)
+                {}
             }
         }
-    }
+        out.write(b);
 
-    @Override
-    public void close() throws IOException {
-        out.close();
-    }
-
-    @Override
-    public void flush() throws IOException {
-        out.flush();
     }
 }
