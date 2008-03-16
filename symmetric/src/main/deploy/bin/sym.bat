37208c81dbc458f4e328329663d833936e6b3a06
erilong <erilong@users.noreply.github.com>
2008-03-16 20:13:22 +0000

    remove the CLASSPATH echoing to console

--- symmetric/src/main/deploy/bin/sym.bat
+++ symmetric/src/main/deploy/bin/sym.bat
@@ -6,6 +6,6 @@ set CLASSPATH=.
 
 for %%i in ("%LIBDIR%\lib\*.jar") do call %PRGDIR%\cpappend.bat "%%i"
 
-echo CLASSPATH=%CLASSPATH%
+rem echo CLASSPATH=%CLASSPATH%
 
 java -cp %CLASSPATH% org.jumpmind.symmetric.SymmetricLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
