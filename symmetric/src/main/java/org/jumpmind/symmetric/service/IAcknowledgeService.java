b5854c1b7c6eadc0e3f2a6b00b7c43e3f01e777d
erilong <erilong@users.noreply.github.com>
2008-03-31 20:47:23 +0000

    keep our APR promise to not remove methods in 1.x

--- symmetric/src/main/java/org/jumpmind/symmetric/service/IAcknowledgeService.java
+++ symmetric/src/main/java/org/jumpmind/symmetric/service/IAcknowledgeService.java
@@ -20,9 +20,13 @@
 
 package org.jumpmind.symmetric.service;
 
+import java.util.List;
+
 import org.jumpmind.symmetric.model.BatchInfo;
 
 public interface IAcknowledgeService
 {
+    public void ack(List<BatchInfo> batches);
+    
     public void ack(BatchInfo batch);
 }
