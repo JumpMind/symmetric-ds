15ce8e38de2bfd5846b889d8a84910320110a4e9
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-04-17 21:08:59 +0000

    added additional statistics

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/statistic/Statistic.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/statistic/Statistic.java
@@ -21,6 +21,8 @@ package org.jumpmind.symmetric.statistic;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Statistic {
 
@@ -31,19 +33,33 @@ public class Statistic {
     private Date captureStartTimeMs;
 
     private BigDecimal total;
-    
+
     private long count;
+
+    private static Map<StatisticName, BigDecimal> lifeTimeTotal = new HashMap<StatisticName, BigDecimal>(
+            StatisticName.values().length);
+
+    private static Map<StatisticName, Long> lifeTimeCount = new HashMap<StatisticName, Long>(StatisticName
+            .values().length);
+
+    static {
+        StatisticName[] allStatistics = StatisticName.values();
+        for (StatisticName statisticName : allStatistics) {
+            lifeTimeCount.put(statisticName, new Long(0));
+            lifeTimeTotal.put(statisticName, BigDecimal.ZERO);
+        }
+    }
 
     public Statistic(StatisticName name, String nodeId) {
         this(name, nodeId, new Date());
     }
-    
+
     public Statistic(StatisticName name, String nodeId, Date startTime) {
         this.name = name;
         this.nodeId = nodeId;
         this.captureStartTimeMs = startTime;
         this.total = BigDecimal.ZERO;
-    }    
+    }
 
     public StatisticName getName() {
         return name;
@@ -56,8 +72,24 @@ public class Statistic {
     public BigDecimal getTotal() {
         return total;
     }
-    
+
+    public BigDecimal getLifetimeTotal() {
+        return lifeTimeTotal.get(name);
+    }
+
+    public long getLifetimeCount() {
+        return lifeTimeCount.get(name);
+    }
+
+    public BigDecimal getLifetimeAverageValue() {
+        return getAverageValue(getLifetimeTotal(), getLifetimeCount());
+    }
+
     public BigDecimal getAverageValue() {
+        return getAverageValue(this.total, this.count);
+    }
+
+    private BigDecimal getAverageValue(BigDecimal total, long count) {
         if (total != null && count > 0) {
             return total.divide(new BigDecimal(count));
         } else {
@@ -65,31 +97,47 @@ public class Statistic {
         }
     }
     
+    public void increment() {
+        add(1,1);
+    }
+
     public void add(int v) {
-        add(v,1);
+        add(v, 1);
     }
 
     public void add(BigDecimal v) {
-        add(v,1);
+        add(v, 1);
     }
-    
+
     public void add(long v) {
-        add(v,1);
+        add(v, 1);
     }
 
     public void add(long v, long count) {
-        this.total = this.total.add(new BigDecimal(v));
-        this.count += this.count;
+        synchronized (name) {
+            this.total = this.total.add(new BigDecimal(v));
+            this.count += this.count;
+            lifeTimeCount.put(name, new Long(lifeTimeCount.get(name) + count));
+            lifeTimeTotal.put(name, lifeTimeTotal.get(name).add(new BigDecimal(v)));
+        }
     }
 
     public void add(int v, long count) {
-        this.total = this.total.add(new BigDecimal(v));
-        this.count += this.count;
+        synchronized (name) {
+            this.total = this.total.add(new BigDecimal(v));
+            this.count += this.count;
+            lifeTimeCount.put(name, new Long(lifeTimeCount.get(name) + count));
+            lifeTimeTotal.put(name, lifeTimeTotal.get(name).add(new BigDecimal(v)));
+        }
     }
 
     public void add(BigDecimal v, long count) {
-        this.total = this.total.add(v);
-        this.count += this.count;
+        synchronized (name) {
+            this.total = this.total.add(v);
+            this.count += this.count;
+            lifeTimeCount.put(name, new Long(lifeTimeCount.get(name) + count));
+            lifeTimeTotal.put(name, lifeTimeTotal.get(name).add(v));
+        }
     }
 
     public Date getCaptureStartTimeMs() {
