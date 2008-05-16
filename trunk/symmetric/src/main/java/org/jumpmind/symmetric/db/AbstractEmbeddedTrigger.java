8337523c61147c489bd308ddf3ca2162a9737857
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-05-16 16:50:01 +0000

    quote boolean "1" or "0", because it might act like an integer on some platforms (Oracle)

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/db/AbstractEmbeddedTrigger.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/db/AbstractEmbeddedTrigger.java
@@ -136,7 +136,7 @@ public abstract class AbstractEmbeddedTrigger {
                         b.append(new String(Base64.encodeBase64(d.getBytes())));
                         b.append("\"");
                     } else if (object instanceof Boolean) {
-                        b.append(((Boolean) object) ? "1" : "0");
+                        b.append(((Boolean) object) ? "\"1\"" : "\"0\"");
                     } else {
                         throw new IllegalStateException("Could not format " + object + " which is of type "
                                 + object.getClass().getName());
