c35f739e2ec384e8901af61f34452a985a4a97e0
chenson42 <chenson42@gmail.com>
2008-07-17 15:01:26 +0000

    check the stat thresholds and publish jmx notification

--- symmetric/src/main/java/org/jumpmind/symmetric/model/StatisticAlertThresholds.java
+++ symmetric/src/main/java/org/jumpmind/symmetric/model/StatisticAlertThresholds.java
@@ -1,29 +1,57 @@
+/*
+ * SymmetricDS is an open source database synchronization solution.
+ *   
+ * Copyright (C) Chris Henson <chenson42@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.jumpmind.symmetric.model;
 
 import java.math.BigDecimal;
 
+import javax.management.Notification;
+
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.jumpmind.symmetric.statistic.Statistic;
 
 public class StatisticAlertThresholds {
 
     String statisticName;
-    BigDecimal threshholdTotalMax;
-    Long threshholdCountMax;
-    BigDecimal threshholdTotalMin;
-    Long threshholdCountMin;
+    BigDecimal thresholdTotalMax;
+    Long thresholdCountMax;
+    BigDecimal thresholdTotalMin;
+    Long thresholdCountMin;
+    BigDecimal thresholdAvgMax;
+    BigDecimal thresholdAvgMin;
+    static long sequenceNumber = System.currentTimeMillis();
 
     public StatisticAlertThresholds() {
     }
 
     public StatisticAlertThresholds(String statisticName, BigDecimal threshholdTotalMax, Long threshholdCountMax,
-            BigDecimal threshholdTotalMin, Long threshholdCountMin) {
+            BigDecimal threshholdTotalMin, Long threshholdCountMin, BigDecimal threshholdAvgMax,
+            BigDecimal threshholdAvgMin) {
         super();
         this.statisticName = statisticName;
-        this.threshholdTotalMax = threshholdTotalMax;
-        this.threshholdCountMax = threshholdCountMax;
-        this.threshholdTotalMin = threshholdTotalMin;
-        this.threshholdCountMin = threshholdCountMin;
+        this.thresholdTotalMax = threshholdTotalMax;
+        this.thresholdCountMax = threshholdCountMax;
+        this.thresholdTotalMin = threshholdTotalMin;
+        this.thresholdCountMin = threshholdCountMin;
+        this.thresholdAvgMax = threshholdAvgMax;
+        this.thresholdAvgMin = threshholdAvgMin;
     }
 
     public String getStatisticName() {
@@ -34,42 +62,100 @@ public class StatisticAlertThresholds {
         this.statisticName = statisticName;
     }
 
-    public BigDecimal getThreshholdTotalMax() {
-        return threshholdTotalMax;
+    public BigDecimal getThresholdTotalMax() {
+        return thresholdTotalMax == null ? BigDecimal.ZERO : thresholdTotalMax;
     }
 
-    public void setThreshholdTotalMax(BigDecimal threshholdTotalMax) {
-        this.threshholdTotalMax = threshholdTotalMax;
+    public void setThresholdTotalMax(BigDecimal threshholdTotalMax) {
+        this.thresholdTotalMax = threshholdTotalMax;
     }
 
-    public Long getThreshholdCountMax() {
-        return threshholdCountMax;
+    public Long getThresholdCountMax() {
+        return thresholdCountMax == null ? 0l : thresholdCountMax;
     }
 
-    public void setThreshholdCountMax(Long threshholdCountMax) {
-        this.threshholdCountMax = threshholdCountMax;
+    public void setThresholdCountMax(Long threshholdCountMax) {
+        this.thresholdCountMax = threshholdCountMax;
     }
 
-    public BigDecimal getThreshholdTotalMin() {
-        return threshholdTotalMin;
+    public BigDecimal getThresholdTotalMin() {
+        return thresholdTotalMin == null ? BigDecimal.ZERO : thresholdTotalMin;
     }
 
-    public void setThreshholdTotalMin(BigDecimal threshholdTotalMin) {
-        this.threshholdTotalMin = threshholdTotalMin;
+    public void setThresholdTotalMin(BigDecimal threshholdTotalMin) {
+        this.thresholdTotalMin = threshholdTotalMin;
     }
 
-    public Long getThreshholdCountMin() {
-        return threshholdCountMin;
+    public Long getThresholdCountMin() {
+        return thresholdCountMin == null ? 0l : thresholdCountMin;
     }
 
-    public void setThreshholdCountMin(Long threshholdCountMin) {
-        this.threshholdCountMin = threshholdCountMin;
+    public void setThresholdCountMin(Long threshholdCountMin) {
+        this.thresholdCountMin = threshholdCountMin;
+    }
+
+    public Notification outsideOfBoundsNotification(Statistic stats) {
+        if (stats != null && stats.getName().name().equals(statisticName)) {
+            boolean createNotification = false;
+            StringBuilder msg = new StringBuilder(statisticName);
+            long count = stats.getCount();
+            BigDecimal total = stats.getTotal();
+            BigDecimal avg = stats.getAverageValue();
+            if ((thresholdCountMax != null && thresholdCountMax > 0 && count > thresholdCountMax)
+                    || (thresholdCountMin != null && thresholdCountMin > 0 && count < thresholdCountMin)) {
+                msg.append(":count=");
+                msg.append(count);
+                createNotification = true;
+            }
+
+            if ((thresholdTotalMax != null && thresholdTotalMax.compareTo(BigDecimal.ZERO) > 0 && total
+                    .compareTo(thresholdTotalMax) > 0)
+                    || (thresholdTotalMin != null && thresholdTotalMin.compareTo(BigDecimal.ZERO) > 0 && total
+                            .compareTo(thresholdTotalMin) < 0)) {
+                msg.append(":total=");
+                msg.append(total);
+                createNotification = true;
+            }
+
+            if ((thresholdAvgMax != null && thresholdAvgMax.compareTo(BigDecimal.ZERO) > 0 && avg
+                    .compareTo(thresholdAvgMax) > 0)
+                    || (thresholdAvgMin != null && thresholdAvgMin.compareTo(BigDecimal.ZERO) > 0 && avg
+                            .compareTo(thresholdAvgMin) < 0)) {
+                msg.append(":avg=");
+                msg.append(avg);
+                createNotification = true;
+            }
+
+            if (createNotification) {
+                return new Notification("SymmetricDS:Alert", stats, sequenceNumber++, System.currentTimeMillis(), msg
+                        .toString());
+            }
+
+        }
+        return null;
+    }
+
+    public BigDecimal getThresholdAvgMax() {
+        return thresholdAvgMax == null ? BigDecimal.ZERO : thresholdAvgMax;
+    }
+
+    public void setThresholdAvgMax(BigDecimal threshholdAvgMax) {
+        this.thresholdAvgMax = threshholdAvgMax;
+    }
+
+    public BigDecimal getThresholdAvgMin() {
+        return thresholdAvgMin == null ? BigDecimal.ZERO : thresholdAvgMin;
+    }
+
+    public void setThresholdAvgMin(BigDecimal threshholdAvgMin) {
+        this.thresholdAvgMin = threshholdAvgMin;
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(statisticName).append(threshholdTotalMax).append(threshholdCountMax)
-                .append(threshholdTotalMin).append(threshholdCountMin).toHashCode();
+        return new HashCodeBuilder(17, 37).append(statisticName).append(getThresholdTotalMax()).append(
+                getThresholdCountMax()).append(getThresholdTotalMin()).append(getThresholdCountMin()).append(
+                getThresholdAvgMax()).append(getThresholdAvgMin()).toHashCode();
 
     }
 
@@ -82,10 +168,11 @@ public class StatisticAlertThresholds {
             return true;
         }
         StatisticAlertThresholds rhs = (StatisticAlertThresholds) obj;
-        return new EqualsBuilder().append(statisticName, rhs.statisticName).append(
-                threshholdTotalMax, rhs.threshholdTotalMax).append(threshholdCountMax, rhs.threshholdCountMax).append(
-                threshholdTotalMin, rhs.threshholdTotalMin).append(threshholdCountMin, rhs.threshholdCountMin)
-                .isEquals();
+        return new EqualsBuilder().append(statisticName, rhs.statisticName).append(getThresholdTotalMax(),
+                rhs.getThresholdTotalMax()).append(getThresholdCountMax(), rhs.getThresholdCountMax()).append(
+                getThresholdTotalMin(), rhs.getThresholdTotalMin()).append(getThresholdCountMin(),
+                rhs.getThresholdCountMin()).append(getThresholdAvgMax(), rhs.getThresholdAvgMax()).append(
+                getThresholdAvgMin(), rhs.getThresholdAvgMin()).isEquals();
     }
 
 }
--- symmetric/src/test/java/org/jumpmind/symmetric/service/impl/StatisticServiceTest.java
+++ symmetric/src/test/java/org/jumpmind/symmetric/service/impl/StatisticServiceTest.java
@@ -3,10 +3,14 @@ package org.jumpmind.symmetric.service.impl;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.management.Notification;
+
 import org.jumpmind.symmetric.AbstractDatabaseTest;
 import org.jumpmind.symmetric.common.Constants;
 import org.jumpmind.symmetric.model.StatisticAlertThresholds;
 import org.jumpmind.symmetric.service.IStatisticService;
+import org.jumpmind.symmetric.statistic.Statistic;
+import org.jumpmind.symmetric.statistic.StatisticName;
 import org.jumpmind.symmetric.util.AppUtils;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -28,7 +32,7 @@ public class StatisticServiceTest extends AbstractDatabaseTest {
         statisticService.removeStatisticAlertThresholds(TEST_STAT_NAME);
 
         StatisticAlertThresholds thresholds = new StatisticAlertThresholds(TEST_STAT_NAME, new BigDecimal(10),
-                new Long(2), new BigDecimal(0), new Long(0));
+                new Long(2), new BigDecimal(0), new Long(0), new BigDecimal(0), new BigDecimal(0));
 
         List<StatisticAlertThresholds> list = statisticService.getAlertThresholds();
         Assert.assertFalse(list.contains(thresholds));
@@ -38,7 +42,7 @@ public class StatisticServiceTest extends AbstractDatabaseTest {
         list = statisticService.getAlertThresholds();
         Assert.assertTrue(list.contains(thresholds));
 
-        thresholds.setThreshholdCountMin(-100l);
+        thresholds.setThresholdCountMin(-100l);
         Assert.assertFalse(list.contains(thresholds));
 
         statisticService.saveStatisticAlertThresholds(thresholds);
@@ -47,5 +51,47 @@ public class StatisticServiceTest extends AbstractDatabaseTest {
         Assert.assertTrue(list.contains(thresholds));
 
         statisticService.removeStatisticAlertThresholds(TEST_STAT_NAME);
+    }
+
+    @Test(groups = "continuous")
+    public void testSaveAlertsWithNulls() throws Exception {
+        statisticService.removeStatisticAlertThresholds(TEST_STAT_NAME);
+
+        StatisticAlertThresholds thresholds = new StatisticAlertThresholds(TEST_STAT_NAME, new BigDecimal(10),
+                new Long(2), null, new Long(0), null, null);
+
+        List<StatisticAlertThresholds> list = statisticService.getAlertThresholds();
+        Assert.assertFalse(list.contains(thresholds));
+
+        statisticService.saveStatisticAlertThresholds(thresholds);
+
+        list = statisticService.getAlertThresholds();
+        Assert.assertTrue(list.contains(thresholds));
+
+        thresholds.setThresholdCountMin(-100l);
+        Assert.assertFalse(list.contains(thresholds));
+
+        statisticService.saveStatisticAlertThresholds(thresholds);
+
+        list = statisticService.getAlertThresholds();
+        Assert.assertTrue(list.contains(thresholds));
+
+        statisticService.removeStatisticAlertThresholds(TEST_STAT_NAME);
+    }
+
+    @Test(groups = "continuous")
+    public void testNotificationCreationForTotals() throws Exception {
+        Statistic stat = new Statistic(StatisticName.INCOMING_BATCH_COUNT, "010101");
+        StatisticAlertThresholds thresholds = new StatisticAlertThresholds(StatisticName.INCOMING_BATCH_COUNT.name(),
+                new BigDecimal(10), null, new BigDecimal(1), null, null, null);
+        Assert.assertNotNull(thresholds.outsideOfBoundsNotification(stat));
+        stat.add(new BigDecimal(1));
+        Assert.assertNull(thresholds.outsideOfBoundsNotification(stat));
+        stat.add(new BigDecimal(9));
+        Assert.assertNull(thresholds.outsideOfBoundsNotification(stat));stat.add(new BigDecimal(1));
+        Notification event = thresholds.outsideOfBoundsNotification(stat);
+        Assert.assertNotNull(event);
+        String expectedMsg = stat.getName().name() + ":total=11";
+        Assert.assertEquals(event.getMessage(), expectedMsg);      
     }
 }
