d15f88d5352cab9684e6a530f762479bb734a0f9
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-07-31 14:14:29 +0000

    number of characters command line on Win2k is limited.  use CLASSPATH env var instead.  fix quoting problems and directories with spaces.

--- branches/1.3/src/main/deploy/bin/sym.bat
+++ branches/1.3/src/main/deploy/bin/sym.bat
@@ -4,8 +4,8 @@ set PRGDIR=%~dp0
 set LIBDIR=%PRGDIR%\..
 set CLASSPATH=.
 
-for %%i in ("%LIBDIR%\lib\*.jar") do call %PRGDIR%\cpappend.bat "%%i"
+for %%i in ("%LIBDIR%\lib\*.jar") do call "%PRGDIR%\cpappend.bat" %%i
 
 echo CLASSPATH=%CLASSPATH%
 
-java -cp %CLASSPATH% org.jumpmind.symmetric.SymmetricLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
+java org.jumpmind.symmetric.SymmetricLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
