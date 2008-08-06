7d2f0cacc9a65cf0ca3c0b0ae86f06b0b288b184
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-08-06 23:53:51 +0000

    license headers

--- trunk/symmetric/src/test/java/org/jumpmind/symmetric/util/MeteredOutputStreamTest.java
+++ trunk/symmetric/src/test/java/org/jumpmind/symmetric/util/MeteredOutputStreamTest.java
@@ -1,5 +1,3 @@
-package org.jumpmind.symmetric.util;
-
 /*
  * SymmetricDS is an open source database synchronization solution.
  *   
@@ -19,6 +17,7 @@ package org.jumpmind.symmetric.util;
  * License along with this library; if not, see
  * <http://www.gnu.org/licenses/>.
  */
+package org.jumpmind.symmetric.util;
 
 import java.io.IOException;
 import java.util.Random;
