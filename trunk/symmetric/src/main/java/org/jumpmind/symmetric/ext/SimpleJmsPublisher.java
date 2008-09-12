52abb509cff1797d0600c39207bd0cdf7b7344f0
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-09-12 19:56:34 +0000

    debug log statement

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/ext/SimpleJmsPublisher.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/ext/SimpleJmsPublisher.java
@@ -1,14 +1,21 @@
 package org.jumpmind.symmetric.ext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jumpmind.symmetric.load.IDataLoaderContext;
 import org.springframework.jms.core.JmsTemplate;
 
 public class SimpleJmsPublisher implements IPublisher {
 
+    static final Log logger = LogFactory.getLog(SimpleJmsPublisher.class);
+
     JmsTemplate jmsTemplate;
-    
+
     public void publish(IDataLoaderContext ctx, String text) {
-       jmsTemplate.convertAndSend(text);
+        if (logger.isDebugEnabled()) {
+            logger.debug(text);
+        }
+        jmsTemplate.convertAndSend(text);
     }
 
     public void setJmsTemplate(JmsTemplate jmsTemplate) {
