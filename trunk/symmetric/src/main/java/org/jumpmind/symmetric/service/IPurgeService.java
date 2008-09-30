60ffc69280e312771a6f8676b796ad5de1754d32
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2008-09-30 01:16:38 +0000

    Clear out unsent events when an initial load is queue'd up

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/service/IPurgeService.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/service/IPurgeService.java
@@ -27,4 +27,6 @@ public interface IPurgeService {
 
     @Transactional
     public void purgeAllIncomingEventsForNode(String nodeId);
+    
+    public void purgeAllOutgoingEventsForNode(String nodeId);
 }
