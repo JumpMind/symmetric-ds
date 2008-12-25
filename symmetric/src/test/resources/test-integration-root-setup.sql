cc43eb1c620072b8ba3c3962ffe5166e6bdc6058
chenson42 <chenson42@gmail.com>
2008-12-25 18:01:39 +0000

    oops.  broke the system channels.  fixed the unit test setup to catch the issue and fixed the channel setup.

--- symmetric/src/test/resources/test-integration-root-setup.sql
+++ symmetric/src/test/resources/test-integration-root-setup.sql
@@ -1,6 +1,4 @@
 insert into sym_channel values('testchannel', 1, 50, 50, 1, null);
-insert into sym_channel values('reload', 1, 50, 50, 1, null);
-insert into sym_channel values('config', 1, 50, 50, 1, null);
 
 insert into sym_node_group values ('test-root-group','a test config');
 insert into sym_node_group values ('test-node-group','a test config');
