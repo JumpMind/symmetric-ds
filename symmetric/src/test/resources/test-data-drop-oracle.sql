0fce99609c732edb19930e8ba096bfe99e20eeb0
chenson42 <chenson42@gmail.com>
2009-01-26 01:59:51 +0000

    drop the rogue table in the unit tests ...

--- symmetric/src/test/resources/test-data-drop-oracle.sql
+++ symmetric/src/test/resources/test-data-drop-oracle.sql
@@ -4,3 +4,4 @@ drop sequence seq_sym_data_data_id;
 drop sequence seq_sym_outgoin_batch_batch_id;
 drop sequence seq_sym_trigger_trigger_id;
 drop sequence seq_sym_trigger_rigger_hist_id;
+drop table test_oracle_dates;
