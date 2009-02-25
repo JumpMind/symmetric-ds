2f116f9c0e59edc4c91f264426834a86af1c997c
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-02-25 21:15:15 +0000

    Added new feature to allow custom node id generation

---
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/ext/INodeIdGenerator.java
@@ -0,0 +1,46 @@
+/*
+ * SymmetricDS is an open source database synchronization solution.
+ *   
+ * Copyright (C) Chris Henson <chenson42@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.jumpmind.symmetric.ext;
+
+import org.jumpmind.symmetric.model.Node;
+
+/**
+ * An {@link IExtensionPoint} that allows SymmetricDS users to implement their
+ * own algorithms for how node_ids and passwords are generated or selected
+ * during registration.
+ */
+public interface INodeIdGenerator extends IExtensionPoint {
+
+    /**
+     * Based on the node parameters passed in generate an expected node id. This
+     * is used in an attempt to match a registration request.
+     */
+    public String selectNodeId(Node node);
+
+    /**
+     * Based on the node parameters passed in generate a brand new node id.
+     */
+    public String generateNodeId(Node node);
+
+    /**
+     * Generate a password to use when opening registration
+     */
+    public String generatePassword(Node node);
+}
--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/service/IRegistrationService.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/service/IRegistrationService.java
@@ -30,8 +30,21 @@ public interface IRegistrationService {
 
     public boolean registerNode(Node node, OutputStream out, boolean isRequestedRegistration) throws IOException;
 
+    /**
+     * Open registration for a single new node given a node group (f.e.,
+     * "STORE") and external ID (f.e., "00001"). The unique node ID and password
+     * are generated and stored in the node and node_security tables with the
+     * registration_enabled flag turned on. The next node to try registering for
+     * this node group and external ID will be given this information.
+     */
     public void openRegistration(String nodeGroupId, String externalId);
 
+    /**
+     * Re-open registration for a single node that already exists in the
+     * database. A new password is generated and the registration_enabled flag
+     * is turned on. The next node to try registering for this node group and
+     * external ID will be given this information.
+     */
     public void reOpenRegistration(String nodeId);
 
     /**
@@ -42,6 +55,13 @@ public interface IRegistrationService {
     
     public boolean isAutoRegistration();
 
+    /**
+     * Client method which attempts to register with the registration.url to
+     * pull configuration if the node has not already been registered. If the
+     * registration server cannot be reach this method will continue to try with
+     * random sleep periods up to one minute up until the registration succeeds
+     * or the maximum number of attempts has been reached.
+     */
     public void registerWithServer();
 
     public boolean isRegisteredWithServer();
