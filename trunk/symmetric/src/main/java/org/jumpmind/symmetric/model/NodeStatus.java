ad79c8800177505f06563dd1875bc227286f579e
eegeek <eegeek@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-02-26 15:28:39 +0000

    Added initial support for determining a node's state.  Currently, is data load started and is data load complete.

---
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/model/NodeStatus.java
@@ -0,0 +1,7 @@
+package org.jumpmind.symmetric.model;
+
+public enum NodeStatus {
+    DATA_LOAD_NOT_STARTED,
+    DATA_LOAD_STARTED,
+    DATA_LOAD_COMPLETED,
+}
--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/service/INodeService.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/service/INodeService.java
@@ -29,6 +29,7 @@ import org.jumpmind.symmetric.ext.INodeIdGenerator;
 import org.jumpmind.symmetric.model.DataEventAction;
 import org.jumpmind.symmetric.model.Node;
 import org.jumpmind.symmetric.model.NodeSecurity;
+import org.jumpmind.symmetric.model.NodeStatus;
 
 /**
  * This service provides an API to access {@link Node}s and Node related
@@ -85,4 +86,17 @@ public interface INodeService {
 
     public void setNodeIdGenerator(INodeIdGenerator nodeIdGenerator);
 
+    /**
+     * Returns true if a data load has occurred and has been completed.
+     * 
+     * @return
+     */
+    public boolean isDataLoadComplete();
+
+    /**
+     * Returns true if a data load has started but not yet completed.
+     * 
+     * @return
+     */
+    public boolean isDataLoadStarted();
 }
