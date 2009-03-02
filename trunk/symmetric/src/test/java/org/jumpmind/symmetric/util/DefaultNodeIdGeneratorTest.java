496213c00394c9bfef0d6d95582e4107bcb2086d
eegeek <eegeek@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-03-02 15:00:30 +0000

    Additional tests

--- trunk/symmetric/src/test/java/org/jumpmind/symmetric/util/DefaultNodeIdGeneratorTest.java
+++ trunk/symmetric/src/test/java/org/jumpmind/symmetric/util/DefaultNodeIdGeneratorTest.java
@@ -38,8 +38,70 @@ public class DefaultNodeIdGeneratorTest {
         final String EXPECTED_NODE_ID = "10001";
         node.setExternalId(EXPECTED_NODE_ID);
         node.setNodeId(EXPECTED_NODE_ID);
-        String selectedNodeId = generator.selectNodeId(new MockNodeService(),
-                node);
+        String selectedNodeId = generator.selectNodeId(new MockNodeService(), node);
         Assert.assertEquals(EXPECTED_NODE_ID, selectedNodeId);
     }
+
+    @Test
+    public void testGenerateNodeIdNoExisting() throws Exception {
+        final String EXPECTED_NODE_ID = "100";
+        DefaultNodeIdGenerator generator = new DefaultNodeIdGenerator();
+        Node node = new Node();
+        node.setExternalId("100");
+        String selectedNodeId = generator.generateNodeId(new MockNodeService(), node);
+        Assert.assertEquals(EXPECTED_NODE_ID, selectedNodeId);
+    }
+
+    @Test
+    public void testGenerateNodeIdExisting() throws Exception {
+        final String EXPECTED_NODE_ID = "100-0";
+        DefaultNodeIdGenerator generator = new DefaultNodeIdGenerator();
+        Node node = new Node();
+        node.setExternalId("100");
+        String selectedNodeId = generator.generateNodeId(new MockNodeService() {
+            @Override
+            public Node findNode(String nodeId) {
+                if (nodeId.equals("100")) {
+                    Node node = new Node();
+                    node.setNodeId("100");
+                    return node;
+                } else {
+                    return null;
+                }
+            }
+        }, node);
+        Assert.assertEquals(EXPECTED_NODE_ID, selectedNodeId);
+    }
+
+    @Test
+    public void testGenerateNodeIdExistingAll() throws Exception {
+        DefaultNodeIdGenerator generator = new DefaultNodeIdGenerator();
+        Node node = new Node();
+        node.setExternalId("100");
+        try {
+            generator.generateNodeId(new MockNodeService() {
+                @Override
+                public Node findNode(String nodeId) {
+                    Node node = new Node();
+                    node.setNodeId(nodeId);
+                    return node;
+                }
+            }, node);
+            Assert.fail("Should have received exception");
+        } catch (Exception e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void testGenerateNodeIdWithNodeIdSet() throws Exception {
+        DefaultNodeIdGenerator generator = new DefaultNodeIdGenerator();
+        Node node = new Node();
+        final String EXPECTED_NODE_ID = "10001";
+        node.setExternalId(EXPECTED_NODE_ID);
+        node.setNodeId(EXPECTED_NODE_ID);
+        String selectedNodeId = generator.generateNodeId(new MockNodeService(), node);
+        Assert.assertEquals(EXPECTED_NODE_ID, selectedNodeId);
+    }
+
 }
