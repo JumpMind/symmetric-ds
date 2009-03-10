555a4f677bf2b58559427d883519514c7e4e1238
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-03-10 00:57:42 +0000

    license headers and a few javadoc comments

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/config/TriggerCreationAdapter.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/config/TriggerCreationAdapter.java
@@ -1,8 +1,31 @@
+/*
+ * SymmetricDS is an open source database synchronization solution.
+ *   
+ * Copyright (C) Chris Henson <chenson42@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.jumpmind.symmetric.config;
 
 import org.jumpmind.symmetric.model.Trigger;
 import org.jumpmind.symmetric.model.TriggerHistory;
 
+/**
+ * An adapter for the trigger listener interface so you need only implement the
+ * methods you are interested in.
+ */
 public class TriggerCreationAdapter implements ITriggerCreationListener {
 
     public void tableDoesNotExist(Trigger trigger) {
