69e082ecbfde496f95021783c3ec672a57df71f5
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-05-15 01:27:18 +0000

    SYMMETRICDS-87 - ISyncUrlExtension

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/service/impl/BandwidthService.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/service/impl/BandwidthService.java
@@ -39,7 +39,7 @@ public class BandwidthService implements IBandwidthService {
     public double getDownloadKbpsFor(String syncUrl, long sampleSize, long maxTestDuration) {
         double downloadSpeed = -1;
         try {
-            BandwidthTestResults bw = getDownloadResultsFor(syncUrl, sampleSize, maxTestDuration);
+            BandwidthTestResults bw = getDownloadResultsFor(syncUrl, sampleSize, maxTestDuration);            
             downloadSpeed = (int) bw.getKbps();
         } catch (Exception e) {
             logger.error(e,e);
@@ -55,14 +55,15 @@ public class BandwidthService implements IBandwidthService {
         try {
             BandwidthTestResults bw = new BandwidthTestResults();
             URL u = new URL(String.format("%s/bandwidth?sampleSize=%s", syncUrl, sampleSize));
-            is = u.openStream();
             bw.start();
+            is = u.openStream();            
             int r;
             while (-1 != (r = is.read(buffer)) && bw.getElapsed() <= maxTestDuration) {
                 bw.transmitted(r);
             }
             is.close();
             bw.stop();
+            logger.info(String.format("%s was calculated to have a download bandwidth of %s kbps", syncUrl, bw.getKbps()));
             return bw;
         } finally {
             IOUtils.closeQuietly(is);
