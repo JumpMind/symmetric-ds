3505a843cae64aa2dfeeeeeac5ea3c68537d57f9
chenson42 <chenson42@gmail.com>
2009-06-03 17:27:25 +0000

    [maven-release-plugin]  copy for tag symmetric-1.6.3

---
+++ src/main/java/org/jumpmind/symmetric/service/impl/BandwidthService.java
@@ -0,0 +1,51 @@
+package org.jumpmind.symmetric.service.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jumpmind.symmetric.service.IBandwidthService;
+import org.jumpmind.symmetric.transport.BandwidthTestResults;
+
+public class BandwidthService implements IBandwidthService {
+
+    protected Log logger = LogFactory.getLog(getClass());
+
+    public double getDownloadKbpsFor(String syncUrl, long sampleSize, long maxTestDuration) {
+        double downloadSpeed = -1d;
+        try {
+            BandwidthTestResults bw = getDownloadResultsFor(syncUrl, sampleSize, maxTestDuration);
+            downloadSpeed = (int) bw.getKbps();
+        } catch (Exception e) {
+            logger.error(e,e);
+        }
+        return downloadSpeed;
+
+    }
+
+    protected BandwidthTestResults getDownloadResultsFor(String syncUrl, long sampleSize, long maxTestDuration)
+            throws IOException {
+        byte[] buffer = new byte[1024];
+        InputStream is = null;
+        try {
+            BandwidthTestResults bw = new BandwidthTestResults();
+            URL u = new URL(String.format("%s/bandwidth?sampleSize=%s", syncUrl, sampleSize));
+            bw.start();
+            is = u.openStream();
+            int r;
+            while (-1 != (r = is.read(buffer)) && bw.getElapsed() <= maxTestDuration) {
+                bw.transmitted(r);
+            }
+            is.close();
+            bw.stop();
+            logger.info(String.format("%s was calculated to have a download bandwidth of %s kbps", syncUrl, bw.getKbps()));
+            return bw;
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+    }
+
+}
