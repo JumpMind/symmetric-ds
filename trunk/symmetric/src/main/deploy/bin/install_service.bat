65b5fabf3edac02fcead6c926c6e121d38b4aa28
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-06-18 18:37:38 +0000

    add quotes in case spaces in directories and echo command for debugging

--- trunk/symmetric/src/main/deploy/bin/install_service.bat
+++ trunk/symmetric/src/main/deploy/bin/install_service.bat
@@ -8,12 +8,14 @@ set PRGDIR=%APPDIR%\bin
 set LIBDIR=%APPDIR%\lib
 set CLASSPATH=%APPDIR%
 
-for %%i in ("%LIBDIR%\*.jar") do call "cpappend.bat" %%i
+for %%i in ("%LIBDIR%\*.jar") do call "%PRGDIR%\cpappend.bat" %%i
 
-%PRGDIR%\sym_service.exe -install "Symmetric" ^
-	%JAVA_HOME%\jre\bin\client\jvm.dll ^
-	-Djava.class.path=%CLASSPATH% ^
-	-Dlog4j.configuration=file:%APPDIR%\conf\log4j.xml ^
+echo on
+
+"%PRGDIR%\sym_service.exe" -install "Symmetric" ^
+	"%JAVA_HOME%\jre\bin\client\jvm.dll" ^
+	-Djava.class.path="%CLASSPATH%" ^
+	-Dlog4j.configuration="file:%APPDIR%\conf\log4j.xml" ^
 	-Dsym.keystore.file="%APPDIR%\security\keystore" ^
 	-Djavax.net.ssl.trustStore="%APPDIR%\security\cacerts" ^
 	-start org.jumpmind.symmetric.SymmetricLauncher ^
@@ -21,7 +23,7 @@ for %%i in ("%LIBDIR%\*.jar") do call "cpappend.bat" %%i
 	-params --properties conf/symmetric.properties --port 8080 --server ^
 	-out logs/service-out.log ^
 	-err logs/service-err.log ^
-	-current %APPDIR%
+	-current "%APPDIR%"
 
 
 
