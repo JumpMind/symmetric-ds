536b9a15ba449b4344d155ac1e72dae0a41ea77f
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-09-07 12:53:26 +0000

    Javadoc

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/route/SubSelectDataRouter.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/route/SubSelectDataRouter.java
@@ -34,8 +34,8 @@ import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
 /**
  * This data router is invoked when the router_name is 'subselect'. The router_expression is always a SQL expression
- * that is used to find the nodes a row of data will be routed to. This router should never be used for high throughput
- * tables because it makes a call back to the database for each row that is synchronized.
+ * that is used to find the list of nodes a row of data will be routed to. This router should never be used for high throughput
+ * tables because it makes a call back to the database for each row that is routed.
  * <P/>
  * The query that is used to select the nodes is as follows:
  * <P/>
@@ -43,7 +43,13 @@ import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
  * select c.node_id from $[sym.sync.table.prefix]_node c where c.node_group_id=:NODE_GROUP_ID and c.sync_enabled=1 and ...
  * </code>
  * <P/>
- * The SQL expression designated in the router_expression is appended to the SQL statement.  
+ * The SQL expression designated in the router_expression is appended to the above SQL statement.  Current and old column values can be
+ * passed into the sub select expression.  For example, say you had an EMPLOYEE table and a PASSWORD table.  When the password changes you want
+ * to route the password to the HOME_STORE that is stored on the EMPLOYEE table.  The sub select expression might look like:
+ * <P/>
+ * <code>
+ * c.external_id in (select home_store from employee where employee_id in (:EMPLOYEE_ID, :OLD_EMPLOYEE_ID))
+ * </code>
  */
 public class SubSelectDataRouter extends AbstractDataRouter {
 
