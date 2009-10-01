098e07001105736c0230e6d39d5193eee0ea2c32
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-10-01 13:00:25 +0000

    Default timeout settings.  We have instances on 1.5, ibm jvm, suse where HttpUrlConnection timeout settings don't seem to be working.  Found -D settings in several online posts.

--- trunk/symmetric/src/main/deploy/bin/sym.bat
+++ trunk/symmetric/src/main/deploy/bin/sym.bat
@@ -8,4 +8,4 @@ for %%i in ("%LIBDIR%\lib\*.jar") do call "%PRGDIR%\cpappend.bat" %%i
 
 rem echo CLASSPATH=%CLASSPATH%
 
-java -Dsym.keystore.file="%PRGDIR%\..\security\keystore" -Djavax.net.ssl.trustStore="%PRGDIR%\..\security\cacerts" org.jumpmind.symmetric.SymmetricLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Dsym.keystore.file="%PRGDIR%\..\security\keystore" -Djavax.net.ssl.trustStore="%PRGDIR%\..\security\cacerts" -Dsun.net.client.defaultReadTimeout=1800000 -Dsun.net.client.defaultConnectTimeout=1800000 org.jumpmind.symmetric.SymmetricLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
