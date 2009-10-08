8fc202a38cd11f556e1cc2a2047430dbbc8708fd
chenson42 <chenson42@gmail.com>
2009-10-08 01:31:13 +0000

    Very simple implementation.  Works on OSX.

--- extensions/symmetric-ext-tray/src/main/java/org/jumpmind/symmetric/ext/tray/SystemTrayStatusListener.java
+++ extensions/symmetric-ext-tray/src/main/java/org/jumpmind/symmetric/ext/tray/SystemTrayStatusListener.java
@@ -1,28 +1,82 @@
 package org.jumpmind.symmetric.ext.tray;
 
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
 import java.util.Set;
 
+import org.jumpmind.symmetric.common.ParameterConstants;
+import org.jumpmind.symmetric.common.logging.ILog;
+import org.jumpmind.symmetric.common.logging.LogFactory;
 import org.jumpmind.symmetric.ext.IHeartbeatListener;
 import org.jumpmind.symmetric.model.Node;
+import org.jumpmind.symmetric.service.IParameterService;
 
 public class SystemTrayStatusListener implements IHeartbeatListener {
 
+    private final ILog logger = LogFactory.getLog(getClass());
+
+    private long timeBetweenHeartbeatsInSeconds = 5;
+    private boolean enabled = true;
+    private TrayIcon trayIcon = null;
+    private IParameterService parameterService;
+
+    private Image AT_REST;
+    private Image WORKING;
+    private Image ERROR;
+
     @Override
     public long getTimeBetweenHeartbeatsInSeconds() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.timeBetweenHeartbeatsInSeconds;
+    }
+
+    protected void init() {
+        if (SystemTray.isSupported()) {
+            try {
+                SystemTray tray = SystemTray.getSystemTray();
+                AT_REST = Toolkit.getDefaultToolkit().getImage(getClass().getResource("symmetric-ds-blank-16x16.gif"));
+                WORKING = Toolkit.getDefaultToolkit().getImage(getClass().getResource("symmetric-ds-work-16x16.gif"));
+                ERROR = Toolkit.getDefaultToolkit().getImage(getClass().getResource("symmetric-ds-stop-16x16.gif"));
+
+                trayIcon = new TrayIcon(AT_REST, parameterService.getString(ParameterConstants.ENGINE_NAME));
+                tray.add(trayIcon);
+            } catch (AWTException e) {
+                logger.error(e);
+            }
+        }
+
     }
 
     @Override
     public void heartbeat(Node me, Set<Node> children) {
-        // TODO Auto-generated method stub
-
+        if (trayIcon != null) {
+            if (me.getBatchInErrorCount() > 0) {
+                trayIcon.setImage(ERROR);
+            } else if (me.getBatchToSendCount() > 0) {
+                trayIcon.setImage(WORKING);
+            } else {
+                trayIcon.setImage(AT_REST);
+            }
+        }
     }
 
     @Override
     public boolean isAutoRegister() {
-        // TODO Auto-generated method stub
-        return false;
+        return enabled;
+    }
+
+    public void setTimeBetweenHeartbeatsInSeconds(long timeBetweenHeartbeatsInSeconds) {
+        this.timeBetweenHeartbeatsInSeconds = timeBetweenHeartbeatsInSeconds;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setParameterService(IParameterService parameterService) {
+        this.parameterService = parameterService;
     }
 
 }
