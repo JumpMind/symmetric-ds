370c3df4406743c66e5817d8ffe41ad81d487394
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-10-28 12:24:54 +0000

    Default the language to english

--- trunk/symmetric/src/main/deploy/bin/sym.bat
+++ trunk/symmetric/src/main/deploy/bin/sym.bat
@@ -9,4 +9,4 @@ for %%i in ("%LIBDIR%\lib\*.jar") do call "%PRGDIR%\cpappend.bat" %%i
 
 rem echo CLASSPATH=%CLASSPATH%
 
-java -Dsym.keystore.file="%PRGDIR%\..\security\keystore" -Djavax.net.ssl.trustStore="%PRGDIR%\..\security\cacerts" -Dlog4j.configuration=file:%CONFDIR%\log4j.xml -Dsun.net.client.defaultReadTimeout=1800000 -Dsun.net.client.defaultConnectTimeout=1800000 org.jumpmind.symmetric.SymmetricLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Duser.language=en -Dsym.keystore.file="%PRGDIR%\..\security\keystore" -Djavax.net.ssl.trustStore="%PRGDIR%\..\security\cacerts" -Dlog4j.configuration=file:%CONFDIR%\log4j.xml -Dsun.net.client.defaultReadTimeout=1800000 -Dsun.net.client.defaultConnectTimeout=1800000 org.jumpmind.symmetric.SymmetricLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
