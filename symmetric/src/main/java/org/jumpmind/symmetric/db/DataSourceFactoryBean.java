b5092e2da235927c946361fcd81173cb5954fcf7
chenson42 <chenson42@gmail.com>
2009-11-03 16:53:43 +0000

    Added the ability to pull a datasource definition from a parent spring application context

--- symmetric/src/main/java/org/jumpmind/symmetric/db/DataSourceFactoryBean.java
+++ symmetric/src/main/java/org/jumpmind/symmetric/db/DataSourceFactoryBean.java
@@ -22,27 +22,33 @@ package org.jumpmind.symmetric.db;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
+import org.jumpmind.symmetric.common.Constants;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * Factory that will create either a jndiDataSource or a basicDataSource based
  * on whether a JNDI name is provided.
  */
-public class DataSourceFactoryBean implements FactoryBean<DataSource>, BeanFactoryAware {
+public class DataSourceFactoryBean implements FactoryBean<DataSource>, ApplicationContextAware {
 
     private String jndiName;
 
     private String beanName;
 
-    private BeanFactory beanFactory;
+    private ApplicationContext applicationContext;
 
     public DataSource getObject() throws Exception {
         if (jndiName == null || jndiName.trim().length() == 0) {
-            return (DataSource)beanFactory.getBean(beanName);
+            if (beanName.startsWith(Constants.PARENT_PROPERTY_PREFIX) && applicationContext.getParent() != null) {
+                return (DataSource) applicationContext.getParent().getBean(
+                        beanName.substring(Constants.PARENT_PROPERTY_PREFIX.length()));
+            }
+            return (DataSource) applicationContext.getBean(beanName);
         } else {
-            return (DataSource)beanFactory.getBean("jndiDataSource");
+            return (DataSource) applicationContext.getBean("jndiDataSource");
         }
     }
 
@@ -54,8 +60,8 @@ public class DataSourceFactoryBean implements FactoryBean<DataSource>, BeanFacto
         return true;
     }
 
-    public void setBeanFactory(BeanFactory beanFactory) {
-        this.beanFactory = beanFactory;
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
     public void setJndiName(String jndiName) {
