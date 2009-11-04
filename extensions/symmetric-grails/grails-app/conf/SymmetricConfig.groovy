6254dd2fb89493d1721ff7eb4fac7b0903258ec8
chenson42 <chenson42@gmail.com>
2009-11-04 01:32:44 +0000

    The plugin officially works ... at least standalone.  Next step.  Extension points and to test the install.

--- extensions/symmetric-grails/grails-app/conf/SymmetricConfig.groovy
+++ extensions/symmetric-grails/grails-app/conf/SymmetricConfig.groovy
@@ -3,17 +3,28 @@ symmetric {
     auto.startup = true
     db.spring.bean.name='parent.dataSource'
     auto.insert.registration.svr.if.not.found=true
+    web.base.servlet.path='/sync'
     sync.url = 'http://localhost:8080/symmetric-grails/sync'
     external.id = 'setme'
     group.id = 'setme'    
 }
 
 environments {
-    development {
+    root {
         symmetric {
             sync.url = 'http://localhost:8080/symmetric-grails/sync'
-            external.id = 'test-external'
-            group.id = 'test-group'            
+            external.id = 'root'
+            group.id = 'root'     
+            auto.registration=true
+            auto.reload=true
         }
     }
+    client {
+        symmetric {
+            sync.url = 'http://localhost:8090/symmetric-grails/sync'
+            external.id = 'client'
+            group.id = 'client'
+            registration.url='http://localhost:8080/symmetric-grails/sync'
+        }
+    }    
 }
