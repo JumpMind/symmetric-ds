9f35c7d2cbfa630eca1d5fa431630496a1dc01ec
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-11-19 00:17:06 +0000

    Added unit test

--- trunk/symmetric/src/test/java/org/jumpmind/symmetric/db/h2/H2PlatformUnitTest.java
+++ trunk/symmetric/src/test/java/org/jumpmind/symmetric/db/h2/H2PlatformUnitTest.java
@@ -73,7 +73,40 @@ public class H2PlatformUnitTest {
         pf.alterTables(null, null, null, db, false);
         Assert.assertEquals(200, jdbcTemplate.queryForInt("select NUMERIC_PRECISION from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TEST_TABLE_NUMERIC' and COLUMN_NAME='COLUMN_1'"));
     }
-    
+
+    @Test
+    public void testChangeDefaultValue() throws Exception {
+        Platform pf = getPlatform();
+        Database db = new Database();
+        Table table = new Table();
+        table.setName("TEST_TABLE_DEFAULTS");
+        Column column1 = new Column();
+        column1.setName("COLUMN_1");
+        column1.setType("NUMERIC");
+        column1.setSize("15");        
+        column1.setDefaultValue("1");
+        Column column2 = new Column();
+        column2.setName("COLUMN_2");
+        column2.setType("VARCHAR");
+        column2.setSize("50");
+        column2.setDefaultValue("test");
+        table.addColumn(column1);
+        table.addColumn(column2);
+        db.addTable(table);
+        pf.createTables(db, false, false);
+        
+        SimpleJdbcTemplate jdbcTemplate = new SimpleJdbcTemplate(pf.getDataSource());
+        Assert.assertEquals(0, jdbcTemplate.queryForInt("select count(*) from TEST_TABLE_DEFAULTS"));
+        Assert.assertEquals("1", jdbcTemplate.queryForObject("select COLUMN_DEFAULT from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TEST_TABLE_DEFAULTS' and COLUMN_NAME='COLUMN_1'", String.class));
+        Assert.assertEquals("'test'", jdbcTemplate.queryForObject("select COLUMN_DEFAULT from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TEST_TABLE_DEFAULTS' and COLUMN_NAME='COLUMN_2'", String.class));
+        
+        column1.setDefaultValue("2");
+        column2.setDefaultValue(null);
+        pf.alterTables(null, null, null, db, false);
+        Assert.assertEquals("2", jdbcTemplate.queryForObject("select COLUMN_DEFAULT from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TEST_TABLE_DEFAULTS' and COLUMN_NAME='COLUMN_1'", String.class));
+        Assert.assertEquals(null, jdbcTemplate.queryForObject("select COLUMN_DEFAULT from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TEST_TABLE_DEFAULTS' and COLUMN_NAME='COLUMN_2'", String.class));
+    }
+
     protected Platform getPlatform() throws Exception {
         PlatformFactory.registerPlatform(H2, H2Platform.class);
         Platform pf = PlatformFactory.createNewPlatformInstance(H2);
