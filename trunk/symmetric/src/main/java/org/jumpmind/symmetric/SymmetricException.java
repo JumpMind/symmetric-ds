da400aa101897a29590a923014ccc429173bd9c9
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2009-12-29 21:10:10 +0000

    javadoc

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/SymmetricException.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/SymmetricException.java
@@ -1,7 +1,30 @@
+/*
+ * SymmetricDS is an open source database synchronization solution.
+ *   
+ * Copyright (C) Chris Henson <chenson42@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.jumpmind.symmetric;
 
 import org.jumpmind.symmetric.common.Message;
 
+/**
+ * This is a {@link RuntimeException} that supports using the SymmetricDS
+ * {@link Message} infrastructure
+ */
 public class SymmetricException extends RuntimeException {
 
     private static final long serialVersionUID = -3111453874504638368L;
@@ -26,7 +49,8 @@ public class SymmetricException extends RuntimeException {
         super(Message.get(messageKey), cause);
     }
 
-    public SymmetricException(String messageKey, Throwable cause, Object... args) {
+    public SymmetricException(String messageKey, Throwable cause,
+            Object... args) {
         super(Message.get(messageKey, args), cause);
     }
 
