7c865d24f0411313f2118d93ca640dd9f5453ec4
erilong <erilong@users.noreply.github.com>
2009-12-31 21:17:11 +0000

    Use Tanuki's Java Service Wrapper

--- symmetric/src/main/deploy/bin/install_service.bat
+++ symmetric/src/main/deploy/bin/install_service.bat
@@ -1,30 +1,58 @@
 @echo off
-rem ---------------------------------------------------------------------------
-rem Install Symmetric as a Windows Service
-rem http://forge.ow2.org/projects/javaservice/
-rem ---------------------------------------------------------------------------
+setlocal
 
-set APPDIR=%~dp0\..
-set PRGDIR=%APPDIR%\bin
-set LIBDIR=%APPDIR%\lib
-set CLASSPATH=%APPDIR%
+rem Copyright (c) 1999, 2009 Tanuki Software, Ltd.
+rem http://www.tanukisoftware.com
+rem All rights reserved.
+rem
+rem This software is the proprietary information of Tanuki Software.
+rem You shall use it only in accordance with the terms of the
+rem license agreement you entered into with Tanuki Software.
+rem http://wrapper.tanukisoftware.org/doc/english/licenseOverview.html
+rem
+rem Java Service Wrapper general NT service install script.
+rem Optimized for use with version 3.3.6 of the Wrapper.
+rem
 
-for %%i in ("%LIBDIR%\*.jar") do call "%PRGDIR%\cpappend.bat" %%i
+if "%OS%"=="Windows_NT" goto nt
+echo This script only works with NT-based versions of Windows.
+goto :eof
 
-echo on
+:nt
+rem
+rem Find the application home.
+rem
+rem %~dp0 is location of current script under NT
+set _REALPATH=%~dp0
 
-"%PRGDIR%\sym_service.exe" -install "Symmetric" ^
-	"%JAVA_HOME%\jre\bin\client\jvm.dll" ^
-	-Djava.class.path="%CLASSPATH%" ^
-	-Dlog4j.configuration="file:%APPDIR%\conf\log4j.xml" ^
-	-Dsym.keystore.file="%APPDIR%\security\keystore" ^
-	-Djavax.net.ssl.trustStore="%APPDIR%\security\cacerts" ^
-	-start org.jumpmind.symmetric.SymmetricLauncher ^
-	-method main ^
-	-params --properties conf/symmetric.properties --port 8080 --server ^
-	-out logs/service-out.log ^
-	-err logs/service-err.log ^
-	-current "%APPDIR%"
+rem Decide on the wrapper binary.
+set _WRAPPER_BASE=sym_service
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
+if exist "%_WRAPPER_EXE%" goto conf
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
+if exist "%_WRAPPER_EXE%" goto conf
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
+if exist "%_WRAPPER_EXE%" goto conf
+echo Unable to locate a Wrapper executable using any of the following names:
+echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
+echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
+echo %_REALPATH%%_WRAPPER_BASE%.exe
+pause
+goto :eof
 
+rem
+rem Find the wrapper.conf
+rem
+:conf
+set _WRAPPER_CONF="%~f1"
+if not %_WRAPPER_CONF%=="" goto startup
+set _WRAPPER_CONF="%_REALPATH%..\conf\sym_service.conf"
 
+rem
+rem Install the Wrapper as an NT service.
+rem
+:startup
+"%_WRAPPER_EXE%" -i %_WRAPPER_CONF%
+if not errorlevel 1 goto :eof
+pause
 
