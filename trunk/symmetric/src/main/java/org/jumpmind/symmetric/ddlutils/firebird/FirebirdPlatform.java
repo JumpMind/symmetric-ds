7308037503b19baed12e0ce79f10a414615dae2b
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-01-17 15:47:05 +0000

    Refactored and standardized how we extend ddlutils

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/ddlutils/firebird/FirebirdPlatform.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/ddlutils/firebird/FirebirdPlatform.java
@@ -2,4 +2,8 @@ package org.jumpmind.symmetric.ddlutils.firebird;
 
 public class FirebirdPlatform extends org.apache.ddlutils.platform.firebird.FirebirdPlatform {
 
+    public FirebirdPlatform() {
+        super();
+        setModelReader(new FirebirdModelReader(this));
+    }
 }
