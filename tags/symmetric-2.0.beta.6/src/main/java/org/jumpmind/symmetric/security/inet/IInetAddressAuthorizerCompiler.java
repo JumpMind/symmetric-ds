eb60740cf4af68485270c7bbd7f00025a1e6e5b8
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-01-19 15:30:17 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.beta.6

---
+++ tags/symmetric-2.0.beta.6/src/main/java/org/jumpmind/symmetric/security/inet/IInetAddressAuthorizerCompiler.java
@@ -0,0 +1,32 @@
+/*
+ * SymmetricDS is an open source database synchronization solution.
+ *   
+ * Copyright (C) Dave Michels <dmichels2@users.sourceforge.net>,
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.jumpmind.symmetric.security.inet;
+
+import java.net.UnknownHostException;
+import java.util.List;
+
+/**
+ * @author dmichels
+ */
+public interface IInetAddressAuthorizerCompiler
+{
+    public List<IRawInetAddressAuthorizer> compile(String[] addresses) throws UnknownHostException;
+}
