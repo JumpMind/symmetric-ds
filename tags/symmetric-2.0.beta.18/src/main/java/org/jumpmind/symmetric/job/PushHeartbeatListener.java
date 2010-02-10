c4f9a4a0c5b7a6ca5d89901e105d4ddb20a85d1d
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-02-10 11:34:20 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.beta.18

---
+++ tags/symmetric-2.0.beta.18/src/main/java/org/jumpmind/symmetric/job/PushHeartbeatListener.java
@@ -0,0 +1,61 @@
+package org.jumpmind.symmetric.job;
+
+import java.util.Set;
+
+import org.jumpmind.symmetric.common.Constants;
+import org.jumpmind.symmetric.ext.IHeartbeatListener;
+import org.jumpmind.symmetric.model.Node;
+import org.jumpmind.symmetric.service.IDataService;
+import org.jumpmind.symmetric.service.INodeService;
+import org.jumpmind.symmetric.service.IOutgoingBatchService;
+
+public class PushHeartbeatListener implements IHeartbeatListener {
+
+    private boolean enabled;
+    private IDataService dataService;
+    private INodeService nodeService;
+    private IOutgoingBatchService outgoingBatchService;
+    private long timeBetweenHeartbeats;
+
+    public void heartbeat(Node me, Set<Node> children) {
+        if (enabled) {
+            // don't send new heart beat events if we haven't sent
+            // the last ones ...
+            if (!nodeService.isRegistrationServer()
+                    && !outgoingBatchService.isUnsentDataOnChannelForNode(Constants.CHANNEL_CONFIG, me.getNodeId())) {
+                dataService.insertHeartbeatEvent(me);
+                for (Node node : children) {
+                    dataService.insertHeartbeatEvent(node);
+                }
+            }
+        }
+    }
+    
+    public long getTimeBetweenHeartbeatsInSeconds() {
+        return this.timeBetweenHeartbeats;
+    }
+    
+    public void setTimeBetweenHeartbeats(long timeBetweenHeartbeats) {
+        this.timeBetweenHeartbeats = timeBetweenHeartbeats;
+    }
+
+    public boolean isAutoRegister() {
+        return enabled;
+    }
+
+    public void setDataService(IDataService dataService) {
+        this.dataService = dataService;
+    }
+
+    public void setNodeService(INodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+
+    public void setOutgoingBatchService(IOutgoingBatchService outgoingBatchService) {
+        this.outgoingBatchService = outgoingBatchService;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
