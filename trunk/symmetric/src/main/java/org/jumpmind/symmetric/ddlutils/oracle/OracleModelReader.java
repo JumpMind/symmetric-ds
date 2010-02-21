50dd7090be65b0c370277c3b0eb97f7e5c04478f
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-02-21 16:39:49 +0000

    SYMMETRICDS-191 - Don't include indexes in the model that don't reference other columns.

--- trunk/symmetric/src/main/java/org/jumpmind/symmetric/ddlutils/oracle/OracleModelReader.java
+++ trunk/symmetric/src/main/java/org/jumpmind/symmetric/ddlutils/oracle/OracleModelReader.java
@@ -1,14 +1,15 @@
 package org.jumpmind.symmetric.ddlutils.oracle;
 
 import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.collections.map.ListOrderedMap;
 import org.apache.ddlutils.Platform;
-import org.apache.ddlutils.model.Index;
-import org.apache.ddlutils.model.IndexColumn;
-import org.apache.ddlutils.model.NonUniqueIndex;
-import org.apache.ddlutils.model.UniqueIndex;
 import org.apache.ddlutils.platform.DatabaseMetaDataWrapper;
 import org.apache.ddlutils.platform.oracle.Oracle10ModelReader;
 
@@ -19,48 +20,73 @@ public class OracleModelReader extends Oracle10ModelReader {
     }
     
     @SuppressWarnings("unchecked")
-    protected void readIndex(DatabaseMetaDataWrapper metaData, Map values, Map knownIndices) throws SQLException
+    protected Collection readIndices(DatabaseMetaDataWrapper metaData, String tableName) throws SQLException
     {
-        Short indexType = (Short)values.get("TYPE");
+        // Oracle has a bug in the DatabaseMetaData#getIndexInfo method which fails when
+        // delimited identifiers are being used
+        // Therefore, we're rather accessing the user_indexes table which contains the same info
+        // This also allows us to filter system-generated indices which are identified by either
+        // having GENERATED='Y' in the query result, or by their index names being equal to the
+        // name of the primary key of the table
 
-        // we're ignoring statistic indices
-        if ((indexType != null) && (indexType.shortValue() == DatabaseMetaData.tableIndexStatistic) &&
-                (indexType.shortValue() == DatabaseMetaData.tableIndexClustered)  &&
-                (indexType.shortValue() == DatabaseMetaData.tableIndexHashed))
+        StringBuffer query = new StringBuffer();
+
+        query.append("SELECT a.INDEX_NAME, a.INDEX_TYPE, a.UNIQUENESS, b.COLUMN_NAME, b.COLUMN_POSITION FROM USER_INDEXES a, USER_IND_COLUMNS b WHERE ");
+        query.append("a.TABLE_NAME=? AND a.GENERATED=? AND a.TABLE_TYPE=? AND a.TABLE_NAME=b.TABLE_NAME AND a.INDEX_NAME=b.INDEX_NAME AND ");
+        query.append("a.INDEX_NAME NOT IN (SELECT DISTINCT c.CONSTRAINT_NAME FROM USER_CONSTRAINTS c WHERE c.CONSTRAINT_TYPE=? AND c.TABLE_NAME=a.TABLE_NAME");
+        if (metaData.getSchemaPattern() != null)
         {
-            return;
+            query.append(" AND c.OWNER LIKE ?) AND a.TABLE_OWNER LIKE ?");
         }
-        
-        String indexName = (String)values.get("INDEX_NAME");
-
-        if (indexName != null)
+        else
         {
-            Index index = (Index)knownIndices.get(indexName);
-    
-            if (index == null)
-            {
-                if (((Boolean)values.get("NON_UNIQUE")).booleanValue())
-                {
-                    index = new NonUniqueIndex();
-                }
-                else
-                {
-                    index = new UniqueIndex();
-                }
-
-                index.setName(indexName);
-                knownIndices.put(indexName, index);
-            }
-    
-            IndexColumn indexColumn = new IndexColumn();
-    
-            indexColumn.setName((String)values.get("COLUMN_NAME"));
-            if (values.containsKey("ORDINAL_POSITION"))
-            {
-                indexColumn.setOrdinalPosition(((Short)values.get("ORDINAL_POSITION")).intValue());
-            }
-            index.addColumn(indexColumn);
+            query.append(")");
         }
+
+        Map               indices = new ListOrderedMap();
+        PreparedStatement stmt    = null;
+
+        try
+        {
+            stmt = getConnection().prepareStatement(query.toString());
+            stmt.setString(1, getPlatform().isDelimitedIdentifierModeOn() ? tableName : tableName.toUpperCase());
+            stmt.setString(2, "N");
+            stmt.setString(3, "TABLE");
+            stmt.setString(4, "P");
+            if (metaData.getSchemaPattern() != null)
+            {
+                stmt.setString(5, metaData.getSchemaPattern().toUpperCase());
+                stmt.setString(6, metaData.getSchemaPattern().toUpperCase());
+            }
+
+            ResultSet rs     = stmt.executeQuery();
+            Map       values = new HashMap();
+
+            while (rs.next())
+            {
+                values.put("INDEX_NAME",       rs.getString(1));
+                String type = rs.getString(2);
+                if (type.startsWith("NORMAL")) {
+                    values.put("INDEX_TYPE",       new Short(DatabaseMetaData.tableIndexOther));
+                } else {
+                    // don't include indexes that might not reference other columns
+                    values.put("INDEX_TYPE",       new Short(DatabaseMetaData.tableIndexStatistic));
+                }
+                values.put("NON_UNIQUE",       "UNIQUE".equalsIgnoreCase(rs.getString(3)) ? Boolean.FALSE : Boolean.TRUE);
+                values.put("COLUMN_NAME",      rs.getString(4));
+                values.put("ORDINAL_POSITION", new Short(rs.getShort(5)));
+
+                readIndex(metaData, values, indices);
+            }
+        }
+        finally
+        {
+            if (stmt != null)
+            {
+                stmt.close();
+            }
+        }
+        return indices.values();
     }
 
 }
