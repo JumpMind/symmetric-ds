2f2a13e4b4f3fb42c7c88ac67275b76eb1b6803e
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-03-17 18:11:18 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.0

---
+++ tags/symmetric-2.0.0/src/main/java/org/jumpmind/symmetric/model/NodeGroupLinkAction.java
@@ -0,0 +1,43 @@
+/*
+ * SymmetricDS is an open source database synchronization solution.
+ *   
+ * Copyright (C) Chris Henson <chenson42@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.jumpmind.symmetric.model;
+
+
+/**
+ * Identifies the action to take when the event watcher sees events in the event
+ * table.
+ */
+public enum NodeGroupLinkAction {
+
+    P, W;
+
+    public static NodeGroupLinkAction fromCode(String code) {
+        if (code != null && code.length() > 0) {
+            if (P.name().equals(code)) {
+                return P;
+            } else if (W.name().equals(code)) {
+                return W;
+            }
+        }
+        return null;
+    }
+
+}
