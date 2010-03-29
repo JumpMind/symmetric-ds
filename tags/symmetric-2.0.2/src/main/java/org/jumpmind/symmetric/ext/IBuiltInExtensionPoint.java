88c56c270a8584ccd61cfbeb56af7cd2eb15d3b4
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-03-29 12:00:51 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.2

---
+++ tags/symmetric-2.0.2/src/main/java/org/jumpmind/symmetric/ext/IBuiltInExtensionPoint.java
@@ -0,0 +1,10 @@
+package org.jumpmind.symmetric.ext;
+
+/**
+ * Implement this interface to indicate that the extension point
+ * is part of the default system.  Used mainly to exclude 
+ * unnecessary logging.
+ */
+public interface IBuiltInExtensionPoint {
+
+}
