65d984f861aa85c4eb18b904583d9b3aa7e4aa33
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-05-12 20:42:25 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.5.2

---
+++ tags/symmetric-2.0.5.2/src/main/resources/org/jumpmind/symmetric/db/db2.sql
@@ -0,0 +1,6 @@
+CREATE VARIABLE sync_node_disabled VARCHAR(50);
+GRANT READ on VARIABLE sync_node_disabled TO PUBLIC;
+GRANT WRITE on VARIABLE sync_node_disabled TO PUBLIC;
+CREATE VARIABLE sync_triggers_disabled INTEGER;
+GRANT READ on VARIABLE sync_triggers_disabled TO PUBLIC;
+GRANT WRITE on VARIABLE sync_triggers_disabled TO PUBLIC;
