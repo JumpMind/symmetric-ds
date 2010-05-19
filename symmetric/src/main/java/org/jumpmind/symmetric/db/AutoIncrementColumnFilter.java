04ff9f8fbf4cf19210cfa1db41f2cc28a88287bd
chenson42 <chenson42@gmail.com>
2010-05-19 17:46:56 +0000

    SYMMETRICDS-272

--- symmetric/src/main/java/org/jumpmind/symmetric/db/AutoIncrementColumnFilter.java
+++ symmetric/src/main/java/org/jumpmind/symmetric/db/AutoIncrementColumnFilter.java
@@ -30,35 +30,44 @@ import org.jumpmind.symmetric.load.IDataLoaderContext;
 import org.jumpmind.symmetric.load.StatementBuilder.DmlType;
 
 public class AutoIncrementColumnFilter implements IColumnFilter {
+
     int[] indexesToRemove = null;
 
-    public String[] filterColumnsNames(IDataLoaderContext ctx, DmlType dml, Table table, String[] columnNames) {
-        ArrayList<String> columns = new ArrayList<String>();
-        CollectionUtils.addAll(columns, columnNames);
+    public String[] filterColumnsNames(IDataLoaderContext ctx, DmlType dml, Table table,
+            String[] columnNames) {
+        indexesToRemove = null;
         if (dml == DmlType.UPDATE) {
             Column[] autoIncrementColumns = table.getAutoIncrementColumns();
-            indexesToRemove = new int[autoIncrementColumns.length];
-            int i = 0;
-            for (Column column : autoIncrementColumns) {
-                String name = column.getName();
-                int index = columns.indexOf(name);
+            if (autoIncrementColumns != null && autoIncrementColumns.length > 0) {
+                ArrayList<String> columns = new ArrayList<String>();
+                CollectionUtils.addAll(columns, columnNames);
+                indexesToRemove = new int[autoIncrementColumns.length];
+                int i = 0;
+                for (Column column : autoIncrementColumns) {
+                    String name = column.getName();
+                    int index = columns.indexOf(name);
 
-                if (index < 0) {
-                    name = name.toLowerCase();
-                    index = columns.indexOf(name);
+                    if (index < 0) {
+                        name = name.toLowerCase();
+                        index = columns.indexOf(name);
+                    }
+                    if (index < 0) {
+                        name = name.toUpperCase();
+                        index = columns.indexOf(name);
+                    }
+                    
+                    indexesToRemove[i++] = index;
+                    columns.remove(name);
                 }
-                if (index < 0) {
-                    name = name.toUpperCase();
-                    index = columns.indexOf(name);
-                }
-                indexesToRemove[i++] = index;
-                columns.remove(name);
+                columnNames = columns.toArray(new String[columns.size()]);
             }
         }
-        return columns.toArray(new String[columns.size()]);
+        return columnNames;
+
     }
 
-    public Object[] filterColumnsValues(IDataLoaderContext ctx, DmlType dml, Table table, Object[] columnValues) {
+    public Object[] filterColumnsValues(IDataLoaderContext ctx, DmlType dml, Table table,
+            Object[] columnValues) {
         if (dml == DmlType.UPDATE && indexesToRemove != null) {
             ArrayList<Object> values = new ArrayList<Object>();
             CollectionUtils.addAll(values, columnValues);
