11b4d00b08763b81c2d91650c4710d09097e56e6
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-06-09 18:03:32 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.6.5

---
+++ tags/symmetric-2.0.6.5/src/main/java/org/jumpmind/symmetric/extract/csv/StreamCreateDataCommand.java
@@ -0,0 +1,22 @@
+package org.jumpmind.symmetric.extract.csv;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import org.jumpmind.symmetric.common.csv.CsvConstants;
+import org.jumpmind.symmetric.extract.DataExtractorContext;
+import org.jumpmind.symmetric.model.Data;
+import org.jumpmind.symmetric.util.CsvUtils;
+
+public class StreamCreateDataCommand extends AbstractStreamDataCommand {
+
+    public void execute(BufferedWriter writer, Data data, String routerId, DataExtractorContext context) throws IOException {        
+        CsvUtils.write(writer, CsvConstants.CREATE, DELIMITER, data.getRowData());        
+        writer.newLine();
+        context.incrementDataEventCount();
+    }
+    
+    public boolean isTriggerHistoryRequired() {
+        return false;
+    }
+}
