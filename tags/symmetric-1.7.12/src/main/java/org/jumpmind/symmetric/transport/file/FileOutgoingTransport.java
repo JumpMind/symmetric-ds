e9c0c35258b6e9f596e7df948e1d53062bf66d50
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-07-22 19:49:01 +0000

    [maven-release-plugin]  copy for tag symmetric-1.7.12

---
+++ tags/symmetric-1.7.12/src/main/java/org/jumpmind/symmetric/transport/file/FileOutgoingTransport.java
@@ -0,0 +1,67 @@
+/*
+ * SymmetricDS is an open source database synchronization solution.
+ *   
+ * Copyright (C) Chris Henson <chenson42@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.jumpmind.symmetric.transport.file;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.io.IOUtils;
+import org.jumpmind.symmetric.io.ThresholdFileWriter;
+import org.jumpmind.symmetric.transport.IOutgoingTransport;
+
+/**
+ * An outgoing transport that writes to the file system
+ */
+public class FileOutgoingTransport implements IOutgoingTransport {
+
+    BufferedWriter out;
+
+    ThresholdFileWriter fileWriter;
+
+    public FileOutgoingTransport(long threshold, String tempFileCategory) throws IOException {
+        this.fileWriter = new ThresholdFileWriter(threshold, tempFileCategory);
+    }
+
+    public BufferedWriter open() throws IOException {
+        out = new BufferedWriter(fileWriter);
+        return out;
+    }
+
+    public boolean isOpen() {
+        return out != null;
+    }
+
+    public void close() throws IOException {
+        IOUtils.closeQuietly(out);
+        out = null;
+    }
+
+    public Reader getReader() throws IOException {
+        return this.fileWriter.getReader();
+    }
+
+    public void delete() {
+        if (fileWriter != null) {
+            fileWriter.delete();
+        }
+    }
+
+}
