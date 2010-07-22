2184e7b873f4dfdf58ea6d41f4520dd9ffe3e411
chenson42 <chenson42@gmail.com>
2010-07-22 19:49:01 +0000

    [maven-release-plugin]  copy for tag symmetric-1.7.12

--- grails/symmetric-console/web-app/js/symmetric.js
+++
@@ -1,8 +0,0 @@
-function showPopup(obj) {
-	obj.getElementsByTagName("div")[0].style.display="inline";
-}
- 
-function hidePopup(obj) {
-	
-	obj.getElementsByTagName("div")[0].style.display="none";
-}
