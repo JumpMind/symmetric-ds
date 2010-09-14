55358409d80acccda64ee85dd1924d3c11ab4382
chenson42 <chenson42@gmail.com>
2010-09-14 14:08:48 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.10

---
+++ src/test/java/org/jumpmind/symmetric/util/AppUtilsUnitTest.java
@@ -0,0 +1,22 @@
+package org.jumpmind.symmetric.util;
+
+import java.util.Date;
+
+import org.apache.commons.lang.time.DateUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppUtilsUnitTest {
+
+    @Test
+    public void testGetLocalDateForOffset() {
+        Date gmt = AppUtils.getLocalDateForOffset("+00:00");
+        Date plusFour = AppUtils.getLocalDateForOffset("+04:00");
+        Date minusFour = AppUtils.getLocalDateForOffset("-04:00");
+        long nearZero = plusFour.getTime() - gmt.getTime() - DateUtils.MILLIS_PER_HOUR * 4;
+        Assert.assertTrue(nearZero + " was the left over ms",  Math.abs(nearZero) < 1000);
+        nearZero = plusFour.getTime() - minusFour.getTime() - DateUtils.MILLIS_PER_HOUR * 8;
+        Assert.assertTrue(nearZero + " was the left over ms", Math.abs(nearZero) < 1000);
+    }
+
+}
