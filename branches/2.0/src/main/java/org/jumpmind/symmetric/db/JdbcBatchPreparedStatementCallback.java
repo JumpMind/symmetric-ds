0acfd8dbb2141f7eaef469bf32ec619c07869868
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2010-10-18 19:48:26 +0000

    SYMMETRICDS-355 - use jdbc batch apis during routing

--- branches/2.0/src/main/java/org/jumpmind/symmetric/db/JdbcBatchPreparedStatementCallback.java
+++ branches/2.0/src/main/java/org/jumpmind/symmetric/db/JdbcBatchPreparedStatementCallback.java
@@ -11,15 +11,13 @@ import org.jumpmind.symmetric.common.logging.LogFactory;
 import org.jumpmind.symmetric.db.oracle.OracleDbDialect;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.InterruptibleBatchPreparedStatementSetter;
-import org.springframework.jdbc.core.ParameterDisposer;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.support.JdbcUtils;
 
 public class JdbcBatchPreparedStatementCallback implements PreparedStatementCallback<Integer> {
 
     static final ILog log = LogFactory.getLog(JdbcBatchPreparedStatementCallback.class);
-    static private boolean firstInitialization = true;    
+    static private boolean firstInitialization = true;
     BatchPreparedStatementSetter pss;
     IDbDialect dbDialect;
     int executeBatchSize;
@@ -59,52 +57,38 @@ public class JdbcBatchPreparedStatementCallback implements PreparedStatementCall
     public Integer doInPreparedStatement(PreparedStatement ps) throws SQLException,
             DataAccessException {
         int rowsAffected = 0;
-        try {
-            boolean oracleStyle = setupForOracleBatching(ps);
-            int batchSize = pss.getBatchSize();
-            InterruptibleBatchPreparedStatementSetter ipss = (pss instanceof InterruptibleBatchPreparedStatementSetter ? (InterruptibleBatchPreparedStatementSetter) pss
-                    : null);
-            if (JdbcUtils.supportsBatchUpdates(ps.getConnection())) {
-                for (int i = 0; i < batchSize; i++) {
-                    pss.setValues(ps, i);
-                    if (ipss != null && ipss.isBatchExhausted(i)) {
-                        break;
-                    }
-                    if (oracleStyle) {
-                        rowsAffected += ps.executeUpdate();
-                    } else {
-                        ps.addBatch();
-                        if (i % executeBatchSize == 0) {
-                            int[] results = ps.executeBatch();
-                            for (int j : results) {
-                                rowsAffected += j;
-                            }
+        boolean oracleStyle = setupForOracleBatching(ps);
+        int batchSize = pss.getBatchSize();
+        if (JdbcUtils.supportsBatchUpdates(ps.getConnection())) {
+            for (int i = 0; i < batchSize; i++) {
+                pss.setValues(ps, i);
+                if (oracleStyle) {
+                    rowsAffected += ps.executeUpdate();
+                } else {
+                    ps.addBatch();
+                    if (i % executeBatchSize == 0) {
+                        int[] results = ps.executeBatch();
+                        for (int j : results) {
+                            rowsAffected += j;
                         }
                     }
                 }
-                if (oracleStyle) {
-                    return ps.executeUpdate() + rowsAffected;
-                } else {
-                    int[] results = ps.executeBatch();
-                    for (int j : results) {
-                        rowsAffected += j;
-                    }
-                    return rowsAffected;
-                }
+            }
+            if (oracleStyle) {
+                return ps.executeUpdate() + rowsAffected;
             } else {
-                for (int i = 0; i < batchSize; i++) {
-                    pss.setValues(ps, i);
-                    if (ipss != null && ipss.isBatchExhausted(i)) {
-                        break;
-                    }
-                    rowsAffected += ps.executeUpdate();
+                int[] results = ps.executeBatch();
+                for (int j : results) {
+                    rowsAffected += j;
                 }
                 return rowsAffected;
             }
-        } finally {
-            if (pss instanceof ParameterDisposer) {
-                ((ParameterDisposer) pss).cleanupParameters();
+        } else {
+            for (int i = 0; i < batchSize; i++) {
+                pss.setValues(ps, i);
+                rowsAffected += ps.executeUpdate();
             }
+            return rowsAffected;
         }
     }
 
