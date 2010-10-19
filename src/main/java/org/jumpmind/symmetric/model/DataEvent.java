0e6f4e32b87e6663a6f50e3961fc5f7c7f07c86a
chenson42 <chenson42@gmail.com>
2010-10-19 12:46:31 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.11

---
+++ src/main/java/org/jumpmind/symmetric/model/DataEvent.java
@@ -0,0 +1,43 @@
+package org.jumpmind.symmetric.model;
+
+public class DataEvent {
+
+    private long dataId;
+   
+    private long batchId;
+    
+    private String routerId;
+
+    public DataEvent() {
+    }
+
+    public DataEvent(long dataId, long batchId, String routerId) {
+        this.dataId = dataId;
+        this.batchId = batchId;
+        this.routerId = routerId;
+    }
+    
+    public long getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(long batchId) {
+        this.batchId = batchId;
+    }
+
+    public long getDataId() {
+        return dataId;
+    }
+
+    public void setDataId(long dataId) {
+        this.dataId = dataId;
+    }
+
+    public void setRouterId(String routerId) {
+        this.routerId = routerId;
+    }
+    
+    public String getRouterId() {
+        return routerId;
+    }
+}
