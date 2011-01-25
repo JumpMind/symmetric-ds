57d73e7fc2910fe67e3bec1fd3e743e80f204def
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-01-25 21:06:32 +0000

    SYMMETRICDS-382

--- trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/db/oracle/OracleDbDialect.java
+++ trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/db/oracle/OracleDbDialect.java
@@ -84,11 +84,13 @@ public class OracleDbDialect extends AbstractDbDialect implements IDbDialect {
     @Override
     protected Integer overrideJdbcTypeForColumn(Map<Object,Object> values) {
         String typeName = (String) values.get("TYPE_NAME");
-        // This is for Oracle's TIMESTAMP(9)
-        if (typeName != null && typeName.startsWith("TIMESTAMP")) {
+        if (typeName != null && typeName.startsWith("DATE")) {
+            return Types.DATE;
+        } else if (typeName != null && typeName.startsWith("TIMESTAMP")) {
+            // This is for Oracle's TIMESTAMP(9)
             return Types.TIMESTAMP;
-            // This is for Oracle's NVARCHAR type
         } else if (typeName != null && typeName.startsWith("NVARCHAR")) {
+            // This is for Oracle's NVARCHAR type
             return Types.VARCHAR;
         } else if (typeName != null && typeName.startsWith("NCHAR")) {
             return Types.CHAR;            
