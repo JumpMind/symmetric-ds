fac01894486ca6fc9f0f1738a96dd4142268cdaa
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-01-31 01:57:31 +0000

    work on incoming batch api

--- trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/db/AbstractDbDialect.java
+++ trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/db/AbstractDbDialect.java
@@ -36,6 +36,7 @@ import java.sql.Time;
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -149,6 +150,14 @@ abstract public class AbstractDbDialect implements IDbDialect {
     protected boolean supportsTransactionViews = false;
     
     protected int queryTimeoutInSeconds = 300;
+    
+    public static void main(String[] args) throws Exception {
+        String test = "2011-01-28 09:02:46.8634";
+        //String test = ""2011-01-28 08:41:49.813735000"";
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+        System.out.println(format.format(format.parse(test)));
+        System.out.println(DateUtils.parseDate(test, TIMESTAMP_PATTERNS));
+    }
 
     protected AbstractDbDialect() {
         _defaultSizes = new HashMap<Integer, String>();
