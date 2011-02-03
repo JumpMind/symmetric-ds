6f6df34f99b8889e2c9786c19ca6e82a69cf9d9a
chenson42 <chenson42@gmail.com>
2011-02-03 00:16:55 +0000

    working on node host stats

--- symmetric/symmetric-server/src/test/java/org/jumpmind/symmetric/statistic/MockStatisticManager.java
+++ symmetric/symmetric-server/src/test/java/org/jumpmind/symmetric/statistic/MockStatisticManager.java
@@ -25,6 +25,9 @@ public class MockStatisticManager implements IStatisticManager {
 
     public void flush() {
     }
+    
+    public void incrementRestart() {
+    }
 
     public synchronized void incrementDataLoadedErrors(String channelId, long count) {
     }
