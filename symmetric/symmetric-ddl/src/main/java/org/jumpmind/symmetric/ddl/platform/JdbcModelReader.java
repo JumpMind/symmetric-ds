6d58100f008a193ab56e0fc03dc850f57d236f54
chenson42 <chenson42@gmail.com>
2011-04-08 22:28:37 +0000

    SYMMETRICDS-413 - ddl utils blows up for table in db2

--- symmetric/symmetric-ddl/src/main/java/org/jumpmind/symmetric/ddl/platform/JdbcModelReader.java
+++ symmetric/symmetric-ddl/src/main/java/org/jumpmind/symmetric/ddl/platform/JdbcModelReader.java
@@ -1032,44 +1032,48 @@ public class JdbcModelReader
      */
     public void determineAutoIncrementFromResultSetMetaData(Connection conn, Table table,
             final Column columnsToCheck[], String catalogSeparator) throws SQLException {
-        if (columnsToCheck == null || columnsToCheck.length == 0) {
-            return;
-        }
         StringBuilder query = new StringBuilder();
-        query.append("SELECT ");
-        for (int idx = 0; idx < columnsToCheck.length; idx++) {
-            if (idx > 0) {
-                query.append(",");
-            }
-            query.append("t.");
-            appendIdentifier(query, columnsToCheck[idx].getName());
-        }
-        query.append(" FROM ");
-
-        if (table.getCatalog() != null && !table.getCatalog().trim().equals("")) {
-            appendIdentifier(query, table.getCatalog());
-            query.append(catalogSeparator);
-        }
-        if (table.getSchema() != null && !table.getSchema().trim().equals("")) {
-            appendIdentifier(query, table.getSchema()).append(".");
-        }
-        appendIdentifier(query, table.getName()).append(" t WHERE 1 = 0");
-
-        Statement stmt = null;
         try {
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query.toString());
-            ResultSetMetaData rsMetaData = rs.getMetaData();
-
+            if (columnsToCheck == null || columnsToCheck.length == 0) {
+                return;
+            }        
+            query.append("SELECT ");
             for (int idx = 0; idx < columnsToCheck.length; idx++) {
-                if (rsMetaData.isAutoIncrement(idx + 1)) {
-                    columnsToCheck[idx].setAutoIncrement(true);
+                if (idx > 0) {
+                    query.append(",");
+                }
+                query.append("t.");
+                appendIdentifier(query, columnsToCheck[idx].getName());
+            }
+            query.append(" FROM ");
+
+            if (table.getCatalog() != null && !table.getCatalog().trim().equals("")) {
+                appendIdentifier(query, table.getCatalog());
+                query.append(catalogSeparator);
+            }
+            if (table.getSchema() != null && !table.getSchema().trim().equals("")) {
+                appendIdentifier(query, table.getSchema()).append(".");
+            }
+            appendIdentifier(query, table.getName()).append(" t WHERE 1 = 0");
+
+            Statement stmt = null;
+            try {
+                stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query.toString());
+                ResultSetMetaData rsMetaData = rs.getMetaData();
+
+                for (int idx = 0; idx < columnsToCheck.length; idx++) {
+                    if (rsMetaData.isAutoIncrement(idx + 1)) {
+                        columnsToCheck[idx].setAutoIncrement(true);
+                    }
+                }
+            } finally {
+                if (stmt != null) {
+                    stmt.close();
                 }
             }
-        } finally {
-            if (stmt != null) {
-                stmt.close();
-            }
+        } catch(SQLException ex) {
+            _log.warn("Failed to run the query: " + query + " to determine auto increment columns.  This is probably not harmful, but should be fixedx.");
         }
     }
 
