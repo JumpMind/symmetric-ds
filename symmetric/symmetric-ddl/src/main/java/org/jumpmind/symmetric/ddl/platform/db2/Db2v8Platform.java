cdbb7aac29cae78803b648fdd067b7e486f701f3
chenson42 <chenson42@gmail.com>
2011-04-14 14:30:07 +0000

    SYMMETRICDS-416

--- symmetric/symmetric-ddl/src/main/java/org/jumpmind/symmetric/ddl/platform/db2/Db2v8Platform.java
+++ symmetric/symmetric-ddl/src/main/java/org/jumpmind/symmetric/ddl/platform/db2/Db2v8Platform.java
@@ -35,16 +35,14 @@ public class Db2v8Platform extends Db2Platform
     public Db2v8Platform()
     {
         super();
-        // DB2 v8 has a maximum identifier length of 128 bytes for things like table names,
-        // stored procedure names etc., 30 bytes for column names and 18 bytes for foreign key names
         // Note that we optimistically assume that number of characters = number of bytes
         // If the name contains characters that are more than one byte in the database's
         // encoding, then the db will report an error anyway, but we cannot really calculate
         // the number of bytes
         getPlatformInfo().setMaxIdentifierLength(128);
-        getPlatformInfo().setMaxColumnNameLength(30);
-        getPlatformInfo().setMaxConstraintNameLength(18);
-        getPlatformInfo().setMaxForeignKeyNameLength(18);
+        getPlatformInfo().setMaxColumnNameLength(128);
+        getPlatformInfo().setMaxConstraintNameLength(128);
+        getPlatformInfo().setMaxForeignKeyNameLength(128);
         setSqlBuilder(new Db2v8Builder(this));
     }
 }
