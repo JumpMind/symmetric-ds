0a9ad2149c73215627c1af8d946e2c0854ce413a
chenson42 <chenson42@gmail.com>
2011-04-18 12:53:18 +0000

    [maven-release-plugin]  copy for tag symmetric-2.0.14

--- future/symmetric3-core/src/main/java/org/jumpmind/symmetric/core/model/Data.java
+++
@@ -1,5 +0,0 @@
-package org.jumpmind.symmetric.core.model;
-
-public class Data {
-
-}
--- symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/ext/IExtraConfigTables.java
+++
@@ -1,9 +0,0 @@
-package org.jumpmind.symmetric.ext;
-
-import java.util.List;
-
-public interface IExtraConfigTables extends IExtensionPoint {
-
-    public List<String> provideTableNames();
-    
-}
