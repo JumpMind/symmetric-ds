e13abdfd313a52deb5ec619a6d67b0262309febe
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-04-28 14:53:41 +0000

    if we can't delete a file, at least attempt to delete the rest.

--- trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/util/AppUtils.java
+++ trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/util/AppUtils.java
@@ -204,7 +204,7 @@ public class AppUtils {
                     FileUtils.forceDelete(it.next());
                     deletedCount++;
                 } catch (Exception ex) {
-                    log.error(ex);
+                    log.error(ex.getMessage());
                 }
             }
             if (deletedCount > 1) {
