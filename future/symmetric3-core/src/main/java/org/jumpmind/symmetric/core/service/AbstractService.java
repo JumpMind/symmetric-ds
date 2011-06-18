0b56e43cd5fafb6a33c13be5de020a0d435c1395
chenson42 <chenson42@gmail.com>
2011-06-18 13:59:08 +0000

    added a bunch of node service methods

--- future/symmetric3-core/src/main/java/org/jumpmind/symmetric/core/service/AbstractService.java
+++ future/symmetric3-core/src/main/java/org/jumpmind/symmetric/core/service/AbstractService.java
@@ -1,10 +1,17 @@
 package org.jumpmind.symmetric.core.service;
 
+import java.util.List;
+
 import org.jumpmind.symmetric.core.IEnvironment;
 import org.jumpmind.symmetric.core.SymmetricTables;
+import org.jumpmind.symmetric.core.common.Log;
+import org.jumpmind.symmetric.core.common.LogFactory;
 import org.jumpmind.symmetric.core.db.IDbDialect;
+import org.jumpmind.symmetric.core.model.Table;
 
 abstract public class AbstractService {
+    
+    protected final Log log = LogFactory.getLog(getClass());
 
     protected IEnvironment environment;
 
@@ -17,4 +24,17 @@ abstract public class AbstractService {
         this.dbDialect = environment.getDbDialect();
         this.tables = this.dbDialect.getSymmetricTables();
     }
+    
+    protected Table getTable(String tableSuffix) {
+        return tables.getSymmetricTable(tableSuffix);
+    }
+    
+    protected Table[] getTables(String... tableSuffixes) {
+        return tables.getSymmetricTables(tableSuffixes);
+    }
+    
+    protected <T> T getFirstEntry(List<T> list) {
+        return list != null && list.size() > 0 ? list.get(0) : null;
+    }
+    
 }
