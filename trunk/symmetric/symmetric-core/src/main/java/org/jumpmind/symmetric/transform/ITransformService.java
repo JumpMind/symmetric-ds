94908cb2e7572e18a049721cad05df20132d7308
gwilmer <gwilmer@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-07-04 17:01:34 +0000

    SYMMETRICDSPRO-33.  Changes to Transform service and symmetric engine to support transformation in the pro console.

--- trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/ITransformService.java
+++ trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/ITransformService.java
@@ -6,5 +6,11 @@ import java.util.Map;
 public interface ITransformService {
     
     public Map<String, List<TransformTable>> findTransformsFor(String nodeGroupId, boolean useCache);
+    
+    public List<TransformTable> getTransformTables();
+    
+    public void saveTransformTable(TransformTable transformTable);
+    
+    public void deleteTransformTable(String transformTableId);
 
 }
