9ee404625186dfc086a2db03a1782167e549a0c5
chenson42 <chenson42@gmail.com>
2011-07-22 18:08:57 +0000

    informix doesn't like my additive column transform

--- symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/AdditiveColumnTransform.java
+++ symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/AdditiveColumnTransform.java
@@ -52,19 +52,19 @@ public class AdditiveColumnTransform implements IColumnTransform, IBuiltInExtens
                 String quote = dbDialect.getPlatform().isDelimitedIdentifierModeOn() ? dbDialect.getPlatform()
                         .getPlatformInfo().getDelimiterToken() : "";
                 JdbcTemplate template = context.getJdbcTemplate();
-                StringBuilder sql = new StringBuilder(String.format("update %s set %s=%s+? where ",
+                StringBuilder sql = new StringBuilder(String.format("update %s set %s=%s+%s where ",
                         getFullyQualifiedTableName(data.getSchemaName(), data.getCatalogName(), data.getTableName()), 
                         quote + column.getTargetColumnName() + quote,
-                        quote + column.getTargetColumnName() + quote));
+                        quote + column.getTargetColumnName() + quote,
+                        value));
 
                 String[] keyNames = data.getKeyNames();
-                Column[] columns = new Column[keyNames.length + 1];
-                columns[0] = table.getColumnWithName(column.getTargetColumnName());
+                Column[] columns = new Column[keyNames.length];
                 for (int i = 0; i < keyNames.length; i++) {
                     if (i > 0) {
                         sql.append("and ");
                     }
-                    columns[i + 1] = table.getColumnWithName(keyNames[i]);
+                    columns[i] = table.getColumnWithName(keyNames[i]);
                     sql.append(quote);
                     sql.append(keyNames[i]);
                     sql.append(quote);
@@ -74,22 +74,13 @@ public class AdditiveColumnTransform implements IColumnTransform, IBuiltInExtens
                 if (0 < template.update(
                         sql.toString(),
                         dbDialect.getObjectValues(context.getBinaryEncoding(),
-                                prepend(value, data.getKeyValues()), columns))) {
+                                data.getKeyValues(), columns))) {
                     throw new IgnoreColumnException();
                 }
 
             }
         }
         return value;
-    }
-
-    protected String[] prepend(String v, String[] array) {
-        String[] dest = new String[array.length + 1];
-        dest[0] = v;
-        for (int i = 0; i < array.length; i++) {
-            dest[i + 1] = array[i];
-        }
-        return dest;
     }
 
     public void setDbDialect(IDbDialect dbDialect) {
