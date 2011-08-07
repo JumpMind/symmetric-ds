3356c7a104abd1b20ee8e1a9bc3e04cd3e8eb442
erilong <erilong@users.noreply.github.com>
2011-08-07 18:22:30 +0000

    add VariableColumnTransform

---
+++ symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/VariableColumnTransform.java
@@ -0,0 +1,35 @@
+package org.jumpmind.symmetric.transform;
+
+import java.util.Map;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.jumpmind.symmetric.ext.IBuiltInExtensionPoint;
+import org.jumpmind.symmetric.ext.ICacheContext;
+
+public class VariableColumnTransform implements IColumnTransform, IBuiltInExtensionPoint {
+
+    public static final String NAME = "variable";
+    
+    protected static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
+    
+    public boolean isAutoRegister() {
+        return true;
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public String transform(ICacheContext context, TransformColumn column,
+            TransformedData data, Map<String, String> sourceValues, String value, String oldValue) throws IgnoreColumnException,
+            IgnoreRowException {
+        String varName = column.getTransformExpression();
+        if (varName != null) {
+            if (varName.equalsIgnoreCase("current_timestamp")) {
+                return DateFormatUtils.format(System.currentTimeMillis(), DATE_PATTERN);
+            }
+        }
+        return null;
+    }
+
+}
