302b2fbed313bd28680df601e0698119012f24ab
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-08-08 00:20:51 +0000

    try to fix unit test

--- trunk/symmetric/symmetric-server/src/test/resources/test-database-setup.sql
+++ trunk/symmetric/symmetric-server/src/test/resources/test-database-setup.sql
@@ -108,7 +108,7 @@ insert into sym_transform_table (transform_id, source_table_name, target_table_n
 insert into sym_transform_column (transform_id, include_on, source_column_name, target_column_name, pk, transform_type, transform_expression, transform_order)
   values ('sourceb_to_b', '*', 'ID', 'ID_B', 1, null, null, 1);
 insert into sym_transform_column (transform_id, include_on, source_column_name, target_column_name, pk, transform_type, transform_expression, transform_order)
-  values ('sourceb_to_b', '*', null, 'S1_B', 0, 'lookup', 'select 12 from sym_node_identity where :ID = 9', 2);
+  values ('sourceb_to_b', '*', null, 'S1_B', 0, 'lookup', 'select 12 from sym_node_identity where  9=:ID', 2);
 insert into sym_transform_column (transform_id, include_on, source_column_name, target_column_name, pk, transform_type, transform_expression, transform_order)
   values ('sourceb_to_b', '*', null, 'S2_B', 0, 'variable', 'current_timestamp', 3);
 
