e9256cf912780c6e13491acc65834affbd703589
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-08-16 02:12:22 +0000

    give bsh transformer scripts access to jdbctemplate

--- trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/BshColumnTransform.java
+++ trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/BshColumnTransform.java
@@ -1,9 +1,31 @@
+/*
+ * Licensed to JumpMind Inc under one or more contributor 
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding 
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU Lesser General Public License (the
+ * "License"); you may not use this file except in compliance
+ * with the License. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see           
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. 
+ */
+
 package org.jumpmind.symmetric.transform;
 
 import java.util.Map;
 
 import org.jumpmind.symmetric.ext.IBuiltInExtensionPoint;
 import org.jumpmind.symmetric.ext.ICacheContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import bsh.Interpreter;
 
@@ -12,6 +34,8 @@ public class BshColumnTransform implements ISingleValueColumnTransform, IBuiltIn
     final String INTERPRETER_KEY = String.format("%d.BshInterpreter", hashCode());
 
     public static final String NAME = "bsh";
+    
+    protected JdbcTemplate jdbcTemplate;
     
     public boolean isAutoRegister() {
         return true;
@@ -26,6 +50,7 @@ public class BshColumnTransform implements ISingleValueColumnTransform, IBuiltIn
             throws IgnoreColumnException, IgnoreRowException {
         try {
             Interpreter interpreter = getInterpreter(context);
+            interpreter.set("jdbcTemplate", jdbcTemplate);
             interpreter.set("currentValue", value);
             interpreter.set("oldValue", oldValue);
             for (String columnName : sourceValues.keySet()) {
@@ -49,5 +74,9 @@ public class BshColumnTransform implements ISingleValueColumnTransform, IBuiltIn
             context.getContextCache().put(INTERPRETER_KEY, interpreter);
         }
         return interpreter;
+    }
+    
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
