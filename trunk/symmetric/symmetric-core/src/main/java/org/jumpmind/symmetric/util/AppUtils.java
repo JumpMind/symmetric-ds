c1521407a1885919c07b1b14e261b273f194c9f7
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-08-20 15:28:22 +0000

    don't allow nullpointerexception

--- trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/util/AppUtils.java
+++ trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/util/AppUtils.java
@@ -24,6 +24,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TimeZone;
@@ -272,5 +273,19 @@ public class AppUtils {
             throw new RuntimeException(e);
         }
     }
+
+    public static <T> Map<String, T> toMap(String[] keyNames, T[] values) {
+        if (values != null && keyNames != null && values.length >= keyNames.length) {
+            Map<String, T> map = new HashMap<String, T>(keyNames.length);
+            for (int i = 0; i < keyNames.length; i++) {
+                map.put(keyNames[i], values[i]);
+            }
+            return map;
+        } else {
+            return new HashMap<String, T>(0);
+        }
+    }
+    
+    
 
 }
