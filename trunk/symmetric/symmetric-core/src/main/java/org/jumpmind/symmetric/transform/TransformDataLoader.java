5550d0c397eb66292dc60c8b436a57a4b7edd491
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-08-26 12:04:10 +0000

    Add support for data loader properties and savepoints for postgres

--- trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/TransformDataLoader.java
+++ trunk/symmetric/symmetric-core/src/main/java/org/jumpmind/symmetric/transform/TransformDataLoader.java
@@ -1,3 +1,23 @@
+/*
+ * Licensed to JumpMind Inc under one or more contributor 
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding 
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU Lesser General Public License (the
+ * "License"); you may not use this file except in compliance
+ * with the License. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see           
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. 
+ */
 package org.jumpmind.symmetric.transform;
 
 import java.util.List;
@@ -130,9 +150,8 @@ public class TransformDataLoader extends AbstractTransformer implements IBuiltIn
                             }
                         } else {
                             // If not keys are specified we are going to assume
-                            // that this is
-                            // intentional and we will simply log a warning and
-                            // not fail.
+                            // that this is intentional and we will simply log a
+                            // warning and not fail.
                             log.warn("Message", ex.getMessage());
                             log.warn("TransformNoPrimaryKeyDefinedNoUpdate", data
                                     .getTransformation().getTransformId());
@@ -165,6 +184,9 @@ public class TransformDataLoader extends AbstractTransformer implements IBuiltIn
                         }
                     }
                 } else {
+                    // If not keys are specified we are going to assume
+                    // that this is intentional and we will simply log a
+                    // warning and not fail.
                     log.warn("TransformNoPrimaryKeyDefinedNoUpdate", data.getTransformation()
                             .getTransformId());
                 }
