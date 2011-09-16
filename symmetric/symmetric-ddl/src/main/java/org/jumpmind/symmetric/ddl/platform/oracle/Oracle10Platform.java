376ab221f74b81edc0d972377d3e0319bc8e5813
chenson42 <chenson42@gmail.com>
2011-09-16 18:36:44 +0000

    SYMMETRICDS-512 - fix drop/recreate problem w/ oracle 11g and ojdbc6.jar

--- symmetric/symmetric-ddl/src/main/java/org/jumpmind/symmetric/ddl/platform/oracle/Oracle10Platform.java
+++ symmetric/symmetric-ddl/src/main/java/org/jumpmind/symmetric/ddl/platform/oracle/Oracle10Platform.java
@@ -1,5 +1,7 @@
 package org.jumpmind.symmetric.ddl.platform.oracle;
 
+import java.sql.Types;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
