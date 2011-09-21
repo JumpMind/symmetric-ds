921d569d86793e6921e909a1a948b8b9c814f74c
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2011-09-21 01:30:12 +0000

    refactor filter and servlet so they are subclassable

--- trunk/symmetric/symmetric-server/src/main/java/org/jumpmind/symmetric/web/AbstractServlet.java
+++ trunk/symmetric/symmetric-server/src/main/java/org/jumpmind/symmetric/web/AbstractServlet.java
@@ -16,8 +16,8 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.  */
-
+ * under the License. 
+ */
 
 package org.jumpmind.symmetric.web;
 
@@ -29,17 +29,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.zip.GZIPInputStream;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.jumpmind.symmetric.common.logging.ILog;
 import org.jumpmind.symmetric.common.logging.LogFactory;
 
 /**
- * Super class for SymmetricDS Servlets.  Contains useful Servlet processing methods.
+ * Super class for SymmetricDS Servlets. Contains useful Servlet processing
+ * methods.
  */
 abstract public class AbstractServlet extends HttpServlet {
 
@@ -51,21 +54,18 @@ abstract public class AbstractServlet extends HttpServlet {
         return resp.getOutputStream();
     }
 
-    protected ILog getLog() {
-        return this.log;
-    }
-
     protected InputStream createInputStream(HttpServletRequest req) throws IOException {
         InputStream is = null;
         String contentType = req.getHeader("Content-Type");
         boolean useCompression = contentType != null && contentType.equalsIgnoreCase("gzip");
 
-        if (getLog().isDebugEnabled()) {
+        if (log.isDebugEnabled()) {
             StringBuilder b = new StringBuilder();
             BufferedReader reader = null;
             if (useCompression) {
-                getLog().debug("ServletCompressedStreamReceived");
-                reader = new BufferedReader(new InputStreamReader(new GZIPInputStream(req.getInputStream())));
+                log.debug("ServletCompressedStreamReceived");
+                reader = new BufferedReader(new InputStreamReader(new GZIPInputStream(
+                        req.getInputStream())));
             } else {
                 reader = req.getReader();
             }
@@ -79,7 +79,7 @@ abstract public class AbstractServlet extends HttpServlet {
                 }
             } while (line != null);
 
-            getLog().debug("ServletReceived", b);
+            log.debug("ServletReceived", b);
             is = new ByteArrayInputStream(b.toString().getBytes());
         } else {
             is = req.getInputStream();
@@ -113,8 +113,61 @@ abstract public class AbstractServlet extends HttpServlet {
      *            a message to put in the body of the response
      * @throws IOException
      */
-    protected boolean sendError(HttpServletResponse resp, int statusCode, String message) throws IOException {
+    protected boolean sendError(HttpServletResponse resp, int statusCode, String message)
+            throws IOException {
         return ServletUtils.sendError(resp, statusCode, message);
+    }
+
+    public boolean matches(IServletExtension ext, HttpServletRequest httpRequest) {
+        boolean retVal = true;
+        final String uri = ServletUtils.normalizeRequestUri(httpRequest);
+        if (!ArrayUtils.isEmpty(ext.getUriPatterns())) {
+            retVal = matchesUriPatterns(uri, ext.getUriPatterns());
+        }
+        return retVal;
+    }
+
+    protected boolean matchesUriPatterns(String uri, String[] uriPatterns) {
+        boolean retVal = false;
+        for (int i = 0; !retVal && i < uriPatterns.length; i++) {
+            retVal = matchesUriPattern(uri, uriPatterns[i]);
+        }
+        return retVal;
+    }
+
+    protected boolean matchesUriPattern(String uri, String uriPattern) {
+        boolean retVal = false;
+        String path = StringUtils.defaultIfEmpty(uri, "/");
+        final String pattern = StringUtils.defaultIfEmpty(uriPattern, "/");
+        if ("/".equals(pattern) || "/*".equals(pattern) || pattern.equals(path)) {
+            retVal = true;
+        } else {
+            final String[] patternParts = StringUtils.split(pattern, "/");
+            final String[] pathParts = StringUtils.split(path, "/");
+            boolean matches = true;
+            for (int i = 0; i < patternParts.length && i < pathParts.length && matches; i++) {
+                final String patternPart = patternParts[i];
+                matches = "*".equals(patternPart) || patternPart.equals(pathParts[i]);
+            }
+            retVal = matches;
+        }
+        return retVal;
+    }
+
+    protected void logException(ServletRequest req, Exception ex, boolean isError) {
+        String nodeId = req.getParameter(WebConstants.NODE_ID);
+        String externalId = req.getParameter(WebConstants.EXTERNAL_ID);
+        String address = req.getRemoteAddr();
+        String hostName = req.getRemoteHost();
+        String method = req instanceof HttpServletRequest ? ((HttpServletRequest) req).getMethod()
+                : "";
+        if (log.isErrorEnabled() && isError) {
+            log.error("ServletProcessingFailedError", ex, method, externalId, nodeId, address,
+                    hostName);
+        } else if (log.isWarnEnabled()) {
+            log.warn("ServletProcessingFailedWarning", method, externalId, nodeId, address,
+                    hostName, ex.getMessage());
+        }
     }
 
     /**
@@ -137,7 +190,8 @@ abstract public class AbstractServlet extends HttpServlet {
      * @return
      */
     protected String getParameter(HttpServletRequest request, String name, String defaultValue) {
-        return StringUtils.defaultIfEmpty(StringUtils.trimToNull(request.getParameter(name)), defaultValue);
+        return StringUtils.defaultIfEmpty(StringUtils.trimToNull(request.getParameter(name)),
+                defaultValue);
     }
 
     protected long getParameterAsNumber(HttpServletRequest request, String name) {
--- trunk/symmetric/symmetric-server/src/main/java/org/jumpmind/symmetric/web/SymmetricServlet.java
+++ trunk/symmetric/symmetric-server/src/main/java/org/jumpmind/symmetric/web/SymmetricServlet.java
@@ -16,8 +16,8 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.  */
-
+ * under the License. 
+ */
 
 package org.jumpmind.symmetric.web;
 
@@ -29,18 +29,12 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jumpmind.symmetric.common.DeploymentType;
-import org.jumpmind.symmetric.common.logging.ILog;
-import org.jumpmind.symmetric.common.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -69,18 +63,10 @@ public class SymmetricServlet extends AbstractServlet {
 
     private static final long serialVersionUID = 1L;
 
-    private static final ILog log = LogFactory.getLog(SymmetricServlet.class);
-
-    private List<IServletExtension> servlets;    
+    private List<IServletExtension> servlets;
 
     @Override
-    protected ILog getLog() {
-        return log;
-    }
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+    public void init() throws ServletException {
         servlets = new ArrayList<IServletExtension>();
         ApplicationContext ctx = ServletUtils.getApplicationContext(getServletContext());
         DeploymentType deploymentType = ctx.getBean(DeploymentType.class);
@@ -90,28 +76,32 @@ public class SymmetricServlet extends AbstractServlet {
         if (ctx.getParent() != null) {
             servletBeans.putAll(ctx.getParent().getBeansOfType(IServletExtension.class));
         }
-        // TODO order using initOrder
+
         for (final Map.Entry<String, IServletExtension> servletEntry : servletBeans.entrySet()) {
             log.debug("ServletInitializing", servletEntry.getKey());
             final IServletExtension extension = servletEntry.getValue();
-            extension.getServlet().init(config);
+            extension.getServlet().init(getServletConfig());
             servlets.add(extension);
         }
 
         if (servlets.size() == 0) {
-            log.error("ServletNoneFound");            
+            log.error("ServletNoneFound");
         }
     }
 
+    @Override
     public void destroy() {
-        for (final IServletExtension extension : servlets) {
-            extension.getServlet().destroy();
+        if (servlets != null) {
+            for (final IServletExtension extension : servlets) {
+                extension.getServlet().destroy();
+            }
         }
     }
 
-    protected Servlet findMatchingServlet(ServletRequest req, ServletResponse resp) {
+    protected Servlet findMatchingServlet(HttpServletRequest req) throws ServletException {
         Servlet retVal = null;
-        for (Iterator<IServletExtension> iterator = servlets.iterator(); retVal == null && iterator.hasNext();) {
+        for (Iterator<IServletExtension> iterator = servlets.iterator(); retVal == null
+                && iterator.hasNext();) {
             IServletExtension extension = iterator.next();
             if (!extension.isDisabled() && matches(extension, req)) {
                 retVal = extension.getServlet();
@@ -120,99 +110,25 @@ public class SymmetricServlet extends AbstractServlet {
         return retVal;
     }
 
-    public boolean matches(IServletExtension ext, ServletRequest request) {
-        boolean retVal = true;
-        if (request instanceof HttpServletRequest) {
-            final HttpServletRequest httpRequest = (HttpServletRequest) request;
-            final String uri = normalizeRequestUri(httpRequest);
-            if (!ArrayUtils.isEmpty(ext.getUriPatterns())) {
-                retVal = matchesUriPatterns(uri, ext.getUriPatterns());
-            }
-        }
-        return retVal;
-    }
-
-    protected boolean matchesUriPatterns(String uri, String[] uriPatterns) {
-        boolean retVal = false;
-        for (int i = 0; !retVal && i < uriPatterns.length; i++) {
-            retVal = matchesUriPattern(uri, uriPatterns[i]);
-        }
-        return retVal;
-    }
-
-    protected boolean matchesUriPattern(String uri, String uriPattern) {
-        boolean retVal = false;
-        String path = StringUtils.defaultIfEmpty(uri, "/");
-        final String pattern = StringUtils.defaultIfEmpty(uriPattern, "/");
-        if ("/".equals(pattern) || "/*".equals(pattern) || pattern.equals(path)) {
-            retVal = true;
-        } else {
-            final String[] patternParts = StringUtils.split(pattern, "/");
-            final String[] pathParts = StringUtils.split(path, "/");
-            boolean matches = true;
-            for (int i = 0; i < patternParts.length && i < pathParts.length && matches; i++) {
-                final String patternPart = patternParts[i];
-                matches = "*".equals(patternPart) || patternPart.equals(pathParts[i]);
-            }
-            retVal = matches;
-        }
-        return retVal;
-    }
-
-    /**
-     * Returns the part of the path we are interested in when doing pattern
-     * matching. This should work whether or not the servlet or filter is
-     * explicitly mapped inside of the web.xml since it always strips off the
-     * contextPath.
-     * 
-     * @param httpRequest
-     * @return
-     */
-    protected String normalizeRequestUri(HttpServletRequest httpRequest) {
-        String retVal = httpRequest.getRequestURI();
-        String contextPath = httpRequest.getContextPath();
-        if (retVal.startsWith(contextPath)) {
-            retVal = retVal.substring(contextPath.length());
-        }
-        return retVal;
-    }
-
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        Servlet servlet = findMatchingServlet(req, res);
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException,
+            IOException {
+        Servlet servlet = findMatchingServlet(req);
         if (servlet != null) {
             try {
                 servlet.service(req, res);
             } catch (Exception e) {
-                logException(req, e, !(e instanceof IOException && StringUtils.isNotBlank(e.getMessage())));
+                logException(req, e,
+                        !(e instanceof IOException && StringUtils.isNotBlank(e.getMessage())));
                 if (!res.isCommitted()) {
-                    if (res instanceof HttpServletResponse) {
-                        ((HttpServletResponse) res).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                    }
+                    res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
                 }
             }
         } else {
-            if (req instanceof HttpServletRequest) {
-                HttpServletRequest httpRequest = (HttpServletRequest) req;
-                log.error("ServletNotFoundToHandleRequest", normalizeRequestUri(httpRequest), req.getRemoteHost(), req.getRemoteAddr(), httpRequest.getQueryString());
-                if (res instanceof HttpServletResponse) {
-                    ((HttpServletResponse) res).sendRedirect("/");
-                }
-            }
+            log.error("ServletNotFoundToHandleRequest", ServletUtils.normalizeRequestUri(req),
+                    req.getRemoteHost(), req.getRemoteAddr(), req.getQueryString());
+            res.sendRedirect("/");
         }
     }
 
-    protected void logException(ServletRequest req, Exception ex, boolean isError) {
-        String nodeId = req.getParameter(WebConstants.NODE_ID);
-        String externalId = req.getParameter(WebConstants.EXTERNAL_ID);
-        String address = req.getRemoteAddr();
-        String hostName = req.getRemoteHost();
-        String method = req instanceof HttpServletRequest ? ((HttpServletRequest) req).getMethod() : "";
-        if (getLog().isErrorEnabled() && isError) {
-            getLog().error("ServletProcessingFailedError", ex, method, externalId, nodeId, address, hostName);
-        } else if (getLog().isWarnEnabled()) {
-            getLog().warn("ServletProcessingFailedWarning", method, externalId, nodeId, address, hostName,
-                    ex.getMessage());
-        }
-    }
 }
