04196938deff0a6a9ecad5fd3f9ec5cb778b3b7e
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-01-24 21:23:10 +0000

    SYMMETRICDS-571 - removed the requirement for setting the stringtype property on the jdbc url

--- branches/2.5/symmetric-core/src/main/java/org/jumpmind/symmetric/db/postgresql/PostgresSqlStatementBuilder.java
+++ branches/2.5/symmetric-core/src/main/java/org/jumpmind/symmetric/db/postgresql/PostgresSqlStatementBuilder.java
@@ -86,4 +86,40 @@ public class PostgresSqlStatementBuilder extends StatementBuilder {
         }
     }
 
+    @Override
+    public void appendColumnQuestions(StringBuilder sql, Column[] columns) {
+        for (int i = 0; i < columns.length; i++) {
+            if (columns[i] != null) {
+                if (columns[i].getTypeCode() == -101) {
+                    sql.append("cast(? as timestamp with time zone)").append(",");
+                } else {
+                    sql.append("?").append(",");
+                }
+            }
+        }
+
+        if (columns.length > 0) {
+            sql.replace(sql.length() - 1, sql.length(), "");
+        }
+    }
+
+    @Override
+    public void appendColumnEquals(StringBuilder sql, Column[] columns, String separator) {
+        for (int i = 0; i < columns.length; i++) {
+            if (columns[i] != null) {
+                if (columns[i].getTypeCode() == -101) {
+                    sql.append(quote).append(columns[i].getName()).append(quote)
+                            .append(" = cast(? as timestamp with time zone)").append(separator);
+                } else {
+                    sql.append(quote).append(columns[i].getName()).append(quote).append(" = ?")
+                            .append(separator);
+                }
+            }
+        }
+
+        if (columns.length > 0) {
+            sql.replace(sql.length() - separator.length(), sql.length(), "");
+        }
+    }
+
 }
