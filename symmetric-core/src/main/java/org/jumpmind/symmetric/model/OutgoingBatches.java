a5b48663c458b2e9c5983fdf5ea44e52b8d234b7
chenson42 <chenson42@gmail.com>
2012-02-01 02:17:45 +0000

    Fix bug in ordering channels during extract

--- symmetric-core/src/main/java/org/jumpmind/symmetric/model/OutgoingBatches.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/model/OutgoingBatches.java
@@ -243,7 +243,7 @@ public class OutgoingBatches implements Serializable {
 
         final HashMap<String, Date> errorChannels = new HashMap<String, Date>();
         for (OutgoingBatch batch : batches) {
-            if (batch.getStatus().equals(OutgoingBatch.Status.ER)) {
+            if (batch.isErrorFlag()) {
                 errorChannels.put(batch.getChannelId(), batch.getLastUpdatedTime());
             }
         }
@@ -255,7 +255,7 @@ public class OutgoingBatches implements Serializable {
                 if (!isError1 && !isError2) {
                     return b1.getProcessingOrder() < b2.getProcessingOrder() ? -1 : 1;
                 } else if (isError1 && isError2) {
-                    return errorChannels.get(b1.getChannelId()).compareTo(errorChannels.get(b2.getChannelId()));
+                    return b1.getProcessingOrder() < b2.getProcessingOrder() ? -1 : 1;
                 } else if (!isError1 && isError2) {
                     return -1;
                 } else {
