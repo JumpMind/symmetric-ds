597c503040b4a908d2ca487b08ca5cbf2210befc
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-03-06 21:12:55 +0000

    dev checkin.  fix pom.xml

--- trunk/symmetric/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/IDatabaseWriterConflictResolver.java
+++ trunk/symmetric/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/IDatabaseWriterConflictResolver.java
@@ -6,5 +6,7 @@ import org.jumpmind.symmetric.io.data.CsvData;
 public interface IDatabaseWriterConflictResolver extends IExtensionPoint {
 
     public void needsResolved(DatabaseWriter writer, DatabaseWriterSettings writerSettings, CsvData data);
+    
+    public void reportConflict(DatabaseWriter writer, DatabaseWriterSettings writerSettings, CsvData data, ConflictEvent conflictEvent);
 
 }
