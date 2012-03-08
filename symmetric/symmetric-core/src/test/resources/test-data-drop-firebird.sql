31b7bb4d18d3cb42677d1e1a954ae5fefd5fa1c9
chenson42 <chenson42@gmail.com>
2012-03-08 18:21:18 +0000

    try to drop other mixed case versions of generators

--- symmetric/symmetric-core/src/test/resources/test-data-drop-firebird.sql
+++ symmetric/symmetric-core/src/test/resources/test-data-drop-firebird.sql
@@ -1,5 +1,10 @@
 drop generator gen_sym_trigger_trigger_hist_id;
 drop generator gen_sym_data_data_id;
 drop generator gen_sym_outgoing_batch_batch_id;
+drop generator "gen_SYM_DATA__DATA_ID_";
+drop generator "gen_SYM_TRIGGER_TRIGGER_HIST_ID";
+drop generator "gen_SYM_DATA_DATA_ID";
+drop generator "gen_SYM_OUTGOING_BATCH_BATCH_ID";
+
 
 drop table TEST_MIXED_CASE;
