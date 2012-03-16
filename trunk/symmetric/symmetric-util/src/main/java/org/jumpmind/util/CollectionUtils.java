f7c98bf56de38726923bb43c6155d4d0833f7226
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-03-16 18:14:48 +0000

    dev checkin.

--- trunk/symmetric/symmetric-util/src/main/java/org/jumpmind/util/CollectionUtils.java
+++ trunk/symmetric/symmetric-util/src/main/java/org/jumpmind/util/CollectionUtils.java
@@ -16,5 +16,27 @@ public class CollectionUtils {
             return new HashMap<String, T>(0);
         }
     }
+    
+    /**
+     * Translate an array of {@link Object} to an array of {@link String} by
+     * creating a new array of {@link String} and putting each of the objects
+     * into the array by calling {@link Object#toString()}
+     * 
+     * @param orig
+     *            the original array
+     * @return a newly constructed string array
+     */
+    public static String[] toStringArray(Object[] orig) {
+        String[] array = null;
+        if (orig != null) {
+            array = new String[orig.length];
+            for (int i = 0; i < orig.length; i++) {
+                if (orig[i] != null) {
+                    array[i] = orig[i].toString();
+                }
+            }
+        }
+        return array;
+    }
 
 }
