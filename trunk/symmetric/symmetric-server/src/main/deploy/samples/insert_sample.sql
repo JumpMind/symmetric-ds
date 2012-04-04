42accdb017cc835b5edb67b45215bcdbb0042394
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-04-04 19:28:15 +0000

    quote tables and columns

--- trunk/symmetric/symmetric-server/src/main/deploy/samples/insert_sample.sql
+++ trunk/symmetric/symmetric-server/src/main/deploy/samples/insert_sample.sql
@@ -1,12 +1,12 @@
 ------------------------------------------------------------------------------
 -- Sample Data
 ------------------------------------------------------------------------------
-insert into item_selling_price (price_id, price, cost) values (1, 0.10, 0.09);
-insert into item (item_id, price_id, name) values (11000001, 1, 'Yummy Gum');
+insert into "item_selling_price" ("price_id", "price", "cost") values (1, 0.10, 0.09);
+insert into "item" ("item_id", "price_id", "name") values (11000001, 1, 'Yummy Gum');
 
-insert into sale_transaction (tran_id, store, workstation, day, seq) 
+insert into "sale_transaction" ("tran_id", "store", "workstation", "day", "seq") 
 values (900, '1', '3', '2007-11-01', 90);
-insert into sale_return_line_item (tran_id, item_id, price, quantity, returned_quantity)
+insert into "sale_return_line_item" ("tran_id", "item_id", "price", "quantity", "returned_quantity")
 values (900, 11000001, 0.10, 1, 0);
 
 ------------------------------------------------------------------------------
