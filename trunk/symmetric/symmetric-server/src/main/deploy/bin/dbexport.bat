e985468df8ade2dd3566c18fdc92536cd55c987c
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-04-13 12:41:59 +0000

    call command class

--- trunk/symmetric/symmetric-server/src/main/deploy/bin/dbexport.bat
+++ trunk/symmetric/symmetric-server/src/main/deploy/bin/dbexport.bat
@@ -11,4 +11,4 @@ for %%i in ("%HOMEDIR%\web\WEB-INF\lib\*.jar") do call "%PRGDIR%cpappend.bat" %%
 
 rem echo CLASSPATH=%CLASSPATH%
 
-java -Duser.language=en -Djava.io.tmpdir=../tmp -Dsym.keystore.file="%HOMEDIR%\security\keystore" -Djavax.net.ssl.trustStore="%HOMEDIR%\security\cacerts" -Dlog4j.configuration="file:%CONFDIR%\log4j.xml" -Dsun.net.client.defaultReadTimeout=1800000 -Dsun.net.client.defaultConnectTimeout=1800000 org.jumpmind.symmetric.DbExport %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Duser.language=en -Djava.io.tmpdir=../tmp -Dsym.keystore.file="%HOMEDIR%\security\keystore" -Djavax.net.ssl.trustStore="%HOMEDIR%\security\cacerts" -Dlog4j.configuration="file:%CONFDIR%\log4j.xml" -Dsun.net.client.defaultReadTimeout=1800000 -Dsun.net.client.defaultConnectTimeout=1800000 org.jumpmind.symmetric.DbExportCommand %1 %2 %3 %4 %5 %6 %7 %8 %9
