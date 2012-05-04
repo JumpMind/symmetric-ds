480e08d97f5452b85989c8cf2a25d0624f1a6885
chenson42 <chenson42@gmail.com>
2012-05-04 17:29:22 +0000

    fix firebird read tables

--- symmetric/symmetric-jdbc/src/test/java/org/jumpmind/db/DatabasePlatformTest.java
+++ symmetric/symmetric-jdbc/src/test/java/org/jumpmind/db/DatabasePlatformTest.java
@@ -2,13 +2,12 @@ package org.jumpmind.db;
 
 import java.io.InputStreamReader;
 
-import junit.framework.Assert;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.jumpmind.db.io.DatabaseIO;
 import org.jumpmind.db.model.Table;
 import org.jumpmind.db.platform.IDatabasePlatform;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -18,29 +17,44 @@ public class DatabasePlatformTest extends AbstractDbTest {
 
     protected final static String SIMPLE_TABLE = "test_simple_table";
 
+    protected final static String UPPERCASE_TABLE = "TEST_UPPERCASE_TABLE";
+
+    protected Level originalLevel;
+
     @BeforeClass
     public static void setup() throws Exception {
         platform = DbTestUtils.createDatabasePlatform(DbTestUtils.ROOT);
     }
 
+    //@Before
+    public void turnOnDebug() {
+        Logger logger = Logger.getLogger("org.jumpmind.db");
+        originalLevel = logger.getLevel();
+        logger.setLevel(Level.TRACE);
+    }
+
+    //@After
+    public void turnOffDebug() {
+        Logger logger = Logger.getLogger("org.jumpmind.db");
+        logger.setLevel(originalLevel);
+    }
+
     @Test
     public void testCreateAndReadTestSimpleTable() throws Exception {
+        platform.createDatabase(new DatabaseIO().read(new InputStreamReader(
+                DatabasePlatformTest.class.getResourceAsStream("/testCreateDatabase.xml"))), true,
+                false);
+        Table table = platform.getTableFromCache(SIMPLE_TABLE, true);
+        Assert.assertNotNull("Could not find " + SIMPLE_TABLE, table);
+        Assert.assertEquals("The id column was not read in as an autoincrement column", true, table
+                .getColumnWithName("id").isAutoIncrement());
+    }
 
-        Logger logger = Logger.getLogger("org.jumpmind.db");
-        Level origLevel = logger.getLevel();
-        try {
-            logger.setLevel(Level.TRACE);
-
-            platform.createDatabase(new DatabaseIO().read(new InputStreamReader(
-                    DatabasePlatformTest.class.getResourceAsStream("/testCreateDatabase.xml"))),
-                    true, false);
-            Table table = platform.getTableFromCache(SIMPLE_TABLE, true);
-            Assert.assertNotNull("Could not find " + SIMPLE_TABLE, table);
-            Assert.assertEquals("The id column was not read in as an autoincrement column", true,
-                    table.getColumnWithName("id").isAutoIncrement());
-            
-        } finally {
-            logger.setLevel(origLevel);
-        }
+    @Test
+    public void testReadTestUppercase() throws Exception {
+        Table table = platform.getTableFromCache(UPPERCASE_TABLE, true);
+        Assert.assertNotNull("Could not find " + UPPERCASE_TABLE, table);
+        Assert.assertEquals("The id column was not read in as an autoincrement column", true, table
+                .getColumnWithName("id").isAutoIncrement());
     }
 }
