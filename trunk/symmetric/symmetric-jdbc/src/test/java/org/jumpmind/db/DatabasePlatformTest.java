c0ab0366e5039fcf2ed050e4c37d8c8d021b0d97
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-05-04 16:30:16 +0000

    help with debugging platform unit test

--- trunk/symmetric/symmetric-jdbc/src/test/java/org/jumpmind/db/DatabasePlatformTest.java
+++ trunk/symmetric/symmetric-jdbc/src/test/java/org/jumpmind/db/DatabasePlatformTest.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 
 import junit.framework.Assert;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.jumpmind.db.io.DatabaseIO;
 import org.jumpmind.db.model.Table;
 import org.jumpmind.db.platform.IDatabasePlatform;
@@ -14,19 +16,31 @@ public class DatabasePlatformTest extends AbstractDbTest {
 
     private static IDatabasePlatform platform;
 
+    protected final static String SIMPLE_TABLE = "test_simple_table";
+
     @BeforeClass
     public static void setup() throws Exception {
         platform = DbTestUtils.createDatabasePlatform(DbTestUtils.ROOT);
     }
 
     @Test
-    public void testCreateDatabase() throws Exception {
-        platform.createDatabase(new DatabaseIO().read(new InputStreamReader(
-                DatabasePlatformTest.class.getResourceAsStream("/testCreateDatabase.xml"))), true,
-                false);
-        
-        Table table = platform.getTableFromCache("test_simple_table", true);
-        Assert.assertNotNull(table);
-        Assert.assertEquals(true, table.getColumnWithName("id").isAutoIncrement());
+    public void testCreateAndReadTestSimpleTable() throws Exception {
+
+        Logger logger = Logger.getLogger("org.jumpmind.db");
+        Level origLevel = logger.getLevel();
+        try {
+            logger.setLevel(Level.TRACE);
+
+            platform.createDatabase(new DatabaseIO().read(new InputStreamReader(
+                    DatabasePlatformTest.class.getResourceAsStream("/testCreateDatabase.xml"))),
+                    true, false);
+            Table table = platform.getTableFromCache(SIMPLE_TABLE, true);
+            Assert.assertNotNull("Could not find " + SIMPLE_TABLE, table);
+            Assert.assertEquals("The id column was not read in as an autoincrement column", true,
+                    table.getColumnWithName("id").isAutoIncrement());
+            
+        } finally {
+            logger.setLevel(origLevel);
+        }
     }
 }
