4e4cd316c1e93e52fa2f438b6d3d383de7e804fa
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-05-09 13:35:23 +0000

    imports

--- trunk/symmetric/symmetric-server/src/main/java/org/jumpmind/symmetric/integrate/XmlPublisherDataLoaderFilter.java
+++ trunk/symmetric/symmetric-server/src/main/java/org/jumpmind/symmetric/integrate/XmlPublisherDataLoaderFilter.java
@@ -29,8 +29,6 @@ import org.jumpmind.db.model.Table;
 import org.jumpmind.symmetric.ext.INodeGroupExtensionPoint;
 import org.jumpmind.symmetric.io.data.CsvData;
 import org.jumpmind.symmetric.io.data.DataContext;
-import org.jumpmind.symmetric.io.data.IDataReader;
-import org.jumpmind.symmetric.io.data.IDataWriter;
 import org.jumpmind.symmetric.io.data.writer.IDatabaseWriterFilter;
 
 /**
