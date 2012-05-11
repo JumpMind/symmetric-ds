0c46d2293f49e87cf70d2c63d4f8f18174f6cc44
chenson42 <chenson42@gmail.com>
2012-05-11 12:49:13 +0000

    Better conflict message.  Should end up in outgoing_batch.

--- symmetric/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/ConflictException.java
+++ symmetric/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/ConflictException.java
@@ -6,27 +6,32 @@ import org.jumpmind.symmetric.io.data.CsvData;
 public class ConflictException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
-    
+
     protected CsvData data;
-    
+
     protected Table table;
-    
+
     protected boolean fallbackOperationFailed = false;
 
     public ConflictException(CsvData data, Table table, boolean fallbackOperationFailed) {
+        super(String.format(
+                "Detected conflict while executing %s on %s.  The primary data was: %s. %s", data
+                        .getDataEventType().toString(), table.getFullyQualifiedTableName(), data
+                        .toColumnNameValuePairs(table.getPrimaryKeyColumnNames(), CsvData.PK_DATA),
+                fallbackOperationFailed ? "Failed to fallback." : ""));
         this.data = data;
         this.table = table;
         this.fallbackOperationFailed = fallbackOperationFailed;
     }
-    
+
     public CsvData getData() {
         return data;
     }
-    
+
     public Table getTable() {
         return table;
     }
-    
+
     public boolean isFallbackOperationFailed() {
         return fallbackOperationFailed;
     }
