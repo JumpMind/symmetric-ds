9a91d9b56cdfc747a574f3275d1e54cc173c353d
Austin Brougher <austin.brougher@gmail.com>
2012-06-17 19:00:28 +0000

    Fixed an issue where ThresholdFileWriter unit tests would periodically fail on windows depending on the order the unit tests were run.
    One of the unit tests held a handle on a file while also trying to delete the file. This caused a second unit test to fail.

--- symmetric/symmetric-io/src/test/java/org/jumpmind/symmetric/io/data/stage/ThresholdFileWriterTest.java
+++ symmetric/symmetric-io/src/test/java/org/jumpmind/symmetric/io/data/stage/ThresholdFileWriterTest.java
@@ -19,6 +19,7 @@
  * under the License.  */
 package org.jumpmind.symmetric.io.data.stage;
 
+import java.io.BufferedReader;
 import java.io.File;
 
 import org.apache.commons.io.IOUtils;
@@ -35,24 +36,38 @@ public class ThresholdFileWriterTest {
         File file = getTestFile();
         ThresholdFileWriter writer = new ThresholdFileWriter(TEST_STR.length() + 1, new StringBuilder(), file);
         writer.write(TEST_STR);
+
+        // File does not exist since we did not meet the threshold
         Assert.assertFalse(file.exists());
+        
+        // Check the contents of the buffer (not yet written to a file)
         Assert.assertEquals(TEST_STR, IOUtils.toString(writer.getReader()));
-        file.delete();
     }
 
     @Test
     public void testWriteToFile() throws Exception {
         File file = getTestFile();
-        ThresholdFileWriter writer = new ThresholdFileWriter(TEST_STR.length() - 1, new StringBuilder(), file);
+        Assert.assertFalse(file.exists());
+
+        ThresholdFileWriter writer = new ThresholdFileWriter( TEST_STR.length() - 1, new StringBuilder(), file);
         writer.write(TEST_STR);
+        writer.close();
+
+        // The write string exceeded the threshold so the writer should have created/written to the file
         Assert.assertTrue(file.exists());
-        Assert.assertEquals(TEST_STR, IOUtils.toString(writer.getReader()));
-        file.delete();
+        
+        BufferedReader reader = writer.getReader();
+        Assert.assertEquals(TEST_STR, IOUtils.toString(reader));
+        reader.close();
+
+        Assert.assertTrue(file.delete());
     }
 
     private File getTestFile() {
         File file = new File("target/test/buffered.file.writer.tst");
         file.getParentFile().mkdirs();
+        
+        // Make sure the file doesn't already exist
         file.delete();
         return file;
     }
