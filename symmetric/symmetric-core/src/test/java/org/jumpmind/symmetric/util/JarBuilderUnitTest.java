c86cf44b524d53d3a5cbd6bfd2a175ed807f38e8
chenson42 <chenson42@gmail.com>
2012-06-21 18:44:38 +0000

    0000660: Created war file has bogus ../war directory entry that causes errors in Tomcat 7.0

--- symmetric/symmetric-core/src/test/java/org/jumpmind/symmetric/util/JarBuilderUnitTest.java
+++ symmetric/symmetric-core/src/test/java/org/jumpmind/symmetric/util/JarBuilderUnitTest.java
@@ -29,9 +29,6 @@ import junit.framework.Assert;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
-/**
- * 
- */
 public class JarBuilderUnitTest {
 
     @Test
@@ -42,23 +39,27 @@ public class JarBuilderUnitTest {
         Assert.assertFalse(outputFile.exists());
         
         FileUtils.deleteDirectory(new File(TEST_JAR_DIR));
-        
+                
         mkdir(TEST_JAR_DIR + "/subdir");
+        mkdir(TEST_JAR_DIR + "/META-INF");
+        emptyFile(TEST_JAR_DIR + "/META-INF/MANIFEST.MF");
         emptyFile(TEST_JAR_DIR + "/subdir/file2.txt");
         emptyFile(TEST_JAR_DIR + "/file2.txt");
         emptyFile("target/file1.txt");
+        emptyFile(TEST_JAR_DIR + "/file3.txt");
         
-        JarBuilder jarFile = new JarBuilder(new File("target"), outputFile, new File[] { new File(TEST_JAR_DIR), new File("target/file1.txt") });
+        JarBuilder jarFile = new JarBuilder(new File(TEST_JAR_DIR), outputFile, new File[] { new File(TEST_JAR_DIR), new File("target/file1.txt") });
         jarFile.build();
         
         Assert.assertTrue(outputFile.exists());
         
         JarFile finalJar = new JarFile(outputFile);
-        Assert.assertNotNull(finalJar.getEntry("test.jar.dir/subdir/file2.txt"));
-        Assert.assertNotNull(finalJar.getEntry("test.jar.dir/file2.txt"));
-        Assert.assertNotNull(finalJar.getEntry("test.jar.dir/file2.txt"));
-        Assert.assertNotNull(finalJar.getEntry("file1.txt"));
-        Assert.assertNotNull(finalJar.getEntry("file1.txt"));
+        Assert.assertNotNull(finalJar.getEntry("subdir/file2.txt"));
+        Assert.assertNotNull(finalJar.getEntry("file2.txt"));
+        Assert.assertNull(finalJar.getEntry("target/test.jar.dir"));
+        Assert.assertNull(finalJar.getEntry("test.jar.dir"));
+        Assert.assertNull(finalJar.getEntry("file1.txt"));
+        Assert.assertNotNull(finalJar.getEntry("file3.txt"));
     }
 
     private void mkdir(String dir) {
