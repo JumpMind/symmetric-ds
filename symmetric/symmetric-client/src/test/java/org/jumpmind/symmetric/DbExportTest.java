f666f41e17516d65bb7fcee7e7a08e169672a47b
chenson42 <chenson42@gmail.com>
2012-06-27 23:43:44 +0000

    working on making dbexport consistent.

--- symmetric/symmetric-client/src/test/java/org/jumpmind/symmetric/DbExportTest.java
+++ symmetric/symmetric-client/src/test/java/org/jumpmind/symmetric/DbExportTest.java
@@ -13,18 +13,27 @@ import org.junit.Test;
 public class DbExportTest extends AbstractServiceTest {
 
     @Test
-    public void exportTestDatabase() throws Exception {
+    public void exportTestDatabaseSQL() throws Exception {
         DataSource ds = getSymmetricEngine().getDataSource();
         DbExport export = new DbExport(ds);
         export.setFormat(Format.SQL);
         export.setNoCreateInfo(false);
         export.setNoData(true);
-        export.setSchema(getSymmetricEngine().getSymmetricDialect().getPlatform().getDefaultSchema());
-        export.setCatalog(getSymmetricEngine().getSymmetricDialect().getPlatform().getDefaultCatalog());
+        export.setSchema(getSymmetricEngine().getSymmetricDialect().getPlatform()
+                .getDefaultSchema());
+        export.setCatalog(getSymmetricEngine().getSymmetricDialect().getPlatform()
+                .getDefaultCatalog());
         export.setCompatible(Compatible.H2);
-        String output = export.exportTables();
-        Assert.assertEquals(output, 64, StringUtils.countMatches(output, "CREATE TABLE"));
-        Assert.assertEquals(output, 37, StringUtils.countMatches(output, "CLOB"));
+        String output = export.exportTables().toLowerCase();
+        Assert.assertEquals(output, 31, StringUtils.countMatches(output, "create table \"sym_")
+                - StringUtils.countMatches(output, "create table \"sym_on_"));
+        int longvarcharCount = StringUtils.countMatches(output, "varchar(" + Integer.MAX_VALUE
+                + ")");
+        // the count may be 34 on platforms that map longvarchar to longvarchar and clob to clob
+        // it will be 35 on platforms that map clob to longvarchar
+        Assert.assertTrue("The count was " + longvarcharCount
+                + ". It should have been 34 or 35.  The sql generated was:\n" + output,
+                longvarcharCount == 34 || longvarcharCount == 35);
     }
-    
+
 }
