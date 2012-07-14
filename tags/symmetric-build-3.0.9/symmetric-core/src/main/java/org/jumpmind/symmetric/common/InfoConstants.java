6f2809ec06dbf8d66c7afeb44c21df5127dd4eea
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-07-14 15:16:41 +0000

    [maven-release-plugin]  copy for tag symmetric-build-3.0.9

---
+++ tags/symmetric-build-3.0.9/symmetric-core/src/main/java/org/jumpmind/symmetric/common/InfoConstants.java
@@ -0,0 +1,23 @@
+package org.jumpmind.symmetric.common;
+
+final public class InfoConstants {
+
+    private InfoConstants() {
+    }
+    
+    public final static String NODE_GROUP_IDS = "group.ids";
+
+    public final static String NODE_ID = "node.id";
+    
+    public final static String DATABASE_TYPE = "database.type";    
+    
+    public final static String DATABASE_VERSION = "database.version";
+    
+    public final static String DEPLOYMENT_TYPE = "deployment.type";
+    
+    public final static String TIMEZONE_OFFSET = "timezone.offset";
+    
+    public final static String SYMMETRIC_VERSION = "symmetric.version"; 
+    
+    
+}
---
+++ tags/symmetric-build-3.0.9/symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/RouterServiceSqlMap.java
@@ -0,0 +1,44 @@
+package org.jumpmind.symmetric.service.impl;
+
+import java.util.Map;
+
+import org.jumpmind.db.platform.IDatabasePlatform;
+
+public class RouterServiceSqlMap extends AbstractSqlMap {
+
+    public RouterServiceSqlMap(IDatabasePlatform platform, Map<String, String> replacementTokens) {
+        super(platform, replacementTokens);
+
+        putSql("selectDataToBatchSql",
+                ""
+                        + "select d.data_id, d.table_name, d.event_type, d.row_data, d.pk_data, d.old_data,                                                                        "
+                        + "  d.create_time, d.trigger_hist_id, d.channel_id, d.transaction_id, d.source_node_id, d.external_data, e.data_id from $(data) d left outer join   "
+                        + "  $(data_event) e on d.data_id=e.data_id where d.channel_id=? and d.data_id > ? order by d.data_id asc                                            ");
+
+        putSql("selectDataUsingGapsSql",
+                ""
+                        + "select d.data_id, d.table_name, d.event_type, d.row_data, d.pk_data, d.old_data,                        "
+                        + "  d.create_time, d.trigger_hist_id, d.channel_id, d.transaction_id, d.source_node_id, d.external_data   "
+                        + "  from $(data) d where d.channel_id=? $(dataRange)                                                "
+                        + "  order by d.data_id asc                                                                                ");
+
+        putSql("selectDistinctDataIdFromDataEventSql",
+                ""
+                        + "select distinct(data_id) from $(data_event) where data_id > ? order by data_id asc   ");
+
+        putSql("selectDistinctDataIdFromDataEventUsingGapsSql",
+                ""
+                        + "select distinct(data_id) from $(data_event) where data_id >=? and data_id <= ? order by data_id asc   ");
+
+        putSql("selectUnroutedCountForChannelSql", ""
+                + "select count(*) from $(data) where channel_id=? and data_id >=?   ");
+
+        putSql("selectLastDataIdRoutedUsingDataRefSql", ""
+                + "select max(ref_data_id) from $(data)_ref   ");
+
+        putSql("selectLastDataIdRoutedUsingDataGapSql", ""
+                + "select max(end_id) from $(data_gap)   ");
+
+    }
+
+}
