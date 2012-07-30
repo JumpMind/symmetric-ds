7f074f72a856e8d2ad401726c1100b856c96f1ae
eegeek <eegeek@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-07-30 21:25:17 +0000

    0000724 - Extensive edits to the Quick Start Tutorial, including a column match filter for routing to specific stores.

--- trunk/symmetric-server/src/main/deploy/samples/insert_sample.sql
+++ trunk/symmetric-server/src/main/deploy/samples/insert_sample.sql
@@ -1,14 +1,14 @@
 ------------------------------------------------------------------------------
 -- Sample Data
 ------------------------------------------------------------------------------
-insert into "item" ("item_id", "price_id", "name") values (11000001, 1, 'Yummy Gum');
-insert into "item_selling_price" ("item_id", "store_id","price", "cost") values (11000001,'001', 0.50, 0.09);
-insert into "item_selling_price" ("item_id", "store_id","price", "cost") values (11000001,'002', 0.60, 0.19);
+insert into "item" ("item_id", "name") values (11000001, 'Yummy Gum');
+insert into "item_selling_price" ("item_id", "store", "price", "cost") values (11000001, '001',0.20, 0.10);
+insert into "item_selling_price" ("item_id", "store", "price", "cost") values (11000001, '002',0.30, 0.20);
 
-insert into "sale_transaction" ("tran_id", "store_id", "workstation", "day", "seq") 
-values (900, '001', '3', '2007-11-01', 90);
+insert into "sale_transaction" ("tran_id", "store", "workstation", "day", "seq") 
+values (900, '001', '3', '2012-12-01', 90);
 insert into "sale_return_line_item" ("tran_id", "item_id", "price", "quantity", "returned_quantity")
-values (900, 11000001, 0.10, 1, 0);
+values (900, 11000001, 0.20, 1, 0);
 
 ------------------------------------------------------------------------------
 -- Sample Symmetric Configuration
@@ -34,8 +34,8 @@ insert into sym_node_group_link (source_node_group_id, target_node_group_id, dat
 values ('corp', 'store', 'W');
 
 insert into sym_node (node_id, node_group_id, external_id, sync_enabled)
-values ('00000', 'corp', '00000', 1);
-insert into sym_node_identity values ('00000');
+values ('000', 'corp', '000', 1);
+insert into sym_node_identity values ('000');
 
 --
 -- Channels
@@ -80,42 +80,49 @@ values('sale_transaction_dead','sale_transaction','sale_transaction',0,0,0,curre
 -- Routers
 --
 
--- In this example, both routers pass everything all the time.
+-- In this example, two routers pass everything all the time, and a third router
+-- passes information to the specific store mentioned in the column 'store'
 
 insert into sym_router 
-(router_id,source_node_group_id,target_node_group_id,create_time,last_update_time)
-values('corp_store_identity', 'corp', 'store', current_timestamp, current_timestamp);
+(router_id,source_node_group_id,target_node_group_id,router_type,create_time,last_update_time)
+values('corp_2_store', 'corp', 'store', 'default',current_timestamp, current_timestamp);
 
 insert into sym_router 
-(router_id,source_node_group_id,target_node_group_id,create_time,last_update_time)
-values('store_corp_identity', 'store', 'corp', current_timestamp, current_timestamp);
+(router_id,source_node_group_id,target_node_group_id,router_type,create_time,last_update_time)
+values('store_2_corp', 'store', 'corp', 'default',current_timestamp, current_timestamp);
+
+insert into sym_router 
+(router_id,source_node_group_id,target_node_group_id,router_type,create_time,last_update_time)
+values('corp_2_one_store', 'corp', 'store', 'column','store_id=:EXTERNAL_ID OLD_store_id=:EXTERNAL_ID'current_timestamp, current_timestamp);
 
 --
 -- Trigger Router Links
 --
 
-insert into sym_trigger_router 
-(trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('item_selling_price','corp_store_identity',100,current_timestamp,current_timestamp);
 
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('item','corp_store_identity', 200, current_timestamp, current_timestamp);
+values('item','corp_2_store', 100, current_timestamp, current_timestamp);
+
+insert into sym_trigger_router 
+(trigger_id,router_id,initial_load_order,initial_load_select,last_update_time,create_time)
+values('item_selling_price','corp_2_one_store',100,'store_id=$(externalId)',current_timestamp,current_timestamp);
+
 
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('sale_transaction','store_corp_identity', 200, current_timestamp, current_timestamp);
+values('sale_transaction','store_2_corp', 200, current_timestamp, current_timestamp);
 
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('sale_return_line_item','store_corp_identity', 205, current_timestamp, current_timestamp);
+values('sale_return_line_item','store_2_corp', 200, current_timestamp, current_timestamp);
 
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('sale_tender_line_item','store_corp_identity', 210, current_timestamp, current_timestamp);
+values('sale_tender_line_item','store_2_corp', 200, current_timestamp, current_timestamp);
 
 -- Example of a "dead" trigger, which is used to only sync the table during initial load
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('sale_transaction_dead','corp_store_identity', 300, current_timestamp, current_timestamp);
+values('sale_transaction_dead','store_2_corp', 200, current_timestamp, current_timestamp);
 
