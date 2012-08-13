b087ef60fcdd41e8e09b91a555b7679f3e18ddaf
chenson42 <chenson42@gmail.com>
2012-08-13 13:19:51 +0000

    0000770: fix firebird unit test.  timestamp precision.

--- symmetric-server/src/test/java/org/jumpmind/symmetric/integrate/XmlPublisherFilterTest.java
+++ symmetric-server/src/test/java/org/jumpmind/symmetric/integrate/XmlPublisherFilterTest.java
@@ -40,7 +40,7 @@ public class XmlPublisherFilterTest {
 
     private static final String TABLE_TEST = "TEST_XML_PUBLISHER";
 
-    private static final String TEST_SIMPLE_TRANSFORM_RESULTS = "<batch xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" id=\"12\" nodeid=\"54321\" batchid=\"1111\" time=\"test\"><row entity=\"TEST_XML_PUBLISHER\" dml=\"I\"><data key=\"ID1\">1</data><data key=\"ID2\">2</data><data key=\"DATA1\">test embedding an &amp;</data><data key=\"DATA2\">3</data><data key=\"DATA3\" xsi:nil=\"true\" /></row></batch>";
+    private static final String TEST_SIMPLE_TRANSFORM_RESULTS = "<batch xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" id=\"12\" binary=\"BASE64\" nodeid=\"54321\" batchid=\"1111\" time=\"test\"><row entity=\"TEST_XML_PUBLISHER\" dml=\"I\"><data key=\"ID1\">1</data><data key=\"ID2\">2</data><data key=\"DATA1\">test embedding an &amp;</data><data key=\"DATA2\">3</data><data key=\"DATA3\" xsi:nil=\"true\" /></row></batch>";
 
     private DataContext context;
 
