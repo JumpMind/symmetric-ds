f8084fc98eb1ea0c0d6d6947710c2293730a42e4
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-08-22 20:42:29 +0000

    [maven-release-plugin]  copy for tag symmetric-build-3.1.2

---
+++ tags/symmetric-build-3.1.2/symmetric-jdbc/src/main/java/org/jumpmind/db/platform/oracle/OracleDatabasePlatform.java
@@ -0,0 +1,102 @@
+package org.jumpmind.db.platform.oracle;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import javax.sql.DataSource;
+
+import org.apache.commons.lang.StringUtils;
+import org.jumpmind.db.model.Column;
+import org.jumpmind.db.platform.AbstractJdbcDatabasePlatform;
+import org.jumpmind.db.platform.DatabaseNamesConstants;
+import org.jumpmind.db.sql.DmlStatement;
+import org.jumpmind.db.sql.DmlStatement.DmlType;
+import org.jumpmind.db.sql.JdbcUtils;
+import org.jumpmind.db.sql.SqlTemplateSettings;
+import org.springframework.jdbc.support.lob.OracleLobHandler;
+
+/*
+ * Provides support for the Oracle platform.
+ */
+public class OracleDatabasePlatform extends AbstractJdbcDatabasePlatform {
+
+    /* The standard Oracle jdbc driver. */
+    public static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+
+    /* The old Oracle jdbc driver. */
+    public static final String JDBC_DRIVER_OLD = "oracle.jdbc.dnlddriver.OracleDriver";
+
+    /* The thin subprotocol used by the standard Oracle driver. */
+    public static final String JDBC_SUBPROTOCOL_THIN = "oracle:thin";
+
+    /* The thin subprotocol used by the standard Oracle driver. */
+    public static final String JDBC_SUBPROTOCOL_OCI8 = "oracle:oci8";
+
+    /* The old thin subprotocol used by the standard Oracle driver. */
+    public static final String JDBC_SUBPROTOCOL_THIN_OLD = "oracle:dnldthin";
+
+    /*
+     * Creates a new platform instance.
+     */
+    public OracleDatabasePlatform(DataSource dataSource, SqlTemplateSettings settings) {
+        super(dataSource, settings);
+    }
+
+    @Override
+    protected OracleDdlBuilder createDdlBuilder() {
+        return new OracleDdlBuilder();
+    }
+
+    @Override
+    protected OracleDdlReader createDdlReader() {
+        return new OracleDdlReader(this);
+    }    
+    
+    @Override
+    protected OracleJdbcSqlTemplate createSqlTemplate() {
+        OracleLobHandler lobHandler = new OracleLobHandler();
+        lobHandler.setNativeJdbcExtractor(JdbcUtils.getNativeJdbcExtractory());
+        return new OracleJdbcSqlTemplate(dataSource, settings, lobHandler, getDatabaseInfo());
+    }
+
+    public String getName() {
+        return DatabaseNamesConstants.ORACLE;
+    }
+
+    public String getDefaultCatalog() {
+        return null;
+    }
+
+    public String getDefaultSchema() {
+        if (StringUtils.isBlank(defaultSchema)) {
+            defaultSchema = (String) getSqlTemplate().queryForObject(
+                    "SELECT sys_context('USERENV', 'CURRENT_SCHEMA') FROM dual", String.class);
+        }
+        return defaultSchema;
+    }
+
+    @Override
+    public DmlStatement createDmlStatement(DmlType dmlType, String catalogName, String schemaName,
+            String tableName, Column[] keys, Column[] columns, boolean[] nullKeyValues) {
+        return new OracleDmlStatement(dmlType, catalogName, schemaName, tableName, keys, columns,
+                getDatabaseInfo().isDateOverridesToTimestamp(), getDatabaseInfo()
+                        .getDelimiterToken(), nullKeyValues);
+    }
+
+}
