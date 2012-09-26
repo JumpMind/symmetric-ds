2c2c8a868a1fb144986830b772de5a4b2586283a
chenson42 <chenson42@gmail.com>
2012-09-26 12:04:45 +0000

    fix unit test

--- symmetric-fs/src/test/java/org/jumpmind/symmetric/fs/client/FileSystemSyncStatusPersisterTest.java
+++ symmetric-fs/src/test/java/org/jumpmind/symmetric/fs/client/FileSystemSyncStatusPersisterTest.java
@@ -2,6 +2,7 @@ package org.jumpmind.symmetric.fs.client;
 
 import java.io.File;
 
+import org.apache.commons.io.FileUtils;
 import org.jumpmind.symmetric.fs.config.DirectorySpec;
 import org.jumpmind.symmetric.fs.config.Node;
 import org.jumpmind.symmetric.fs.config.NodeDirectorySpecKey;
@@ -12,8 +13,9 @@ import org.junit.Test;
 public class FileSystemSyncStatusPersisterTest {
 
     @Test
-    public void testSave() {
+    public void testSave() throws Exception {
         final File dir = new File("target/sync_status");
+        FileUtils.deleteDirectory(dir);
         dir.mkdirs();
         FileSystemSyncStatusPersister persister = new FileSystemSyncStatusPersister(dir.getAbsolutePath());
         Node node = new Node("12345", "clientgroup", "http://10.2.34.11/fsync", "DFR#$#3223S#D%%");
@@ -29,7 +31,7 @@ public class FileSystemSyncStatusPersisterTest {
         Assert.assertNotNull(newStatus.getStage());
         Assert.assertEquals(status.getStage(), newStatus.getStage());
         
-        File expectedFile = new File(dir, "12345.status");
+        File expectedFile = new File(dir, persister.buildFileNameFor(key));
         Assert.assertTrue(expectedFile.exists());
         
     }
