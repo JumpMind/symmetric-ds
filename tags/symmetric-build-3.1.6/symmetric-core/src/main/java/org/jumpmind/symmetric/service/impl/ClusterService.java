158b9a4c96204bb3b941feafc3ed2b06fd3871b8
symmetricds <symmetricds@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-10-04 11:22:36 +0000

    [maven-release-plugin]  copy for tag symmetric-build-3.1.6

---
+++ tags/symmetric-build-3.1.6/symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/ClusterService.java
@@ -0,0 +1,185 @@
+/*
+ * Licensed to JumpMind Inc under one or more contributor 
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding 
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU Lesser General Public License (the
+ * "License"); you may not use this file except in compliance
+ * with the License. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see           
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. 
+ */
+package org.jumpmind.symmetric.service.impl;
+
+import static org.jumpmind.symmetric.service.ClusterConstants.HEARTBEAT;
+import static org.jumpmind.symmetric.service.ClusterConstants.PULL;
+import static org.jumpmind.symmetric.service.ClusterConstants.PURGE_DATA_GAPS;
+import static org.jumpmind.symmetric.service.ClusterConstants.PURGE_INCOMING;
+import static org.jumpmind.symmetric.service.ClusterConstants.PURGE_OUTGOING;
+import static org.jumpmind.symmetric.service.ClusterConstants.PURGE_STATISTICS;
+import static org.jumpmind.symmetric.service.ClusterConstants.PUSH;
+import static org.jumpmind.symmetric.service.ClusterConstants.ROUTE;
+import static org.jumpmind.symmetric.service.ClusterConstants.SYNCTRIGGERS;
+import static org.jumpmind.symmetric.service.ClusterConstants.STAGE_MANAGEMENT;
+import static org.jumpmind.symmetric.service.ClusterConstants.WATCHDOG;
+import static org.jumpmind.symmetric.service.ClusterConstants.STATISTICS;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.time.DateUtils;
+import org.jumpmind.db.sql.ISqlRowMapper;
+import org.jumpmind.db.sql.Row;
+import org.jumpmind.db.sql.UniqueKeyException;
+import org.jumpmind.symmetric.common.ParameterConstants;
+import org.jumpmind.symmetric.db.ISymmetricDialect;
+import org.jumpmind.symmetric.model.Lock;
+import org.jumpmind.symmetric.service.IClusterService;
+import org.jumpmind.symmetric.service.IParameterService;
+import org.jumpmind.symmetric.util.AppUtils;
+
+/**
+ * @see IClusterService
+ */
+public class ClusterService extends AbstractService implements IClusterService {
+
+    private String serverId = AppUtils.getServerId();
+
+    public ClusterService(IParameterService parameterService, ISymmetricDialect dialect) {
+        super(parameterService, dialect);
+        setSqlMap(new ClusterServiceSqlMap(symmetricDialect.getPlatform(),
+                createSqlReplacementTokens()));
+    }
+
+    public void init() {
+        initLockTable(ROUTE);
+        initLockTable(PULL);
+        initLockTable(PUSH);
+        initLockTable(HEARTBEAT);
+        initLockTable(PURGE_INCOMING);
+        initLockTable(PURGE_OUTGOING);
+        initLockTable(PURGE_STATISTICS);
+        initLockTable(SYNCTRIGGERS);
+        initLockTable(PURGE_DATA_GAPS);
+        initLockTable(STAGE_MANAGEMENT);
+        initLockTable(WATCHDOG);
+        initLockTable(STATISTICS);
+    }
+
+    public void initLockTable(final String action) {
+        try {
+            sqlTemplate.update(getSql("insertLockSql"), new Object[] { action });
+            log.debug("Inserted into the NODE_LOCK table for {}", action);
+        } catch (UniqueKeyException ex) {
+            log.debug(
+                    "Failed to insert to the NODE_LOCK table for {}.  Must be initialized already.",
+                    action);
+        }
+    }
+
+    public void clearAllLocks() {
+        sqlTemplate.update(getSql("clearAllLocksSql"));
+    }
+
+    public boolean lock(final String action) {
+        if (isClusteringEnabled()) {
+            final Date timeout = DateUtils.add(new Date(), Calendar.MILLISECOND,
+                    (int) -parameterService.getLong(ParameterConstants.CLUSTER_LOCK_TIMEOUT_MS));
+            return lock(action, timeout, new Date(), serverId);
+        } else {
+            return true;
+        }
+    }
+
+    protected boolean lock(String action, Date timeToBreakLock, Date timeLockAquired,
+            String serverId) {
+        return sqlTemplate.update(getSql("aquireLockSql"), new Object[] { serverId,
+                timeLockAquired, action, timeToBreakLock, serverId }) == 1;
+    }
+
+    public Map<String, Lock> findLocks() {
+        final Map<String, Lock> locks = new HashMap<String, Lock>();
+        if (isClusteringEnabled()) {
+            sqlTemplate.query(getSql("findLocksSql"), new ISqlRowMapper<Lock>() {
+                public Lock mapRow(Row rs) {
+                    Lock lock = new Lock();
+                    lock.setLockAction(rs.getString("lock_action"));
+                    lock.setLockingServerId(rs.getString("locking_server_id"));
+                    lock.setLockTime(rs.getDateTime("lock_time"));
+                    lock.setLastLockingServerId(rs.getString("last_locking_server_id"));
+                    lock.setLastLockTime(rs.getDateTime("last_lock_time"));
+                    locks.put(lock.getLockAction(), lock);
+                    return lock;
+                }
+            });
+        }
+        return locks;
+    }
+
+    public String getServerId() {
+        return serverId;
+    }
+
+    public void unlock(final String action) {
+        if (isClusteringEnabled()) {
+            if (!unlock(action, serverId)) {
+                log.warn("Failed to release lock for action:{} server:{}", action, serverId);
+            }
+        }
+    }
+
+    protected boolean unlock(String action, String serverId) {
+        return sqlTemplate.update(getSql("releaseLockSql"), new Object[] { serverId, action,
+                serverId }) > 0;
+    }
+
+    public boolean isClusteringEnabled() {
+        return parameterService.is(ParameterConstants.CLUSTER_LOCKING_ENABLED);
+    }
+
+    public boolean isInfiniteLocked(String action) {
+        Map<String, Lock> locks = findLocks();
+        Lock lock = locks.get(action);
+        if (lock != null && lock.getLockTime() != null && new Date().before(lock.getLockTime())
+                && Lock.STOPPED.equals(lock.getLockingServerId())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void aquireInfiniteLock(String action) {
+        if (isClusteringEnabled()) {
+            int tries = 60;
+            Date futureTime = DateUtils.add(new Date(), Calendar.YEAR, 100);
+            while (tries > 0) {
+                if (!lock(action, new Date(), futureTime, Lock.STOPPED)) {
+                    AppUtils.sleep(1000);
+                    tries--;
+                } else {
+                    tries = 0;
+                }
+            }
+        }
+    }
+
+    public void clearInfiniteLock(String action) {
+        Map<String, Lock> all = findLocks();
+        Lock lock = all.get(action);
+        if (lock != null && Lock.STOPPED.equals(lock.getLockingServerId())) {
+            unlock(action, Lock.STOPPED);
+        }
+    }
+
+}
---
+++ tags/symmetric-build-3.1.6/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/AbstractProtocolDataWriter.java
@@ -0,0 +1,218 @@
+package org.jumpmind.symmetric.io.data.writer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.jumpmind.db.model.Column;
+import org.jumpmind.db.model.Table;
+import org.jumpmind.db.util.BinaryEncoding;
+import org.jumpmind.symmetric.io.data.Batch;
+import org.jumpmind.symmetric.io.data.CsvConstants;
+import org.jumpmind.symmetric.io.data.CsvData;
+import org.jumpmind.symmetric.io.data.DataContext;
+import org.jumpmind.symmetric.io.data.IDataWriter;
+import org.jumpmind.util.Statistics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+abstract public class AbstractProtocolDataWriter implements IDataWriter {
+    
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    protected DataContext context;
+
+    protected Batch batch;
+
+    protected Table table;
+
+    protected Map<String, Table> processedTables = new HashMap<String, Table>();
+
+    protected String delimiter = ",";
+
+    protected boolean flushNodeId = true;
+
+    protected Map<Batch, Statistics> statistics = new HashMap<Batch, Statistics>();
+
+    protected List<IProtocolDataWriterListener> listeners;
+
+    protected String sourceNodeId;
+
+    public AbstractProtocolDataWriter(String sourceNodeId,
+            List<IProtocolDataWriterListener> listeners) {
+        this.listeners = listeners;
+        this.sourceNodeId = sourceNodeId;
+    }
+
+    public void open(DataContext context) {
+        this.context = context;
+    }
+
+    public void close() {
+    }
+
+    public void start(Batch batch) {
+        this.statistics.put(batch, new Statistics());
+        this.batch = batch;
+
+        if (listeners != null) {
+            for (IProtocolDataWriterListener listener : listeners) {
+                listener.start(context, batch);
+            }
+        }
+        
+        if (StringUtils.isBlank(sourceNodeId)) {
+            sourceNodeId = batch.getSourceNodeId();
+        }
+
+        if (flushNodeId) {
+            if (StringUtils.isNotBlank(sourceNodeId)) {
+                println(CsvConstants.NODEID, sourceNodeId);
+            }
+            BinaryEncoding binaryEncoding = batch.getBinaryEncoding();
+            if (binaryEncoding != null) {
+                println(CsvConstants.BINARY, binaryEncoding.name());
+            }
+            flushNodeId = false;
+        }
+        if (StringUtils.isNotBlank(batch.getChannelId())) {
+            println(CsvConstants.CHANNEL, batch.getChannelId());
+        }
+        println(CsvConstants.BATCH, Long.toString(batch.getBatchId()));
+    }
+
+    public boolean start(Table table) {
+        if (!batch.isIgnored()) {
+            this.table = table;
+            String catalogName = table.getCatalog();
+            println(CsvConstants.CATALOG, StringUtils.isNotBlank(catalogName) ? catalogName : "");
+            String schemaName = table.getSchema();
+            println(CsvConstants.SCHEMA, StringUtils.isNotBlank(schemaName) ? schemaName : "");
+            println(CsvConstants.TABLE, table.getName());
+            if (!processedTables.containsKey(table.getName()) || 
+                    !processedTables.get(table.getName()).equals(table)) {
+                println(CsvConstants.KEYS, table.getPrimaryKeyColumns());
+                println(CsvConstants.COLUMNS, table.getColumns());
+                this.processedTables.put(table.getName(), table);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void write(CsvData data) {
+        if (!batch.isIgnored()) {
+            statistics.get(batch).increment(DataWriterStatisticConstants.STATEMENTCOUNT);
+            statistics.get(batch).increment(DataWriterStatisticConstants.LINENUMBER);
+            switch (data.getDataEventType()) {
+                case INSERT:
+                    println(CsvConstants.INSERT, data.getCsvData(CsvData.ROW_DATA));
+                    break;
+
+                case UPDATE:
+                    String oldData = data.getCsvData(CsvData.OLD_DATA);
+                    if (StringUtils.isNotBlank(oldData)) {
+                        println(CsvConstants.OLD, oldData);
+                    }
+                    println(CsvConstants.UPDATE, data.getCsvData(CsvData.ROW_DATA),
+                            data.getCsvData(CsvData.PK_DATA));
+                    break;
+
+                case DELETE:
+                    oldData = data.getCsvData(CsvData.OLD_DATA);
+                    if (StringUtils.isNotBlank(oldData)) {
+                        println(CsvConstants.OLD, oldData);
+                    }
+                    println(CsvConstants.DELETE, data.getCsvData(CsvData.PK_DATA));
+                    break;
+
+                case CREATE:
+                    println(CsvConstants.CREATE, data.getCsvData(CsvData.ROW_DATA));
+                    break;
+
+                case BSH:
+                    println(CsvConstants.BSH, data.getCsvData(CsvData.ROW_DATA));
+                    break;
+
+                case SQL:
+                    println(CsvConstants.SQL, data.getCsvData(CsvData.ROW_DATA));
+                    break;
+                    
+                case RELOAD:
+                default:
+                    break;                      
+            }
+        }
+    }
+
+    public void end(Table table) {
+    }
+
+    final public void end(Batch batch, boolean inError) {
+        
+        if (batch.isIgnored()) {
+            println(CsvConstants.IGNORE);
+        }
+        
+        if (!inError) {
+            println(CsvConstants.COMMIT, Long.toString(batch.getBatchId()));
+            endBatch(batch);
+        }
+
+        if (listeners != null && !inError) {
+            for (IProtocolDataWriterListener listener : listeners) {
+                notifyEndBatch(batch, listener);
+            }
+        }
+    }
+
+    abstract protected void endBatch(Batch batch);
+
+    abstract protected void notifyEndBatch(Batch batch, IProtocolDataWriterListener listener);
+
+    protected int println(String key, List<Column> columns) {
+        return println(key, columns.toArray(new Column[columns.size()]));
+    }
+
+    protected int println(String key, Column[] columns) {
+        StringBuilder buffer = new StringBuilder(key);
+        for (int i = 0; i < columns.length; i++) {
+            buffer.append(delimiter);
+            buffer.append(columns[i].getName());
+        }
+        println(buffer.toString());
+        return buffer.length();
+    }
+
+    abstract protected void print(Batch batch, String data);
+
+    protected long println(String... data) {
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < data.length; i++) {
+            if (i != 0) {
+                buffer.append(delimiter);
+            }
+            buffer.append(data[i]);
+        }
+        buffer.append("\n");
+        print(batch, buffer.toString());
+        long byteCount = buffer.length();
+        statistics.get(batch).increment(DataWriterStatisticConstants.BYTECOUNT, byteCount);
+        return byteCount;
+    }
+
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public Map<Batch, Statistics> getStatistics() {
+        return statistics;
+    }
+
+}
