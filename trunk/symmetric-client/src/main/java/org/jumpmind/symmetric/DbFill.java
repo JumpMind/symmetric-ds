4519331d3cb7a9a8cdfaf53c0787d20da0505011
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-10-15 19:22:33 +0000

    lessen the precision on the randomly generated double

--- trunk/symmetric-client/src/main/java/org/jumpmind/symmetric/DbFill.java
+++ trunk/symmetric-client/src/main/java/org/jumpmind/symmetric/DbFill.java
@@ -203,7 +203,10 @@ class DbFill {
     }
     
     private Object randomDouble() {
-        return new java.util.Random().nextDouble();
+        final long places = 1000000000000l;
+        double d = Math.random()*places;
+        long l = Math.round(d);
+        return ((double)l)/(double)places+2 + (double)randomInt();
     }
 
     private Object randomTinyInt() {
