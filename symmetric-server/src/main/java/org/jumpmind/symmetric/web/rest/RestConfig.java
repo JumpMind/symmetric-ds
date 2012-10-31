a696be7154aac7a91524cfbcc48cdc821a5ed882
chenson42 <chenson42@gmail.com>
2012-10-31 13:10:04 +0000

    start of development of a rest api

--- symmetric-server/src/main/java/org/jumpmind/symmetric/web/rest/RestConfig.java
+++ symmetric-server/src/main/java/org/jumpmind/symmetric/web/rest/RestConfig.java
@@ -20,13 +20,23 @@
  */
 package org.jumpmind.symmetric.web.rest;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = "org.jumpmind.symmetric.web.rest")
 @EnableWebMvc
 public class RestConfig {
+    
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(50000000);
+        return multipartResolver;
+    }
 
 }
