903addd9f35062528f42a0f7639d46fda5d117d0
chenson42 <chenson42@gmail.com>
2012-11-01 22:59:59 +0000

    0000872: Add an uninstall feature to SymmetricDS

--- symmetric-server/src/main/java/org/jumpmind/symmetric/web/rest/RestService.java
+++ symmetric-server/src/main/java/org/jumpmind/symmetric/web/rest/RestService.java
@@ -1,3 +1,23 @@
+/*
+ * Licensed to JumpMind Inc under one or more contributor 
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding 
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU Lesser General Public License (the
+ * "License"); you may not use this file except in compliance
+ * with the License. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see           
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. 
+ */
 package org.jumpmind.symmetric.web.rest;
 
 import java.lang.annotation.Annotation;
@@ -99,7 +119,9 @@ public class RestService {
                 response.setMessage(buffer.toString());
                 return response;
             } else if (actionName.equals("uninstall")) {
-                
+                engine.uninstall();
+                ActionResponse response = new ActionResponse("SymmetricDS uninstalled");
+                return response;
             } else if (actionName.equals("reinitialize")) {
 
             } else if (actionName.equals("start")) {
