eda5e58fac18b8792ca6d4f6d0c3a06754359001
abrougher <abrougher@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-11-24 02:12:39 +0000

    0000925: DbFill should ignore the symmetric tables by default.

--- trunk/symmetric-client/src/main/java/org/jumpmind/symmetric/DbFillCommand.java
+++ trunk/symmetric-client/src/main/java/org/jumpmind/symmetric/DbFillCommand.java
@@ -2,38 +2,40 @@ package org.jumpmind.symmetric;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
+import org.apache.commons.lang.ArrayUtils;
+import org.jumpmind.symmetric.common.ParameterConstants;
+import org.jumpmind.symmetric.service.IParameterService;
 
 public class DbFillCommand extends AbstractCommandLauncher {
-    
 
     private static final String OPTION_SCHEMA = "schema";
-    
+
     private static final String OPTION_CATALOG = "catalog";
-    
+
     private static final String OPTION_COUNT = "count";
-    
+
     private static final String OPTION_CASCADE = "cascade";
-    
+
     private static final String OPTION_IGNORE_TABLES = "ignore";
-    
+
     public DbFillCommand() {
         super("dbfill", "[tablename...]", "DbFill.Option.");
     }
-    
+
     public static void main(String[] args) {
         new DbFillCommand().execute(args);
     }
-    
+
     @Override
     protected boolean printHelpIfNoOptionsAreProvided() {
         return false;
     }
-    
+
     @Override
     protected boolean requiresPropertiesFile() {
         return true;
     }
-    
+
     @Override
     protected void printHelp(CommandLine cmd, Options options) {
         System.out.println(app + " version " + Version.version());
@@ -50,7 +52,7 @@ public class DbFillCommand extends AbstractCommandLauncher {
         addOption(options, null, OPTION_CASCADE, false);
         addOption(options, null, OPTION_IGNORE_TABLES, true);
     }
-    
+
     @Override
     protected boolean executeWithOptions(CommandLine line) throws Exception {
         DbFill dbFill = new DbFill(getDatabasePlatform(false));
@@ -60,23 +62,28 @@ public class DbFillCommand extends AbstractCommandLauncher {
         }
         if (line.hasOption(OPTION_CATALOG)) {
             dbFill.setCatalog(line.getOptionValue(OPTION_CATALOG));
-        }    
+        }
         if (line.hasOption(OPTION_COUNT)) {
             dbFill.setRecordCount(Integer.parseInt(line.getOptionValue(OPTION_COUNT)));
         }
         if (line.hasOption(OPTION_CASCADE)) {
             dbFill.setCascading(true);
         }
-        if (line.hasOption(OPTION_IGNORE_TABLES)) {
-            dbFill.setIgnore((line.getOptionValue(OPTION_IGNORE_TABLES).split(",")));
-        }
-        String[] args = line.getArgs();
-        dbFill.fillTables(args);
         
+        String ignore[] = null;
+        if (line.hasOption(OPTION_IGNORE_TABLES)) {
+            ignore = line.getOptionValue(OPTION_IGNORE_TABLES).split(",");
+        }
+        // Ignore the Symmetric config tables.
+        getSymmetricEngine();
+        IParameterService parameterService = engine.getParameterService();
+        String cfgPrefix = parameterService.getString(ParameterConstants.RUNTIME_CONFIG_TABLE_PREFIX);
+        dbFill.setIgnore((String[])ArrayUtils.add(ignore, cfgPrefix));
+        
+        String[] tables = line.getArgs();
+        dbFill.fillTables(tables);
+
         return true;
     }
-    
-    
-    
 
 }
