f4dec276347b42d74eba10bbfc7ab19d82266575
chenson42 <chenson42@gmail.com>
2012-11-26 23:43:13 +0000

    check for null statistics objects

--- symmetric-core/src/main/java/org/jumpmind/symmetric/model/IncomingBatch.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/model/IncomingBatch.java
@@ -107,20 +107,25 @@ public class IncomingBatch implements Serializable {
 
     public void setValues(Statistics readerStatistics, Statistics writerStatistics,
             boolean isSuccess) {
-        byteCount = readerStatistics.get(DataReaderStatistics.READ_BYTE_COUNT);
-        filterMillis = writerStatistics.get(DataWriterStatisticConstants.FILTERMILLIS);
-        databaseMillis = writerStatistics.get(DataWriterStatisticConstants.DATABASEMILLIS);
-        statementCount = writerStatistics.get(DataWriterStatisticConstants.STATEMENTCOUNT);
-        fallbackInsertCount = writerStatistics
-                .get(DataWriterStatisticConstants.FALLBACKINSERTCOUNT);
-        fallbackUpdateCount = writerStatistics
-                .get(DataWriterStatisticConstants.FALLBACKUPDATECOUNT);
-        missingDeleteCount = writerStatistics.get(DataWriterStatisticConstants.MISSINGDELETECOUNT);
-        ignoreCount = writerStatistics.get(DataWriterStatisticConstants.IGNORECOUNT);
-        lastUpdatedTime = new Date();
-        if (!isSuccess) {
-            failedRowNumber = statementCount;
-            failedLineNumber = writerStatistics.get(DataWriterStatisticConstants.LINENUMBER);
+        if (readerStatistics != null) {
+            byteCount = readerStatistics.get(DataReaderStatistics.READ_BYTE_COUNT);
+        }
+        if (writerStatistics != null) {
+            filterMillis = writerStatistics.get(DataWriterStatisticConstants.FILTERMILLIS);
+            databaseMillis = writerStatistics.get(DataWriterStatisticConstants.DATABASEMILLIS);
+            statementCount = writerStatistics.get(DataWriterStatisticConstants.STATEMENTCOUNT);
+            fallbackInsertCount = writerStatistics
+                    .get(DataWriterStatisticConstants.FALLBACKINSERTCOUNT);
+            fallbackUpdateCount = writerStatistics
+                    .get(DataWriterStatisticConstants.FALLBACKUPDATECOUNT);
+            missingDeleteCount = writerStatistics
+                    .get(DataWriterStatisticConstants.MISSINGDELETECOUNT);
+            ignoreCount = writerStatistics.get(DataWriterStatisticConstants.IGNORECOUNT);
+            lastUpdatedTime = new Date();
+            if (!isSuccess) {
+                failedRowNumber = statementCount;
+                failedLineNumber = writerStatistics.get(DataWriterStatisticConstants.LINENUMBER);
+            }
         }
     }
 
