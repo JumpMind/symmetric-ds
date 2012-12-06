e0f4cbaeb759237b8f6f9425d6fe344b91aedc1e
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-12-06 18:41:11 +0000

    fix oracle hint

--- trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/common/ParameterConstants.java
+++ trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/common/ParameterConstants.java
@@ -129,6 +129,7 @@ final public class ParameterConstants {
 
     public final static String DBDIALECT_ORACLE_USE_TRANSACTION_VIEW = "oracle.use.transaction.view";
     public final static String DBDIALECT_ORACLE_TEMPLATE_NUMBER_SPEC = "oracle.template.precision";
+    public final static String DBDIALECT_ORACLE_USE_HINTS = "oracle.use.hints";
     
     public final static String DBDIALECT_ORACLE_TRANSACTION_VIEW_CLOCK_SYNC_THRESHOLD_MS = "oracle.transaction.view.clock.sync.threshold.ms";
 
