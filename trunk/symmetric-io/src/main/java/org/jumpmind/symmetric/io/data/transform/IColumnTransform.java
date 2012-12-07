bfdc9c34ee0da22cd38b253852742f30399835ee
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2012-12-07 16:11:38 +0000

    javadoc

--- trunk/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/transform/IColumnTransform.java
+++ trunk/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/transform/IColumnTransform.java
@@ -26,6 +26,12 @@ import org.jumpmind.db.platform.IDatabasePlatform;
 import org.jumpmind.extension.IExtensionPoint;
 import org.jumpmind.symmetric.io.data.DataContext;
 
+/**
+ * An extension point that can be implemented to provide custom transformation
+ * logic. Column transforms are stateless and so should not keep references to
+ * objects as attributes (Don't use ISymmetricEngineAware in a multi-homed
+ * environment because you might end up with a handle to the wrong engine.)
+ */
 public interface IColumnTransform<T> extends IExtensionPoint {
 
     public String getName();
