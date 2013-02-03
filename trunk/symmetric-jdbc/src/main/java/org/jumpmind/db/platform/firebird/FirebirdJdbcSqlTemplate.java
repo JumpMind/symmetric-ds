f6e762fe1280e3d27ccade0062e5d9968dd2adf4
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-02-03 15:44:50 +0000

    0001030: Exception in clustered environment

--- trunk/symmetric-jdbc/src/main/java/org/jumpmind/db/platform/firebird/FirebirdJdbcSqlTemplate.java
+++ trunk/symmetric-jdbc/src/main/java/org/jumpmind/db/platform/firebird/FirebirdJdbcSqlTemplate.java
@@ -1,9 +1,13 @@
 package org.jumpmind.db.platform.firebird;
 
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.jumpmind.db.platform.DatabaseInfo;
+import org.jumpmind.db.sql.ConcurrencySqlException;
 import org.jumpmind.db.sql.JdbcSqlTemplate;
+import org.jumpmind.db.sql.SqlException;
 import org.jumpmind.db.sql.SqlTemplateSettings;
 import org.jumpmind.db.sql.SymmetricLobHandler;
 
@@ -23,6 +27,16 @@ public class FirebirdJdbcSqlTemplate extends JdbcSqlTemplate {
     @Override
     protected boolean allowsNullForIdentityColumn() {
         return true;
+    }
+    
+    @Override
+    public SqlException translate(String message, Throwable ex) {
+        if (ex instanceof SQLException) {
+            if (((SQLException)ex).getErrorCode() == 335544336) {
+                throw new ConcurrencySqlException(message, ex);
+            }
+        }
+        return super.translate(message, ex);
     }
 
 }
