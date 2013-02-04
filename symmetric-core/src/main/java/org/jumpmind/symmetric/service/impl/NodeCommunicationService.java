e7649cc26f4307eee0bc321eb2761944014d5a33
chenson42 <chenson42@gmail.com>
2013-02-04 21:04:24 +0000

    0001033: Before allocating push/pull jobs check to see if thread count has changed and if so, restart thread pool

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/NodeCommunicationService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/NodeCommunicationService.java
@@ -147,21 +147,28 @@ public class NodeCommunicationService extends AbstractService implements INodeCo
 
     protected ThreadPoolExecutor getExecutor(final CommunicationType communicationType) {
         ThreadPoolExecutor service = executors.get(communicationType);
+        
+        String threadCountParameter = "";
+        switch (communicationType) {
+            case PULL:
+                threadCountParameter = ParameterConstants.PULL_THREAD_COUNT_PER_SERVER;
+                break;
+            case PUSH:
+                threadCountParameter = ParameterConstants.PUSH_THREAD_COUNT_PER_SERVER;
+                break;
+        }
+        int threadCount = parameterService.getInt(threadCountParameter, 1);
+        
+        if (service != null && service.getCorePoolSize() != threadCount) {
+            log.info("{} has changed from {} to {}.  Restarting thread pool", new Object[] { threadCountParameter, service.getCorePoolSize(), threadCount });
+            stop();
+            service = null;
+        }
+        
         if (service == null) {
             synchronized (this) {
                 service = executors.get(communicationType);
                 if (service == null) {
-                    String threadCountParameter = "";
-                    switch (communicationType) {
-                        case PULL:
-                            threadCountParameter = ParameterConstants.PULL_THREAD_COUNT_PER_SERVER;
-                            break;
-                        case PUSH:
-                            threadCountParameter = ParameterConstants.PUSH_THREAD_COUNT_PER_SERVER;
-                            break;
-                    }
-                    int threadCount = parameterService.getInt(threadCountParameter, 1);
-
                     if (threadCount <= 0) {
                         log.warn("{}={} is not a legal value. Defaulting to 1",
                                 threadCountParameter, threadCount);
