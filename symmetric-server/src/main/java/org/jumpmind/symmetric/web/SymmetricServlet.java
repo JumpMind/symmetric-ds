f186a4c7b53b690bfab90980aaaf0b05a04122d5
chenson42 <chenson42@gmail.com>
2013-02-08 15:55:38 +0000

    0001010: Log better messages when server node is unavailable giving users a clue as to what action needs to be taken

--- symmetric-server/src/main/java/org/jumpmind/symmetric/web/SymmetricServlet.java
+++ symmetric-server/src/main/java/org/jumpmind/symmetric/web/SymmetricServlet.java
@@ -142,7 +142,7 @@ public class SymmetricServlet extends HttpServlet {
                 engine = holder.getEngines().get(engineName);
             }
 
-            if (engine == null && holder.getNumerOfEnginesStarting() <= 1 && 
+            if (engineName == null && engine == null && holder.getNumerOfEnginesStarting() <= 1 && 
                     holder.getEngines().size() == 1) {
                 engine = holder.getEngines().values().iterator().next();
             }
