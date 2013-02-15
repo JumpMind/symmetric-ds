352ae4e1cf0fc67b582e3dfe183af321ce03ce5e
symmetricds <symmetricds@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-02-15 20:37:21 +0000

    [maven-release-plugin]  copy for tag symmetric-build-3.3.1.2

---
+++ tags/symmetric-build-3.3.1.2/symmetric-client/src/main/java/org/jumpmind/symmetric/db/JdbcSymmetricDialectFactory.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to JumpMind Inc under one or more contributor 
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding 
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU Lesser General Public License (the
+ * "License"); you may not use this file except in compliance
+ * with the License. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see           
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. 
+ */
+
+package org.jumpmind.symmetric.db;
+
+import org.jumpmind.db.platform.IDatabasePlatform;
+import org.jumpmind.db.platform.db2.Db2DatabasePlatform;
+import org.jumpmind.db.platform.derby.DerbyDatabasePlatform;
+import org.jumpmind.db.platform.firebird.FirebirdDatabasePlatform;
+import org.jumpmind.db.platform.greenplum.GreenplumPlatform;
+import org.jumpmind.db.platform.h2.H2DatabasePlatform;
+import org.jumpmind.db.platform.hsqldb.HsqlDbDatabasePlatform;
+import org.jumpmind.db.platform.hsqldb2.HsqlDb2DatabasePlatform;
+import org.jumpmind.db.platform.informix.InformixDatabasePlatform;
+import org.jumpmind.db.platform.interbase.InterbaseDatabasePlatform;
+import org.jumpmind.db.platform.mariadb.MariaDBDatabasePlatform;
+import org.jumpmind.db.platform.mssql.MsSqlDatabasePlatform;
+import org.jumpmind.db.platform.mysql.MySqlDatabasePlatform;
+import org.jumpmind.db.platform.oracle.OracleDatabasePlatform;
+import org.jumpmind.db.platform.postgresql.PostgreSqlDatabasePlatform;
+import org.jumpmind.db.platform.sqlite.SqliteDatabasePlatform;
+import org.jumpmind.db.platform.sybase.SybaseDatabasePlatform;
+import org.jumpmind.symmetric.db.db2.Db2SymmetricDialect;
+import org.jumpmind.symmetric.db.db2.Db2v9SymmetricDialect;
+import org.jumpmind.symmetric.db.derby.DerbySymmetricDialect;
+import org.jumpmind.symmetric.db.firebird.FirebirdSymmetricDialect;
+import org.jumpmind.symmetric.db.h2.H2SymmetricDialect;
+import org.jumpmind.symmetric.db.hsqldb.HsqlDbSymmetricDialect;
+import org.jumpmind.symmetric.db.hsqldb2.HsqlDb2SymmetricDialect;
+import org.jumpmind.symmetric.db.informix.InformixSymmetricDialect;
+import org.jumpmind.symmetric.db.interbase.InterbaseSymmetricDialect;
+import org.jumpmind.symmetric.db.mariadb.MariaDBSymmetricDialect;
+import org.jumpmind.symmetric.db.mssql.MsSqlSymmetricDialect;
+import org.jumpmind.symmetric.db.mysql.MySqlSymmetricDialect;
+import org.jumpmind.symmetric.db.oracle.OracleSymmetricDialect;
+import org.jumpmind.symmetric.db.postgresql.GreenplumSymmetricDialect;
+import org.jumpmind.symmetric.db.postgresql.PostgreSqlSymmetricDialect;
+import org.jumpmind.symmetric.db.sqlite.SqliteSymmetricDialect;
+import org.jumpmind.symmetric.db.sybase.SybaseSymmetricDialect;
+import org.jumpmind.symmetric.service.IParameterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Factory class that is responsible for creating the appropriate
+ * {@link ISymmetricDialect} for the configured database.
+ */
+public class JdbcSymmetricDialectFactory {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    private IParameterService parameterService;
+
+    private IDatabasePlatform platform;
+
+    public JdbcSymmetricDialectFactory(IParameterService parameterService, IDatabasePlatform platform) {
+        this.parameterService = parameterService;
+        this.platform = platform;
+    }
+
+    public ISymmetricDialect create() {
+
+        AbstractSymmetricDialect dialect = null;
+
+        if (platform instanceof MariaDBDatabasePlatform) {
+            dialect = new MariaDBSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof MySqlDatabasePlatform) {
+                dialect = new MySqlSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof OracleDatabasePlatform) {
+            dialect = new OracleSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof MsSqlDatabasePlatform) {
+            dialect = new MsSqlSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof GreenplumPlatform) {
+            dialect = new GreenplumSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof PostgreSqlDatabasePlatform) {
+            dialect = new PostgreSqlSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof DerbyDatabasePlatform) {
+            dialect = new DerbySymmetricDialect(parameterService, platform);
+        } else if (platform instanceof H2DatabasePlatform) {
+            dialect = new H2SymmetricDialect(parameterService, platform);
+        } else if (platform instanceof HsqlDbDatabasePlatform) {
+            dialect = new HsqlDbSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof HsqlDb2DatabasePlatform) {
+            dialect = new HsqlDb2SymmetricDialect(parameterService, platform);
+        } else if (platform instanceof InformixDatabasePlatform) {
+            dialect = new InformixSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof Db2DatabasePlatform) {
+            int dbMajorVersion = platform.getSqlTemplate().getDatabaseMajorVersion();
+            int dbMinorVersion = platform.getSqlTemplate().getDatabaseMinorVersion();
+            if (dbMajorVersion < 9 || (dbMajorVersion == 9 && dbMinorVersion < 5)) {
+                dialect = new Db2SymmetricDialect(parameterService, platform);
+            } else {
+                dialect = new Db2v9SymmetricDialect(parameterService, platform);
+            }
+        } else if (platform instanceof FirebirdDatabasePlatform) {
+            dialect = new FirebirdSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof SybaseDatabasePlatform) {
+            dialect = new SybaseSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof InterbaseDatabasePlatform) {
+            dialect = new InterbaseSymmetricDialect(parameterService, platform);
+        } else if (platform instanceof SqliteDatabasePlatform) {
+            dialect = new SqliteSymmetricDialect(parameterService, platform);
+        } else {
+            throw new DbNotSupportedException();
+        }
+        return dialect;
+    }
+
+}
---
+++ tags/symmetric-build-3.3.1.2/symmetric-util/src/main/java/org/jumpmind/util/AbstractVersion.java
@@ -0,0 +1,157 @@
+/*
+ * Licensed to JumpMind Inc under one or more contributor 
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding 
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU Lesser General Public License (the
+ * "License"); you may not use this file except in compliance
+ * with the License. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see           
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. 
+ */
+package org.jumpmind.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Follow the Apache versioning scheme documented <a
+ * href="http://apr.apache.org/versioning.html">here</a>.
+ */
+abstract public class AbstractVersion {
+
+    final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory
+            .getLog(getClass());
+
+    public static final int MAJOR_INDEX = 0;
+
+    public static final int MINOR_INDEX = 1;
+
+    public static final int PATCH_INDEX = 2;
+
+    private String version = null;
+
+    abstract protected String getPropertiesFileLocation();
+
+    public String version() {
+        if (version == null) {
+            InputStream is = AbstractVersion.class.getResourceAsStream(getPropertiesFileLocation());
+            if (is != null) {
+                Properties p = new Properties();
+                try {
+                    p.load(is);
+                    version = p.getProperty("version");
+                } catch (IOException e) {
+                    version = "unknown";
+                    log.warn(e, e);
+                }
+            } else {
+                version = "development";
+            }
+        }
+        return version;
+    }
+
+    public String versionWithUnderscores() {
+        return version().replace("[\\.\\-]", "_");
+    }
+
+    public int[] parseVersion(String version) {
+        version = version.replaceAll("[^0-9\\.]", "");
+        int[] versions = new int[3];
+        if (!StringUtils.isEmpty(version)) {
+            String[] splitVersion = version.split("\\.");
+            if (splitVersion.length >= 3) {
+                versions[PATCH_INDEX] = parseVersionComponent(splitVersion[2]);
+            }
+            if (splitVersion.length >= 2) {
+                versions[MINOR_INDEX] = parseVersionComponent(splitVersion[1]);
+            }
+            if (splitVersion.length >= 1) {
+                versions[MAJOR_INDEX] = parseVersionComponent(splitVersion[0]);
+            }
+        }
+        return versions;
+    }
+
+    private int parseVersionComponent(String versionComponent) {
+        int version = 0;
+        try {
+            version = Integer.parseInt(versionComponent);
+        } catch (NumberFormatException e) {
+        }
+        return version;
+    }
+
+    protected boolean isOlderMajorVersion(String version) {
+        return isOlderMajorVersion(parseVersion(version));
+    }
+
+    protected boolean isOlderMajorVersion(int[] versions) {
+        int[] softwareVersion = parseVersion(version());
+        if (versions[MAJOR_INDEX] < softwareVersion[MAJOR_INDEX]) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isOlderVersion(String version) {
+        return isOlderThanVersion(version, version());
+    }
+
+    public boolean isOlderThanVersion(String checkVersion, String targetVersion) {
+
+        if (noVersion(targetVersion) || noVersion(checkVersion)) {
+            return false;
+        }
+
+        int[] checkVersions = parseVersion(checkVersion);
+        int[] targetVersions = parseVersion(targetVersion);
+
+        if (checkVersions[MAJOR_INDEX] < targetVersions[MAJOR_INDEX]) {
+            return true;
+        } else if (checkVersions[MAJOR_INDEX] == targetVersions[MAJOR_INDEX]
+                && checkVersions[MINOR_INDEX] < targetVersions[MINOR_INDEX]) {
+            return true;
+        } else if (checkVersions[MAJOR_INDEX] == targetVersions[MAJOR_INDEX]
+                && checkVersions[MINOR_INDEX] == targetVersions[MINOR_INDEX]
+                && checkVersions[PATCH_INDEX] < targetVersions[PATCH_INDEX]) {
+            return true;
+        }
+        return false;
+    }
+
+    protected boolean noVersion(String targetVersion) {
+        return StringUtils.isBlank(targetVersion);
+    }
+
+    public boolean isOlderMinorVersion(String oldVersion) {
+        String currentVersion = version();
+        if (noVersion(currentVersion) || noVersion(oldVersion)) {
+            return false;
+        }
+
+        int[] checkVersions = parseVersion(oldVersion);
+        int[] targetVersions = parseVersion(currentVersion);
+
+        if (checkVersions[MAJOR_INDEX] < targetVersions[MAJOR_INDEX]) {
+            return true;
+        } else if (checkVersions[MAJOR_INDEX] == targetVersions[MAJOR_INDEX]
+                && checkVersions[MINOR_INDEX] < targetVersions[MINOR_INDEX]) {
+            return true;
+        }
+        return false;
+    }
+}
