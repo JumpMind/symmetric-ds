3f6d6a4255348478443570ff6f43b5250b8462f8
abrougher <abrougher@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-02-16 16:39:52 +0000

    0001049: DbExport allows --sql option without a table name argument.

--- trunk/symmetric-client/src/main/java/org/jumpmind/symmetric/DbExportCommand.java
+++ trunk/symmetric-client/src/main/java/org/jumpmind/symmetric/DbExportCommand.java
@@ -162,11 +162,15 @@ public class DbExportCommand extends AbstractCommandLauncher {
             dbExport.setCatalog(line.getOptionValue(OPTION_CATALOG));
         }        
  
-        String[] args = line.getArgs();
-        if (args.length == 0) {
-            dbExport.exportTables(System.out);
-        } else if (line.hasOption(OPTION_SQL)) {
+        String[] args = line.getArgs();  
+        if (line.hasOption(OPTION_SQL)) {
+            if (args.length != 1) {
+                throw new ParseException(
+                        "When specifying a SQL statement, a table name argument must be provided.");
+            }
             dbExport.exportTable(System.out, args[0], line.getOptionValue(OPTION_SQL));
+        } else if (args.length == 0) {
+            dbExport.exportTables(System.out);
         } else {
             dbExport.exportTables(System.out, args);
         }
