09df5103833a68c02e5ff2432b8b4995887996c3
gwilmer <gwilmer@users.noreply.github.com>
2013-02-22 14:23:00 +0000

    Issue 1057 - Fix Postgres Trigger Template datetime with timestamp formatting when timezone offset is negative and timezone offset contains minutes.

--- symmetric-client/src/main/java/org/jumpmind/symmetric/db/postgresql/PostgreSqlTriggerTemplate.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/db/postgresql/PostgreSqlTriggerTemplate.java
@@ -23,7 +23,7 @@ public class PostgreSqlTriggerTemplate extends AbstractTriggerTemplate {
         		"   when extract(timezone_hour from $(tableAlias).\"$(columnName)\") < 0 then                                        " +
         		"     '\"' || to_char($(tableAlias).\"$(columnName)\", 'YYYY-MM-DD HH24:MI:SS.US ')||'-'||                           " +
         		"     lpad(cast(abs(extract(timezone_hour from $(tableAlias).\"$(columnName)\")) as varchar),2,'0')||':'||           " +
-        		"     lpad(cast(extract(timezone_minute from $(tableAlias).\"$(columnName)\") as varchar), 2, '0') || '\"'           " +
+        		"     lpad(cast(abs(extract(timezone_minute from $(tableAlias).\"$(columnName)\")) as varchar), 2, '0') || '\"'           " +
                 "   else                                                                                                             " +
         		"     '\"' || to_char($(tableAlias).\"$(columnName)\", 'YYYY-MM-DD HH24:MI:SS.US ')||'+'||                           " +
         		"     lpad(cast(extract(timezone_hour from $(tableAlias).\"$(columnName)\") as varchar),2,'0')||':'||                " +
