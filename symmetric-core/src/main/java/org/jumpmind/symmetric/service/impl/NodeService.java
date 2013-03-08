87ebabe5263d6825a9893a06f0ba5bf0ae94f0ca
chenson42 <chenson42@gmail.com>
2013-03-08 13:08:32 +0000

    0001101: sqlite create tables doesn't work because a transaction is in process and the database is locked 0001100: sqlite create tables quotes create_timestamp default values

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/NodeService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/NodeService.java
@@ -672,7 +672,6 @@ public class NodeService extends AbstractService implements INodeService {
                 }
             }
         }
-        log.info("Returning offline list");
         return offlineNodeList;
     }    
 
