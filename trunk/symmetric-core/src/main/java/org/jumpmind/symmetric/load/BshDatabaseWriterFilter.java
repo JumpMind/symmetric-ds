dd30e7d0ad493111b2cf9ea93b2ebe5775fe5901
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-03-09 15:02:51 +0000

    0001104: Exclude sym_ tables from being effected by wildcard load filters

--- trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/load/BshDatabaseWriterFilter.java
+++ trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/load/BshDatabaseWriterFilter.java
@@ -214,7 +214,10 @@ public class BshDatabaseWriterFilter implements IDatabaseWriterFilter, IDatabase
         boolean writeRow = true;
         LoadFilter currentFilter = null;
 
-        List<LoadFilter> wildcardLoadFilters =  loadFilters.get(Table.getFullyQualifiedTableName(table.getCatalog(), table.getSchema(), FormatUtils.WILDCARD));
+        List<LoadFilter> wildcardLoadFilters = null;
+        if (!table.getName().toLowerCase().startsWith(symmetricEngine.getTablePrefix())) {
+            wildcardLoadFilters = loadFilters.get(Table.getFullyQualifiedTableName(table.getCatalog(), table.getSchema(), FormatUtils.WILDCARD));
+        }
         List<LoadFilter> tableSpecificLoadFilters = loadFilters.get(table.getFullyQualifiedTableName());
         int size = (wildcardLoadFilters != null ? wildcardLoadFilters.size() : 0) + (tableSpecificLoadFilters != null ? tableSpecificLoadFilters.size() : 0);
 
