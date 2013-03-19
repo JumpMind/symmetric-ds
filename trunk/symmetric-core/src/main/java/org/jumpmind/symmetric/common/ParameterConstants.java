7963335db610defd79592dbae13a0f370e3c3913
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-03-19 18:30:56 +0000

    0001128: Add new parameter to make heartbeat at server startup optional. Defaulting heartbeat at startup to off.

--- trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/common/ParameterConstants.java
+++ trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/common/ParameterConstants.java
@@ -194,6 +194,7 @@ final public class ParameterConstants {
 
     public final static String OFFLINE_NODE_DETECTION_PERIOD_MINUTES = "offline.node.detection.period.minutes";
     public final static String HEARTBEAT_SYNC_ON_PUSH_PERIOD_SEC = "heartbeat.sync.on.push.period.sec";
+    public final static String HEARTBEAT_SYNC_ON_STARTUP = "heartbeat.sync.on.startup";
     
     public final static String HEARTBEAT_ENABLED = "heartbeat.sync.on.push.enabled";
 
