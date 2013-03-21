6724cf024d22f0cc3e9864eaeb85eac3aa38601e
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-03-21 19:04:29 +0000

    0001086: Make the 3.x use 1.x protocol for backwards compatibility

--- trunk/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/TransformWriter.java
+++ trunk/symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/TransformWriter.java
@@ -152,6 +152,11 @@ public class TransformWriter implements IDataWriter {
     }
 
     public void write(CsvData data) {
+        if (data.requiresTable() && sourceTable == null) {
+            // if we cross batches and the table isn't specified, then
+            // use the last table we used
+            start(context.getLastParsedTable());
+        }        
         DataEventType eventType = data.getDataEventType();
         if (activeTransforms != null && activeTransforms.size() > 0 && isTransformable(eventType)) {
             Map<String, String> sourceValues = data.toColumnNameValuePairs(this.sourceTable.getColumnNames(),
