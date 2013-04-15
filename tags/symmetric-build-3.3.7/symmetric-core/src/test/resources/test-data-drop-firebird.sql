1c9309aa932d1ab5fa41ad0058c35086fb57647a
symmetricds <symmetricds@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-04-15 23:13:56 +0000

    [maven-release-plugin]  copy for tag symmetric-build-3.3.7

---
+++ tags/symmetric-build-3.3.7/symmetric-core/src/test/resources/test-data-drop-firebird.sql
@@ -0,0 +1,7 @@
+drop generator gen_sym_trigger_trigger_hist_id;
+drop generator gen_sym_data_data_id;
+drop generator gen_sym_outgoing_batch_batch_id;
+drop generator "gen_SYM_DATA__DATA_ID_";
+drop generator "gen_SYM_TRIGGER_TRIGGER_HIST_ID";
+drop generator "gen_SYM_DATA_DATA_ID";
+drop generator "gen_SYM_OUTGOING_BATCH_BATCH_ID";
