e358ba29423d2e562b50830f41efd9da0af8ea34
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-04-26 17:20:54 +0000

    get rid of deprecation warnings and unused variables

--- trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/config/NodeIdCreatorAdaptor.java
+++ trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/config/NodeIdCreatorAdaptor.java
@@ -3,9 +3,11 @@ package org.jumpmind.symmetric.config;
 import org.jumpmind.symmetric.model.Node;
 import org.jumpmind.symmetric.service.INodeService;
 
+@SuppressWarnings("deprecation")
 public class NodeIdCreatorAdaptor implements INodeIdCreator {
 
     private INodeService nodeService;
+    
     private INodeIdGenerator nodeIdGenerator;
 
     public NodeIdCreatorAdaptor(INodeIdGenerator generator, INodeService nodeService) {
