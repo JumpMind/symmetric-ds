6f968e8be998415bc7a89b56d1dfc5f1c34a97ec
chenson42 <chenson42@gmail.com>
2013-04-29 12:35:32 +0000

    0001064: Add interactive command line sql tool. dbsql

--- symmetric-client/src/main/java/org/jumpmind/symmetric/DbSqlCommand.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/DbSqlCommand.java
@@ -21,17 +21,33 @@
 package org.jumpmind.symmetric;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.h2.tools.Shell;
 
 public class DbSqlCommand extends AbstractCommandLauncher {
     
+    private static final String OPTION_SQL = "sql";
+    
     public DbSqlCommand() {
-        super("dbsql", "", "DbSqlShell.Option.");
+        super("dbsql", "", "DbSql.Option.");
     }
     
     public static void main(String[] args) {
         new DbSqlCommand().execute(args);
+    }    
+    
+    @Override
+    protected void printHelp(CommandLine cmd, Options options) {
+        System.out.println(app + " version " + Version.version());
+        System.out.println("Provides a sql shell for database interaction from the command line.\n");
+        super.printHelp(cmd, options);
+    }
+
+    @Override
+    protected void buildOptions(Options options) {
+        super.buildOptions(options);
+        addOption(options, null, OPTION_SQL, true);
     }    
 
     @Override
@@ -52,7 +68,13 @@ public class DbSqlCommand extends AbstractCommandLauncher {
         String password = basicDataSource.getPassword();
         String driver = basicDataSource.getDriverClassName();
         Shell shell = new Shell();
-        shell.runTool("-url", url, "-user", user, "-password", password, "-driver", driver);
+        
+        if (line.hasOption(OPTION_SQL)) {
+            String sql = line.getOptionValue(OPTION_SQL);
+            shell.runTool("-url", url, "-user", user, "-password", password, "-driver", driver, "-sql", sql);
+        } else {        
+            shell.runTool("-url", url, "-user", user, "-password", password, "-driver", driver);
+        }
         return true;
     }
 
