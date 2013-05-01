e5994044d2de20f0fcfa51682d58781605b648e8
chenson42 <chenson42@gmail.com>
2013-05-01 12:18:37 +0000

    0000032: Implement file synchronization - dev checkin

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/FileSyncServiceSqlMap.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/FileSyncServiceSqlMap.java
@@ -15,9 +15,16 @@ public class FileSyncServiceSqlMap extends AbstractSqlMap {
                 " select trigger_id, base_dir, recursive, includes_files, excludes_files,       " +
         		"       sync_on_create, sync_on_modified, sync_on_delete, create_time,          " +
         		"       last_update_by, last_update_time                                        " +
-        		" from $(file_trigger)                                                          ");
+        		" from $(file_trigger) t                                                        ");
         
         putSql("whereTriggerId", "where trigger_id=?");
+        
+        putSql("fileTriggerForCurrentNodeWhere", " " +
+        		" inner join $(file_trigger_router) tr on " +
+        		"  t.trigger_id=tr.trigger_id " +
+        		" inner join $(router) r on " +
+        		"  tr.router_id=r.router_id " +
+        		" where r.source_node_group_id=? ");
         
         putSql("updateFileTriggerSql",                 
                 " update $(file_trigger) set base_dir=?, recursive=?, includes_files=?,         " +
@@ -28,7 +35,7 @@ public class FileSyncServiceSqlMap extends AbstractSqlMap {
                 " insert into $(file_trigger) (base_dir, recursive, includes_files,             " +
                 "  excludes_files, sync_on_create, sync_on_modified, sync_on_delete,            " +
                 "  last_update_by, last_update_time, trigger_id, create_time)                   " +
-                " values(?,?,?,?,?,?,?,?,?,?,?)                                                 ");
+                " values(?,?,?,?,?,?,?,?,?,?,?)                                                 ");                
         
         putSql("selectFileSnapshotSql", 
                 " select trigger_id, file_path, file_name, last_event_type, crc32_checksum,     " +
@@ -47,6 +54,29 @@ public class FileSyncServiceSqlMap extends AbstractSqlMap {
                 "  file_size, file_modified_time, create_time, last_update_time,                " +
                 "  last_update_by, trigger_id, file_path, file_name                             " +
                 " ) values(?,?,?,?,?,?,?,?,?,?)                                                 ");
+        
+        putSql("selectFileTriggerRoutersSql", 
+                " select " +
+                "  trigger_id, router_id, enabled, initial_load_enabled, target_base_dir,       " +
+                "  conflict_strategy, create_time, last_update_by, last_update_time             " +
+                " from $(file_trigger_router)                                                   ");
+        
+        putSql("whereTriggerRouterId", "where trigger_id=? and router_id=?");        
+        
+        putSql("updateFileTriggerRouterSql",                 
+                " update $(file_trigger_router) set                                             " +
+                        "  enabled=?, initial_load_enabled=?, target_base_dir=?,                " +
+                        "  conflict_strategy=?, last_update_by=?, last_update_time=?            " +
+                " where trigger_id=? and router_id=?                                            ");
+
+        putSql("insertFileTriggerRouterSql",                 
+                " insert into $(file_trigger_router) (                                          " +
+                        "  enabled, initial_load_enabled, target_base_dir,                      " +
+                        "  conflict_strategy, create_time, last_update_by,                      " +
+                        "  last_update_time, trigger_id, router_id                              " +
+                " ) values(?,?,?,?,?,?,?,?,?)                                                   ");        
+        
+        
         
         
     }
