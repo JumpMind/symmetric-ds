e7f874b74a645f61caf33aa89eadd7f7964d853d
Austin Brougher <austin.brougher@gmail.com>
2013-05-03 19:17:03 +0000

    Adding support for the Sybase ASE dialect.

--- symmetric-jdbc/src/main/java/org/jumpmind/db/platform/sybase/SybaseJdbcSqlTemplate.java
+++ symmetric-jdbc/src/main/java/org/jumpmind/db/platform/sybase/SybaseJdbcSqlTemplate.java
@@ -3,6 +3,8 @@ package org.jumpmind.db.platform.sybase;
 import javax.sql.DataSource;
 
 import org.jumpmind.db.platform.DatabaseInfo;
+import org.jumpmind.db.platform.mssql.MsSqlJdbcSqlTransaction;
+import org.jumpmind.db.sql.ISqlTransaction;
 import org.jumpmind.db.sql.JdbcSqlTemplate;
 import org.jumpmind.db.sql.SqlTemplateSettings;
 import org.jumpmind.db.sql.SymmetricLobHandler;
@@ -14,10 +16,15 @@ public class SybaseJdbcSqlTemplate extends JdbcSqlTemplate {
         super(dataSource, settings, lobHandler, databaseInfo);
         primaryKeyViolationCodes = new int[] {423,511,515,530,547,2601,2615,2714};
     }
-        
+
+    @Override
+    public ISqlTransaction startSqlTransaction() {
+        return new SybaseJdbcSqlTransaction(this);
+    }
+
     @Override
     protected boolean allowsNullForIdentityColumn() {
         return false;
-    } 
-    
+    }
+
 }
