73856a9f876a6cee4fda05f5b24eff6534bbc9db
chenson42 <chenson42@gmail.com>
2013-05-08 12:10:54 +0000

    fix unit test.  port fix to trunk

--- symmetric-client/src/test/java/org/jumpmind/symmetric/DbExportImportTest.java
+++ symmetric-client/src/test/java/org/jumpmind/symmetric/DbExportImportTest.java
@@ -127,7 +127,7 @@ public class DbExportImportTest extends AbstractServiceTest {
         String output = export.exportTables(tables).toLowerCase();
 
         Assert.assertEquals(output, 39, StringUtils.countMatches(output, "create table \"sym_"));
-        Assert.assertEquals(36,
+        Assert.assertEquals(38,
                 StringUtils.countMatches(output, "varchar(" + Integer.MAX_VALUE + ")"));
     }
 
