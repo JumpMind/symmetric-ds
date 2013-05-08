162389ade30bfd6c46b3395aa5670b36805ec9ba
symmetricds <symmetricds@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-05-08 00:49:51 +0000

    [maven-release-plugin]  copy for tag symmetric-build-3.4.3

---
+++ tags/symmetric-build-3.4.3/symmetric-db/src/main/java/org/jumpmind/db/model/ColumnTypes.java
@@ -0,0 +1,14 @@
+package org.jumpmind.db.model;
+
+final public class ColumnTypes {
+
+    private ColumnTypes() {
+    }
+    
+    public static final int SQLXML = 2009;
+    public static final int NCHAR = -15;
+    public static final int NCLOB = 2011;
+    public static final int NVARCHAR = -9;
+    public static final int LONGNVARCHAR = -16;
+    
+}
