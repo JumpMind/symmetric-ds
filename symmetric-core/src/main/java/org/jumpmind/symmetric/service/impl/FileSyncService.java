88a43c8f438cb87e985a2be178551daedca07e49
Austin Brougher <austin.brougher@gmail.com>
2013-05-30 20:46:18 +0000

    Add additional UI support to the file sync service.

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/FileSyncService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/FileSyncService.java
@@ -1,22 +1,22 @@
 /*
- * Licensed to JumpMind Inc under one or more contributor 
+ * Licensed to JumpMind Inc under one or more contributor
  * license agreements.  See the NOTICE file distributed
- * with this work for additional information regarding 
+ * with this work for additional information regarding
  * copyright ownership.  JumpMind Inc licenses this file
  * to you under the GNU Lesser General Public License (the
  * "License"); you may not use this file except in compliance
- * with the License. 
- * 
+ * with the License.
+ *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see           
+ * License along with this library; if not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License. 
+ * under the License.
  */
 package org.jumpmind.symmetric.service.impl;
 
@@ -203,33 +203,45 @@ public class FileSyncService extends AbstractService implements IFileSyncService
     public void saveFileTriggerRouter(FileTriggerRouter fileTriggerRouter) {
         fileTriggerRouter.setLastUpdateTime(new Date());
         if (0 == sqlTemplate.update(
-                getSql("updateFileTriggerSql"),
+                getSql("updateFileTriggerRouterSql"),
                 new Object[] { fileTriggerRouter.isEnabled() ? 1 : 0,
                         fileTriggerRouter.isInitialLoadEnabled() ? 1 : 0,
                         fileTriggerRouter.getTargetBaseDir(),
                         fileTriggerRouter.getTargetFilePath(),
                         fileTriggerRouter.getConflictStrategy().name(),
-                        fileTriggerRouter.getLastUpdateBy(), fileTriggerRouter.getLastUpdateTime(),
+                        fileTriggerRouter.getLastUpdateBy(),
+                        fileTriggerRouter.getLastUpdateTime(),
                         fileTriggerRouter.getFileTrigger().getTriggerId(),
-                        fileTriggerRouter.getRouter().getRouterId() }, new int[] { Types.SMALLINT,
-                        Types.SMALLINT, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
-                        Types.VARCHAR, Types.TIMESTAMP, Types.VARCHAR, Types.VARCHAR })) {
+                        fileTriggerRouter.getRouter().getRouterId() },
+                        new int[] { Types.SMALLINT,  Types.SMALLINT, Types.VARCHAR,
+                        Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                        Types.TIMESTAMP, Types.VARCHAR, Types.VARCHAR })) {
             fileTriggerRouter.setCreateTime(fileTriggerRouter.getLastUpdateTime());
             sqlTemplate.update(
-                    getSql("insertFileTriggerSql"),
+                    getSql("insertFileTriggerRouterSql"),
                     new Object[] { fileTriggerRouter.isEnabled() ? 1 : 0,
                             fileTriggerRouter.isInitialLoadEnabled() ? 1 : 0,
                             fileTriggerRouter.getTargetBaseDir(),
                             fileTriggerRouter.getTargetFilePath(),
                             fileTriggerRouter.getConflictStrategy().name(),
-                            fileTriggerRouter.getCreateTime(), fileTriggerRouter.getLastUpdateBy(),
+                            fileTriggerRouter.getCreateTime(),
+                            fileTriggerRouter.getLastUpdateBy(),
                             fileTriggerRouter.getLastUpdateTime(),
                             fileTriggerRouter.getFileTrigger().getTriggerId(),
-                            fileTriggerRouter.getRouter().getRouterId() }, new int[] {
-                            Types.SMALLINT, Types.SMALLINT, Types.VARCHAR, Types.VARCHAR,
+                            fileTriggerRouter.getRouter().getRouterId() },
+                            new int[] { Types.SMALLINT, Types.SMALLINT, Types.VARCHAR,
                             Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP, Types.VARCHAR,
                             Types.TIMESTAMP, Types.VARCHAR, Types.VARCHAR });
         }
+    }
+
+    public void deleteFileTriggerRouter(FileTriggerRouter fileTriggerRouter) {
+        sqlTemplate.update(getSql("deleteFileTriggerRouterSql"), (Object) fileTriggerRouter.getFileTrigger()
+                .getTriggerId(), fileTriggerRouter.getRouter().getRouterId());
+    }
+
+    public void deleteFileTrigger(FileTrigger fileTrigger) {
+        sqlTemplate.update(getSql("deleteFileTriggerSql"), (Object) fileTrigger.getTriggerId());
     }
 
     public List<FileTriggerRouter> getFileTriggerRouters(FileTrigger fileTrigger) {
