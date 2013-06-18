4b70de717a3312ca3d8cc13c702eed80b94dc06c
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-06-18 14:25:03 +0000

    if external_data is set.  add it to the map, regardless of event type

--- branches/3.4/symmetric-core/src/main/java/org/jumpmind/symmetric/route/AbstractDataRouter.java
+++ branches/3.4/symmetric-core/src/main/java/org/jumpmind/symmetric/route/AbstractDataRouter.java
@@ -28,6 +28,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.jumpmind.db.platform.DatabaseNamesConstants;
 import org.jumpmind.symmetric.SymmetricException;
 import org.jumpmind.symmetric.db.ISymmetricDialect;
@@ -152,7 +153,13 @@ public abstract class AbstractDataRouter implements IDataRouter {
             if (data.size() == 0) {
                 data.putAll(getPkDataAsString(dataMetaData, symmetricDialect));
             }
-            data.put("EXTERNAL_DATA", dataMetaData.getData().getExternalData());
+        }
+        
+        if (StringUtils.isNotBlank(dataMetaData.getData().getExternalData())) {
+        	if (data == null) {
+        		data = new HashMap<String, Object>(1);
+        	}
+            data.put("EXTERNAL_DATA", dataMetaData.getData().getExternalData());        	
         }
         return data;
     }
