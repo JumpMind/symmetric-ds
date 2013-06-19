a1820f79a18581abeda7eff4a14fddef26970c9f
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-06-19 19:35:46 +0000

    0001290: Mariab db is chosen when going through MySQL database setup until SymmetricDS is restarted after setup

--- trunk/symmetric-jdbc/src/main/java/org/jumpmind/db/util/BasicDataSourceFactory.java
+++ trunk/symmetric-jdbc/src/main/java/org/jumpmind/db/util/BasicDataSourceFactory.java
@@ -37,6 +37,25 @@ import org.slf4j.LoggerFactory;
 
 public class BasicDataSourceFactory {
     
+    public static void prepareDriver(String clazzName) throws Exception {
+        Driver driver = (Driver)Class.forName(clazzName).newInstance();
+        synchronized (DriverManager.class) {
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
+            while (drivers.hasMoreElements()) {
+                Driver driver2 = (Driver) drivers.nextElement();                    
+                /* 
+                 * MySQL and Maria DB drivers cannot co-exist because
+                 * they use the same JDBC URL.
+                 */
+                if ((driver.getClass().getName().equals("com.mysql.jdbc.Driver") &&
+                        driver2.getClass().getName().equals("org.mariadb.jdbc.Driver")) ||
+                        (driver.getClass().getName().equals("org.mariadb.jdbc.Driver") &&
+                                driver2.getClass().getName().equals("com.mysql.jdbc.Driver"))) {
+                    DriverManager.deregisterDriver(driver2);
+                }
+            }
+        }
+    }
     
     public static BasicDataSource create(TypedProperties properties) {
         return create(properties, SecurityServiceFactory.create(SecurityServiceType.CLIENT, properties));
@@ -50,23 +69,7 @@ public class BasicDataSourceFactory {
         dataSource.setDriverClassName(properties.get(
                 BasicDataSourcePropertyConstants.DB_POOL_DRIVER, null));
         try {
-            Driver driver = (Driver)Class.forName(dataSource.getDriverClassName()).newInstance();
-            synchronized (DriverManager.class) {
-                Enumeration<Driver> drivers = DriverManager.getDrivers();
-                while (drivers.hasMoreElements()) {
-                    Driver driver2 = (Driver) drivers.nextElement();
-                    /* 
-                     * MySQL and Maria DB drivers cannot co-exist because
-                     * they use the same JDBC URL.
-                     */
-                    if ((driver.getClass().equals("com.mysql.jdbc.Driver") &&
-                            driver2.getClass().getName().equals("org.mariadb.jdbc.Driver")) ||
-                            (driver.getClass().equals("org.mariadb.jdbc.Driver") &&
-                                    driver2.getClass().getName().equals("com.mysql.jdbc.Driver"))) {
-                        DriverManager.deregisterDriver(driver2);
-                    }
-                }
-            }
+            prepareDriver(dataSource.getDriverClassName());
         } catch (Exception e) {
             throw new IllegalStateException("Had trouble registering the jdbc driver: " + dataSource.getDriverClassName(),e);
         }
