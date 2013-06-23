424119208dc89c8dafa66c895ecc2bc02e36f4df
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-06-23 01:23:51 +0000

    unit tests

--- trunk/symmetric-server/src/test/java/org/jumpmind/symmetric/test/NonDmlEventsTest.java
+++ trunk/symmetric-server/src/test/java/org/jumpmind/symmetric/test/NonDmlEventsTest.java
@@ -38,25 +38,113 @@ public class NonDmlEventsTest extends AbstractTest {
     // test sendSchema
     // test sendScript
     // test send reload to multiple nodes
-    
-    Table camelCase;
+
+    Table testTable;
 
     @Override
     protected Table[] getTables(String name) {
-        camelCase = new Table("CamelCase");
-        camelCase.addColumn(new Column("Id", true, Types.BIGINT, -1, -1));
-        camelCase.addColumn(new Column("Notes", false, Types.VARCHAR, 255, 0));
-        return new Table[] { camelCase };
+        testTable = new Table("CamelCase");
+        testTable.addColumn(new Column("Id", true, Types.BIGINT, -1, -1));
+        testTable.addColumn(new Column("Notes", false, Types.VARCHAR, 255, 0));
+
+        Table a = new Table("A");
+        a.addColumn(new Column("Id", true, Types.BIGINT, -1, -1));
+
+        Table b = new Table("B");
+        b.addColumn(new Column("Id", true, Types.BIGINT, -1, -1));
+
+        return new Table[] { testTable, a, b };
     }
 
     @Override
     protected void test(ISymmetricEngine rootServer, ISymmetricEngine clientServer)
             throws Exception {
         loadConfigAndRegisterNode("client", "root");
-        
-        List<TriggerHistory> histories = rootServer.getTriggerRouterService().findTriggerHistories(null, null, camelCase.getName());
+
+        // pull to clear out any heartbeat events
+        pull("client");
+
+        List<TriggerHistory> histories = rootServer.getTriggerRouterService().findTriggerHistories(
+                null, null, testTable.getName());
         Assert.assertNotNull(histories);
         Assert.assertEquals(1, histories.size());
+
+        String quote = rootServer.getDatabasePlatform().getDatabaseInfo().getDelimiterToken();
+        for (int i = 0; i < 100; i++) {
+            rootServer.getSqlTemplate().update(
+                    String.format("insert into %sCamelCase%s values (?,?)", quote, quote), i,
+                    "this is a test");
+        }
+
+        String countSql = String.format("select count(*) from %sCamelCase%s", quote, quote);
+        Assert.assertEquals(100,
+                rootServer.getDatabasePlatform().getSqlTemplate().queryForInt(countSql));
+        Assert.assertEquals(0,
+                clientServer.getDatabasePlatform().getSqlTemplate().queryForInt(countSql));
+
+        // we installed a dead trigger, so no data should have been captured
+        Assert.assertFalse(pull("client"));
+
+        rootServer.getDataService().reloadTable("client", null, null, testTable.getName());
+
+        Assert.assertTrue(pull("client"));
+
+        Assert.assertEquals(100,
+                clientServer.getDatabasePlatform().getSqlTemplate().queryForInt(countSql));
+
+        String clientQuote = clientServer.getDatabasePlatform().getDatabaseInfo()
+                .getDelimiterToken();
+
+        rootServer.getDataService().sendSQL(
+                "client",
+                null,
+                null,
+                testTable.getName(),
+                String.format("insert into %sCamelCase%s values (101,'direct insert')",
+                        clientQuote, clientQuote));
+        rootServer.getDataService().sendSQL(
+                "client",
+                null,
+                null,
+                testTable.getName(),
+                String.format("insert into %sCamelCase%s values (102,'direct insert')",
+                        clientQuote, clientQuote));
+
+        Assert.assertTrue(pull("client"));
+
+        Assert.assertEquals(102,
+                clientServer.getDatabasePlatform().getSqlTemplate().queryForInt(countSql));
+
+        rootServer.getDataService().sendSQL("client", null, null, testTable.getName(),
+                String.format("delete from %sCamelCase%s", clientQuote, clientQuote));
+
+        Assert.assertTrue(pull("client"));
+
+        Assert.assertEquals(0,
+                clientServer.getDatabasePlatform().getSqlTemplate().queryForInt(countSql));
+
+        rootServer.getDataService().reloadTable("client", null, null, testTable.getName(),
+                String.format("%sId%s < 50", quote, quote));
+
+        Assert.assertTrue(pull("client"));
+
+        Assert.assertEquals(50,
+                clientServer.getDatabasePlatform().getSqlTemplate().queryForInt(countSql));
+        
+        // test a wildcard table
+        for (int i = 0; i < 10; i++) {
+            rootServer.getSqlTemplate().update("insert into a values (?)", i);
+        }
+        
+        Assert.assertFalse(pull("client"));
+        
+        rootServer.getDataService().reloadTable("client", null, null, "A");
+
+        Assert.assertTrue(pull("client"));
+
+        Assert.assertEquals(10,
+                clientServer.getDatabasePlatform().getSqlTemplate().queryForInt("select count(*) from A"));
+
     }
 
 }
