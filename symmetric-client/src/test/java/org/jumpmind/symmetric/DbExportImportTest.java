305865c1040c5a529790566aec0600ff20b17f4e
chenson42 <chenson42@gmail.com>
2013-07-14 19:25:33 +0000

    dev checkin for initial load extract job - fix unit test to recognize new table

--- symmetric-client/src/test/java/org/jumpmind/symmetric/DbExportImportTest.java
+++ symmetric-client/src/test/java/org/jumpmind/symmetric/DbExportImportTest.java
@@ -148,7 +148,7 @@ public class DbExportImportTest extends AbstractServiceTest {
         export.setCompatible(Compatible.H2);
         String output = export.exportTables(tables).toLowerCase();
 
-        Assert.assertEquals(output, 40, StringUtils.countMatches(output, "create table \"sym_"));
+        Assert.assertEquals(output, 41, StringUtils.countMatches(output, "create table \"sym_"));
         final int EXPECTED_VARCHAR_MAX = engine.getDatabasePlatform().getName().equals(DatabaseNamesConstants.SQLITE) ? 246 : 41;
         final String EXPECTED_STRING = "varchar(" + Integer.MAX_VALUE + ")";
         Assert.assertEquals("Expected " + EXPECTED_VARCHAR_MAX + " " + EXPECTED_STRING
