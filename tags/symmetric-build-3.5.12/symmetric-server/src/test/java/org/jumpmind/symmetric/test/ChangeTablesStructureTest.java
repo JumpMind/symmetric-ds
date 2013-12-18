5b96b70723a45058a1459e689964c8b4ad74be10
symmetricds <symmetricds@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2013-12-18 22:38:16 +0000

    [maven-release-plugin]  copy for tag symmetric-build-3.5.12

---
+++ tags/symmetric-build-3.5.12/symmetric-server/src/test/java/org/jumpmind/symmetric/test/ChangeTablesStructureTest.java
@@ -0,0 +1,7 @@
+package org.jumpmind.symmetric.test;
+
+public class ChangeTablesStructureTest {
+
+    
+    // table change in same batch - make change.  remove column.  sync triggers.  make change.  route.  sync. 
+}
