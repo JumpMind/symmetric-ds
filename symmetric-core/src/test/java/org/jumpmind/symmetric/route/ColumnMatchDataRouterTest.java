77564ab12227d912215f0645d97f271d92a178c9
erilong <erilong@users.noreply.github.com>
2014-02-11 13:27:35 +0000

    0001566: Column match router support for "contains" and "not contains"

--- symmetric-core/src/test/java/org/jumpmind/symmetric/route/ColumnMatchDataRouterTest.java
+++ symmetric-core/src/test/java/org/jumpmind/symmetric/route/ColumnMatchDataRouterTest.java
@@ -20,10 +20,20 @@
  */
 package org.jumpmind.symmetric.route;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static org.junit.Assert.*;
-
+import org.jumpmind.db.model.Table;
+import org.jumpmind.symmetric.io.data.DataEventType;
+import org.jumpmind.symmetric.model.Data;
+import org.jumpmind.symmetric.model.DataMetaData;
+import org.jumpmind.symmetric.model.Node;
+import org.jumpmind.symmetric.model.NodeChannel;
+import org.jumpmind.symmetric.model.Router;
+import org.jumpmind.symmetric.model.TriggerHistory;
 import org.jumpmind.symmetric.route.ColumnMatchDataRouter.Expression;
 import org.junit.Test;
 
@@ -36,7 +46,7 @@ public class ColumnMatchDataRouterTest {
         assertEquals(3, expressions.size());
         assertEquals("two",expressions.get(0).tokens[1]);
         assertEquals("three",expressions.get(2).tokens[0]);
-        assertEquals(false,expressions.get(2).equals);
+        assertEquals(false,expressions.get(2).hasEquals);
     }
     
     @Test
@@ -46,7 +56,7 @@ public class ColumnMatchDataRouterTest {
         assertEquals(3, expressions.size());
         assertEquals("door",expressions.get(0).tokens[1]);
         assertEquals("three",expressions.get(2).tokens[0]);
-        assertEquals(false,expressions.get(2).equals);
+        assertEquals(false,expressions.get(2).hasEquals);
     }
     
     @Test
@@ -88,7 +98,7 @@ public class ColumnMatchDataRouterTest {
         assertEquals("two",expressions.get(0).tokens[1]);
         assertEquals("three",expressions.get(1).tokens[0]);
         assertEquals("five",expressions.get(2).tokens[0]);
-        assertEquals(false,expressions.get(2).equals);
+        assertEquals(false,expressions.get(2).hasEquals);
     }
     
     @Test
@@ -99,5 +109,257 @@ public class ColumnMatchDataRouterTest {
         assertEquals("ORDER_ID",expressions.get(0).tokens[0]);
         assertEquals(":EXTERNAL_ID",expressions.get(0).tokens[1]);
     }
-    
+
+    @Test
+    public void testExpressionEqualsNodeId() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("100", "client"));
+        nodes.add(new Node("200", "client"));
+        nodes.add(new Node("300", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,NODE_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,100,Super Dooper");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("NODE_ID = :NODE_ID");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(1, result.size());
+        assertEquals(true, result.contains("100"));
+    }
+
+    @Test
+    public void testExpressionNotEqualsNodeId() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("100", "client"));
+        nodes.add(new Node("200", "client"));
+        nodes.add(new Node("300", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,NODE_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,100,Super Dooper");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("NODE_ID != :NODE_ID");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(2, result.size());
+        assertEquals(true, result.contains("200"));
+        assertEquals(true, result.contains("300"));
+    }
+
+    @Test
+    public void testExpressionEqualsExternalId() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("1000", "client"));
+        nodes.add(new Node("100", "client"));
+        nodes.add(new Node("10", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,STORE_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,100,Super Dooper");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("STORE_ID = :EXTERNAL_ID");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(1, result.size());
+        assertEquals(true, result.contains("100"));
+    }
+
+    @Test
+    public void testExpressionEqualsGroupId() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("10", "server"));
+        nodes.add(new Node("20", "server"));
+        nodes.add(new Node("3", "client"));
+        nodes.add(new Node("2", "client"));
+        nodes.add(new Node("1", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,GROUP_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,client,Super Dooper");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("GROUP_ID = :NODE_GROUP_ID");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(3, result.size());
+        assertEquals(true, result.contains("1"));
+        assertEquals(true, result.contains("2"));
+        assertEquals(true, result.contains("3"));
+    }
+
+    @Test
+    public void testExpressionEqualsNull() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("100", "client"));
+        nodes.add(new Node("200", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,NODE_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,100,");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("COLUMN2 = NULL");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(2, result.size());
+        assertEquals(true, result.contains("100"));
+        assertEquals(true, result.contains("200"));
+    }
+
+    @Test
+    public void testExpressionNotEqualsNull() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("100", "client"));
+        nodes.add(new Node("200", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,NODE_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,100,");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("COLUMN2 != NULL");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testExpressionExternalData() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("100", "client"));
+        nodes.add(new Node("200", "client"));
+        nodes.add(new Node("300", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,NODE_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,100,Super Dooper");
+        data.setExternalData("100");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("EXTERNAL_DATA = :NODE_ID");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(1, result.size());
+        assertEquals(true, result.contains("100"));
+    }
+
+    @Test
+    public void testExpressionContains() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("100", "client"));
+        nodes.add(new Node("200", "client"));
+        nodes.add(new Node("300", "client"));
+        nodes.add(new Node("1000", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,NODE_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,100,Super Dooper");
+        data.setExternalData("1000,200");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("EXTERNAL_DATA contains :NODE_ID");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(2, result.size());
+        assertEquals(true, result.contains("1000"));
+        assertEquals(true, result.contains("200"));        
+    }
+
+    @Test
+    public void testExpressionNotContains() {
+        ColumnMatchDataRouter router = new ColumnMatchDataRouter();
+        SimpleRouterContext routingContext = new SimpleRouterContext();
+        HashSet<Node> nodes = new HashSet<Node>();
+        nodes.add(new Node("100", "client"));
+        nodes.add(new Node("200", "client"));
+        nodes.add(new Node("300", "client"));
+        nodes.add(new Node("1000", "client"));
+
+        TriggerHistory triggerHist = new TriggerHistory("mytable","ID","ID,NODE_ID,COLUMN2");
+        Data data = new Data();
+        data.setDataId(1);
+        data.setDataEventType(DataEventType.INSERT);
+        data.setRowData("1,100,Super Dooper");
+        data.setExternalData("1000,200");
+        data.setTriggerHistory(triggerHist);
+        Table table = new Table();
+        NodeChannel nodeChannel = new NodeChannel();
+        Router route = new Router();
+        route.setRouterExpression("EXTERNAL_DATA not contains :NODE_ID");
+        route.setRouterId("route1");
+        DataMetaData dataMetaData = new DataMetaData(data, table, route, nodeChannel);
+        
+        Set<String> result = router.routeToNodes(routingContext, dataMetaData, nodes, false, false);
+        assertEquals(2, result.size());
+        assertEquals(true, result.contains("100"));
+        assertEquals(true, result.contains("300"));
+    }
 }
