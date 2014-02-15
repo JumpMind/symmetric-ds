2a0f4534da69242a19ba68ce9b4c350fbf9e9627
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-02-15 20:35:52 +0000

    [maven-release-plugin]  copy for tag symmetric-build-3.5.17

---
+++ tags/symmetric-build-3.5.17/symmetric-client/src/main/java/org/jumpmind/symmetric/db/hsqldb2/HsqlDb2TriggerTemplate.java
@@ -0,0 +1,111 @@
+/**
+ * Licensed to JumpMind Inc under one or more contributor
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU General Public License, version 3.0 (GPLv3)
+ * (the "License"); you may not use this file except in compliance
+ * with the License.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * version 3.0 (GPLv3) along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jumpmind.symmetric.db.hsqldb2;
+
+import java.util.HashMap;
+
+import org.jumpmind.symmetric.db.AbstractTriggerTemplate;
+import org.jumpmind.symmetric.db.ISymmetricDialect;
+
+public class HsqlDb2TriggerTemplate extends AbstractTriggerTemplate {
+
+    public HsqlDb2TriggerTemplate(ISymmetricDialect symmetricDialect) {
+        super(symmetricDialect);
+        emptyColumnTemplate = "''" ;
+        stringColumnTemplate = "case when $(tableAlias).\"$(columnName)\" is null then '' else '\"'||replace(replace($(tableAlias).\"$(columnName)\",'\\','\\\\'),'\"','\\\"')||'\"' end" ;
+        numberColumnTemplate = "case when $(tableAlias).\"$(columnName)\" is null then '' else '\"'||$(tableAlias).\"$(columnName)\"||'\"' end" ;
+        datetimeColumnTemplate = "case when $(tableAlias).\"$(columnName)\" is null then '' else '\"'||$(tableAlias).\"$(columnName)\"||'\"' end" ;
+        clobColumnTemplate = "case when $(tableAlias).\"$(columnName)\" is null then '' else '\"'||replace(replace($(tableAlias).\"$(columnName)\",'\\','\\\\'),'\"','\\\"')||'\"' end" ;
+        blobColumnTemplate = "case when $(tableAlias).\"$(columnName)\" is null then ''else '\"'||rawtohex($(tableAlias).\"$(columnName)\")||'\"' end" ;
+        booleanColumnTemplate = "case when $(tableAlias).\"$(columnName)\" is null then '' when $(tableAlias).\"$(columnName)\" then '\"1\"' else '\"0\"' end" ;
+        triggerConcatCharacter = "||" ;
+        newTriggerValue = "newrow" ;
+        oldTriggerValue = "oldrow" ;
+        oldColumnPrefix = "" ;
+        newColumnPrefix = "" ;
+
+        sqlTemplates = new HashMap<String,String>();
+        sqlTemplates.put("insertTriggerTemplate" ,
+"create trigger $(triggerName) after insert on $(schemaName)$(tableName)                                                                                                                                " +
+"                                referencing new row as newrow                                                                                                                                          " +
+"                                for each row begin atomic                                                                                                                                              " +
+"                                  if $(syncOnInsertCondition) and $(syncOnIncomingBatchCondition) then                                                                                                 " +
+"                                    insert into $(defaultCatalog)$(prefixName)_data (table_name, event_type, trigger_hist_id, row_data, channel_id, transaction_id, source_node_id, external_data, create_time)" +
+"                                    values(                                                                                                                                                            " +
+"                                      '$(targetTableName)',                                                                                                                                            " +
+"                                      'I',                                                                                                                                                             " +
+"                                      $(triggerHistoryId),                                                                                                                                             " +
+"                                      $(columns),                                                                                                                                                      " +
+"                                      '$(channelName)', $(txIdExpression), $(prefixName)_get_session('node_value'),                                                                                    " +
+"                                      $(externalSelect),                                                                                                                                               " +
+"                                      CURRENT_TIMESTAMP                                                                                                                                                " +
+"                                    );                                                                                                                                                                 " +
+"                                  end if;                                                                                                                                                              " +
+"                                  $(custom_on_insert_text)                                                                                                                                             " +
+"                                end                                                                                                                                                                    " );
+
+        sqlTemplates.put("updateTriggerTemplate" ,
+"create trigger $(triggerName) after update on $(schemaName)$(tableName)                                                                                                                                " +
+"                                referencing new row as newrow                                                                                                                                          " +
+"                                            old row as oldrow                                                                                                                                          " +
+"                                for each row begin atomic                                                                                                                                              " +
+"                                  if $(syncOnUpdateCondition) and $(syncOnIncomingBatchCondition) then                                                                                                 " +
+"	                                    insert into $(defaultCatalog)$(prefixName)_data (table_name, event_type, trigger_hist_id, pk_data, row_data, old_data, channel_id, transaction_id, source_node_id, external_data, create_time)" +
+"	                                    values(                                                                                                                                                           " +
+"	                                      '$(targetTableName)',                                                                                                                                           " +
+"	                                      'U',                                                                                                                                                            " +
+"	                                      $(triggerHistoryId),                                                                                                                                            " +
+"	                                      $(oldKeys),                                                                                                                                                     " +
+"	                                      $(columns),                                                                                                                                                     " +
+"	                                      $(oldColumns),                                                                                                                                                  " +
+"	                                      '$(channelName)', $(txIdExpression), $(prefixName)_get_session('node_value'),                                                                                   " +
+"	                                      $(externalSelect),                                                                                                                                              " +
+"	                                      CURRENT_TIMESTAMP                                                                                                                                               " +
+"	                                    );                                                                                                                                                                " +
+"                                  end if;                                                                                                                                                              " +
+"                                  $(custom_on_update_text)                                                                                                                                             " +
+"                                end                                                                                                                                                                    " );
+
+        sqlTemplates.put("deleteTriggerTemplate" ,
+"create trigger $(triggerName) after delete on $(schemaName)$(tableName)                                                                                                                                " +
+"                                referencing old row as oldrow                                                                                                                                          " +
+"                                for each row begin atomic                                                                                                                                              " +
+"                                  if $(syncOnDeleteCondition) and $(syncOnIncomingBatchCondition) then                                                                                                 " +
+"                                    insert into $(defaultCatalog)$(prefixName)_data (table_name, event_type, trigger_hist_id, pk_data, old_data, channel_id, transaction_id, source_node_id, external_data, create_time)" +
+"                                    values(                                                                                                                                                            " +
+"                                      '$(targetTableName)',                                                                                                                                            " +
+"                                      'D',                                                                                                                                                             " +
+"                                      $(triggerHistoryId),                                                                                                                                             " +
+"                                      $(oldKeys),                                                                                                                                                      " +
+"                                      $(oldColumns),                                                                                                                                                   " +
+"                                      '$(channelName)', $(txIdExpression), $(prefixName)_get_session('node_value'),                                                                                    " +
+"                                      $(externalSelect),                                                                                                                                               " +
+"                                      CURRENT_TIMESTAMP                                                                                                                                                " +
+"                                    );                                                                                                                                                                 " +
+"                                  end if;                                                                                                                                                              " +
+"                                  $(custom_on_delete_text)                                                                                                                                             " +
+"                                end                                                                                                                                                                    " );
+
+        sqlTemplates.put("initialLoadSqlTemplate" ,
+"select $(columns) from $(schemaName)$(tableName) t where $(whereClause)                                                                                                                                " );
+    }
+
+}
