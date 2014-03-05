b73534f3e84fcaa537dd1ad9b70ce82b43fc07f3
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-03-05 21:52:49 +0000

    0001634: Snapshot utility properties show changes, remove passwords, and sort

--- branches/3.5/symmetric-client/src/main/java/org/jumpmind/symmetric/util/SnapshotUtil.java
+++ branches/3.5/symmetric-client/src/main/java/org/jumpmind/symmetric/util/SnapshotUtil.java
@@ -4,29 +4,41 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.TreeSet;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.jumpmind.db.model.Table;
 import org.jumpmind.exception.IoException;
+import org.jumpmind.properties.DefaultParameterParser.ParameterMetaData;
 import org.jumpmind.symmetric.ISymmetricEngine;
 import org.jumpmind.symmetric.Version;
 import org.jumpmind.symmetric.common.ParameterConstants;
 import org.jumpmind.symmetric.common.TableConstants;
 import org.jumpmind.symmetric.io.data.DbExport;
 import org.jumpmind.symmetric.io.data.DbExport.Format;
+import org.jumpmind.symmetric.job.IJob;
+import org.jumpmind.symmetric.job.IJobManager;
 import org.jumpmind.symmetric.model.DataGap;
+import org.jumpmind.symmetric.model.Lock;
 import org.jumpmind.symmetric.model.Trigger;
 import org.jumpmind.symmetric.model.TriggerHistory;
+import org.jumpmind.symmetric.service.IClusterService;
+import org.jumpmind.symmetric.service.INodeService;
 import org.jumpmind.symmetric.service.ITriggerRouterService;
 import org.jumpmind.util.JarBuilder;
 import org.slf4j.Logger;
@@ -83,7 +95,7 @@ public class SnapshotUtil {
             Table table = engine.getDatabasePlatform().getTableFromCache(triggerHistory.getSourceCatalogName(),
                     triggerHistory.getSourceSchemaName(), triggerHistory.getSourceTableName(),
                     false);
-            if (table != null) {
+            if (table != null && !table.getName().toUpperCase().startsWith(engine.getSymmetricDialect().getTablePrefix().toUpperCase())) {
                 tables.add(table);
             }
         }
@@ -104,7 +116,7 @@ public class SnapshotUtil {
             engine.getDataExtractorService().extractConfigurationStandalone(engine.getNodeService().findIdentity(),
                     fwriter, TableConstants.SYM_NODE, TableConstants.SYM_NODE_SECURITY,
                     TableConstants.SYM_NODE_IDENTITY, TableConstants.SYM_NODE_HOST,
-                    TableConstants.SYM_NODE_CHANNEL_CTL);
+                    TableConstants.SYM_NODE_CHANNEL_CTL, TableConstants.SYM_CONSOLE_USER);
         } catch (IOException e) {
             log.warn("Failed to export symmetric configuration", e);
         } finally {
@@ -190,19 +202,53 @@ public class SnapshotUtil {
         try {
             fos = new FileOutputStream(new File(tmpDir, "parameters.properties"));
             Properties effectiveParameters = engine.getParameterService().getAllParameters();
-            effectiveParameters.store(fos, "parameters.properties");
+            SortedProperties parameters = new SortedProperties();
+            parameters.putAll(effectiveParameters);
+            parameters.remove("db.password");            
+            parameters.store(fos, "parameters.properties");
         } catch (IOException e) {
-            log.warn("Failed to export thread information", e);
+            log.warn("Failed to export parameter information", e);
+        } finally {
+            IOUtils.closeQuietly(fos);
+        }
+
+        fos = null;
+        try {
+            fos = new FileOutputStream(new File(tmpDir, "parameters-changed.properties"));
+            Properties defaultParameters = new Properties();
+            InputStream in = SnapshotUtil.class.getResourceAsStream("/symmetric-default.properties");
+            defaultParameters.load(in);
+            IOUtils.closeQuietly(in);
+            in = SnapshotUtil.class.getResourceAsStream("/symmetric-console-default.properties");
+            defaultParameters.load(in);
+            IOUtils.closeQuietly(in);
+            Properties effectiveParameters = engine.getParameterService().getAllParameters();
+            Properties changedParameters = new SortedProperties();
+            Map<String, ParameterMetaData> parameters = ParameterConstants.getParameterMetaData();
+            for (String key: parameters.keySet()) {
+                String defaultValue = defaultParameters.getProperty((String) key);
+                String currentValue = effectiveParameters.getProperty((String) key);
+                if (defaultValue == null  && currentValue != null || (defaultValue != null && ! defaultValue.equals(currentValue))) {
+                    changedParameters.put(key, currentValue == null ? "" : currentValue);
+                }
+            }
+            changedParameters.remove("db.password");
+            changedParameters.store(fos, "parameters-changed.properties");
+        } catch (IOException e) {
+            log.warn("Failed to export parameters-changed information", e);
         } finally {
             IOUtils.closeQuietly(fos);
         }
 
         writeRuntimeStats(engine, tmpDir);
+        writeJobsStats(engine, tmpDir);
 
         fos = null;
         try {
             fos = new FileOutputStream(new File(tmpDir, "system.properties"));
-            System.getProperties().store(fos, "system.properties");
+            SortedProperties props = new SortedProperties();
+            props.putAll(System.getProperties());
+            props.store(fos, "system.properties");
         } catch (IOException e) {
             log.warn("Failed to export thread information", e);
         } finally {
@@ -246,12 +292,92 @@ public class SnapshotUtil {
 
             }
             
+            RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
+            List<String> arguments = runtimeMxBean.getInputArguments();
+            runtimeProperties.setProperty("jvm.arguments", arguments.toString());
+            
             runtimeProperties.store(fos, "runtime-stats.properties");
         } catch (IOException e) {
-            log.warn("Failed to export thread information", e);
+            log.warn("Failed to export runtime-stats information", e);
         } finally {
             IOUtils.closeQuietly(fos);
         }
     }
+    
+    protected static void writeJobsStats(ISymmetricEngine engine, File tmpDir) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(new File(tmpDir, "jobs.txt"));
+            IJobManager jobManager = engine.getJobManager();
+            IClusterService clusterService = engine.getClusterService();
+            INodeService nodeService = engine.getNodeService();
+            writer.write("Clustering is " + (clusterService.isClusteringEnabled() ? "" : "not ") + 
+                    "enabled and there are " + nodeService.findNodeHosts(nodeService.findIdentityNodeId()).size() +
+                    " instances in the cluster\n\n");
+            writer.write(StringUtils.rightPad("Job Name", 30) + StringUtils.rightPad("Schedule", 20) + 
+                    StringUtils.rightPad("Status", 10) + StringUtils.rightPad("Server Id", 30) +
+                    StringUtils.rightPad("Last Server Id", 30) + StringUtils.rightPad("Last Finish Time", 30) +
+                    StringUtils.rightPad("Last Run Period", 20) + StringUtils.rightPad("Avg. Run Period", 20) + "\n");
+            List<IJob> jobs = jobManager.getJobs();
+            Map<String, Lock> locks = clusterService.findLocks();
+            for (IJob job : jobs) {
+                Lock lock = locks.get(job.getClusterLockName());
+                String status = getJobStatus(job, lock);
+                String runningServerId = lock != null ? lock.getLockingServerId() : "";
+                String lastServerId = clusterService.getServerId();
+                if (lock != null) {
+                    lastServerId = lock.getLastLockingServerId();
+                }
+                String schedule = StringUtils.isBlank(job.getCronExpression()) ? Long.toString(job
+                        .getTimeBetweenRunsInMs()) : job.getCronExpression();
+                String lastFinishTime = getLastFinishTime(job, lock);
+    
+                writer.write(StringUtils.rightPad(job.getClusterLockName().replace("_", " "), 30)+ 
+                        StringUtils.rightPad(schedule, 20) + StringUtils.rightPad(status, 10) + 
+                        StringUtils.rightPad(runningServerId == null ? "" : runningServerId, 30) +
+                        StringUtils.rightPad(lastServerId == null ? "" : lastServerId, 30) + 
+                        StringUtils.rightPad(lastFinishTime == null ? "" : lastFinishTime, 30) + 
+                        StringUtils.rightPad(job.getLastExecutionTimeInMs() + "", 20) + 
+                        StringUtils.rightPad(job.getAverageExecutionTimeInMs() + "", 20) + "\n");
+            }
+        } catch (IOException e) {
+            log.warn("Failed to write jobs information", e);
+        } finally {
+            IOUtils.closeQuietly(writer);
+        }
+    }
+
+    protected static String getJobStatus(IJob job, Lock lock) {
+        String status = "IDLE";
+        if (lock != null) {
+            if (lock.isStopped()) {
+                status = "STOPPED";
+            } else if (lock.getLockTime() != null) {
+                status = "RUNNING";
+            }
+        } else {
+            status = job.isRunning() ? "RUNNING" : job.isPaused() ? "PAUSED" : job
+                    .isStarted() ? "IDLE" : "STOPPED";
+        }
+        return status;
+    }
+
+    protected static String getLastFinishTime(IJob job, Lock lock) {
+        if (lock != null && lock.getLastLockTime() != null) {
+            return DateFormatUtils.ISO_DATETIME_TIME_ZONE_FORMAT.format(lock.getLastLockTime());
+        } else {
+            return job.getLastFinishTime() == null ? null : DateFormatUtils.ISO_DATETIME_TIME_ZONE_FORMAT.format(job
+                    .getLastFinishTime());
+        }
+    }
+
+    static class SortedProperties extends Properties {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public synchronized Enumeration<Object> keys() {
+            return Collections.enumeration(new TreeSet<Object>(super.keySet()));
+        }
+    };
 
 }
