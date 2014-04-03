bde63a5589408dd4fe70a1ef37d8494b7ea04bf0
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-04-03 20:55:50 +0000

    0001659: Manage->Nodes screen shows offline - fix group by in sql

--- trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/NodeServiceSqlMap.java
+++ trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/NodeServiceSqlMap.java
@@ -172,8 +172,7 @@ public class NodeServiceSqlMap extends AbstractSqlMap {
         putSql("findNodeHeartbeatsSql",
                 "select h.node_id, h.heartbeat_time, h.timezone_offset from $(node_host) h inner join $(node) n on h.node_id=n.node_id"
               + " where n.sync_enabled = 1 and n.node_id != ? and n.created_at_node_id = ?"
-              + " and h.heartbeat_time = (select max(hh.heartbeat_time) from $(node_host) hh where hh.node_id = h.node_id)"
-              + " group by h.node_id, h.timezone_offset");
+              + " and h.heartbeat_time = (select max(hh.heartbeat_time) from $(node_host) hh where hh.node_id = h.node_id)");
 
     }
 
