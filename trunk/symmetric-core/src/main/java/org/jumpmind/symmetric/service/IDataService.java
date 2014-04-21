23b5c4b6dc76ca316d22bb5f31bcfa0612dee330
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-04-21 16:21:13 +0000

    0001626: Create event should not store XML is row data

--- trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/service/IDataService.java
+++ trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/service/IDataService.java
@@ -100,7 +100,7 @@ public interface IDataService {
 
     public void insertSqlEvent(Node targetNode, String sql, boolean isLoad, long loadId, String createBy);
 
-    public void insertCreateEvent(Node targetNode, TriggerHistory triggerHistory, String xml, boolean isLoad, long loadId, String createBy);
+    public void insertCreateEvent(Node targetNode, TriggerHistory triggerHistory, String routerId, boolean isLoad, long loadId, String createBy);
     
     /**
      * Count the number of data ids in a range
