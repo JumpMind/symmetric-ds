e39e21c65801992826a531fc6acb49f5ef2c92d0
James <jhicks@users.noreply.github.com>
2014-04-29 20:45:01 +0000

    Test fix for default node id creator to match default external Id behavior change

--- symmetric-core/src/test/java/org/jumpmind/symmetric/util/DefaultNodeIdCreatorTest.java
+++ symmetric-core/src/test/java/org/jumpmind/symmetric/util/DefaultNodeIdCreatorTest.java
@@ -20,14 +20,19 @@
  */
 package org.jumpmind.symmetric.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
 
 import org.jumpmind.security.SecurityServiceFactory;
+import org.jumpmind.symmetric.common.ParameterConstants;
 import org.jumpmind.symmetric.model.Node;
 import org.jumpmind.symmetric.model.NodeSecurity;
+import org.jumpmind.symmetric.service.IParameterService;
 import org.jumpmind.symmetric.service.impl.MockNodeService;
 import org.jumpmind.symmetric.service.impl.MockParameterService;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 
 public class DefaultNodeIdCreatorTest {
@@ -77,10 +82,37 @@ public class DefaultNodeIdCreatorTest {
 
     @Test
     public void testGenerateNodeIdExisting() throws Exception {
-        final String EXPECTED_NODE_ID = "100-0";
+        final String EXPECTED_NODE_ID = "100";
         DefaultNodeIdCreator generator = new DefaultNodeIdCreator(new MockParameterService(), new MockNodeService() {
             @Override
             public Node findNode(String nodeId) {
+                if (nodeId.equals("100")) {
+                    Node node = new Node();
+                    node.setNodeId("100");
+                    return node;
+                } else {
+                    return null;
+                }
+            }
+        }, SecurityServiceFactory.create());
+        Node node = new Node();
+        node.setExternalId("100");
+        String selectedNodeId = generator.generateNodeId(node, null, null);
+        
+        assertEquals(EXPECTED_NODE_ID, selectedNodeId);
+    }
+    
+    @Test
+    public void testGenerateNodeIdExistingWithUniqueParam() throws Exception {
+        final String EXPECTED_NODE_ID = "100-0";
+        IParameterService paramService = mock(IParameterService.class);
+        Mockito.when(paramService.is(ParameterConstants.EXTERNAL_ID_IS_UNIQUE)).thenReturn(true);
+        Mockito.when(paramService.getInt(ParameterConstants.NODE_ID_CREATOR_MAX_NODES, 100)).thenReturn(100);
+        
+        DefaultNodeIdCreator generator = new DefaultNodeIdCreator(paramService, new MockNodeService() {
+            @Override
+            public Node findNode(String nodeId) {
+
                 if (nodeId.equals("100")) {
                     Node node = new Node();
                     node.setNodeId("100");
@@ -98,7 +130,11 @@ public class DefaultNodeIdCreatorTest {
 
     @Test
     public void testGenerateNodeIdExistingAll() throws Exception {
-        DefaultNodeIdCreator generator = new DefaultNodeIdCreator(new MockParameterService(), new MockNodeService() {
+        IParameterService paramService = mock(IParameterService.class);
+        Mockito.when(paramService.is(ParameterConstants.EXTERNAL_ID_IS_UNIQUE)).thenReturn(true);
+        Mockito.when(paramService.getInt(ParameterConstants.NODE_ID_CREATOR_MAX_NODES, 100)).thenReturn(100);
+        
+        DefaultNodeIdCreator generator = new DefaultNodeIdCreator(paramService, new MockNodeService() {
             @Override
             public Node findNode(String nodeId) {
                 Node node = new Node();
