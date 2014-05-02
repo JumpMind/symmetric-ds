f1ffc922dee975df0dc9e8cb9f1a4f2ae83565d5
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-05-02 12:41:41 +0000

    0001708: Disable page level locking and enable row level locking on sym_data and sym_data_event

--- trunk/symmetric-client/src/main/java/org/jumpmind/symmetric/db/mssql/MsSqlSymmetricDialect.java
+++ trunk/symmetric-client/src/main/java/org/jumpmind/symmetric/db/mssql/MsSqlSymmetricDialect.java
@@ -65,6 +65,22 @@ public class MsSqlSymmetricDialect extends AbstractSymmetricDialect implements I
     }
     
     @Override
+    public boolean createOrAlterTablesIfNecessary(String... tableNames) {
+        boolean altered = super.createOrAlterTablesIfNecessary(tableNames);
+        ISqlTemplate sqlTemplate = platform.getSqlTemplate();        
+        String tablePrefix = getTablePrefix();
+        if (sqlTemplate
+                .queryForInt("select count(*) from sys.indexes i inner join sys.tables t on t.object_id=i.object_id where t.name in ('"+tablePrefix.toLowerCase()+"_data', '"+tablePrefix.toLowerCase()+"_data_event') and (i.allow_row_locks !='true' or t.lock_escalation != 1)") > 0) {
+            log.info("Updating indexes to prevent lock escalation");
+            sqlTemplate.update("ALTER INDEX ALL ON "+tablePrefix.toUpperCase()+"_DATA SET (ALLOW_ROW_LOCKS = ON)");
+            sqlTemplate.update("ALTER INDEX ALL ON "+tablePrefix.toUpperCase()+"_DATA_EVENT SET (ALLOW_ROW_LOCKS = ON)");
+            sqlTemplate.update("ALTER TABLE "+tablePrefix.toUpperCase()+"_DATA SET (LOCK_ESCALATION = DISABLE)");
+            sqlTemplate.update("ALTER TABLE "+tablePrefix.toUpperCase()+"_DATA_EVENT SET (LOCK_ESCALATION = DISABLE)");
+        }
+        return altered;
+    }
+    
+    @Override
     public void verifyDatabaseIsCompatible() {
         super.verifyDatabaseIsCompatible();
         ISqlTemplate template = getPlatform().getSqlTemplate();
