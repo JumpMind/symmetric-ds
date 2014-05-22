70ea5fdf2393496bcd30aec446da9d9e72072d76
erilong <erilong@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-05-22 14:17:06 +0000

    0001728: MSSQL Bulk Loader support for columns in different order

--- trunk/symmetric-client/src/test/java/org/jumpmind/symmetric/io/data/writer/MsSqlBulkDatabaseWriterTest.java
+++ trunk/symmetric-client/src/test/java/org/jumpmind/symmetric/io/data/writer/MsSqlBulkDatabaseWriterTest.java
@@ -1,8 +1,11 @@
 package org.jumpmind.symmetric.io.data.writer;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.jumpmind.db.DbTestUtils;
+import org.jumpmind.db.model.Column;
 import org.jumpmind.db.model.Table;
 import org.jumpmind.db.platform.IDatabasePlatform;
 import org.jumpmind.db.platform.mssql.MsSql2000DatabasePlatform;
@@ -10,10 +13,12 @@ import org.jumpmind.db.platform.mssql.MsSql2005DatabasePlatform;
 import org.jumpmind.db.platform.mssql.MsSql2008DatabasePlatform;
 import org.jumpmind.db.util.BasicDataSourcePropertyConstants;
 import org.jumpmind.symmetric.io.data.CsvData;
+import org.jumpmind.symmetric.io.data.DataEventType;
 import org.jumpmind.symmetric.io.stage.IStagingManager;
 import org.jumpmind.symmetric.io.stage.StagingManager;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.springframework.jdbc.support.nativejdbc.CommonsDbcpNativeJdbcExtractor;
 
 public class MsSqlBulkDatabaseWriterTest extends AbstractBulkDatabaseWriterTest {
@@ -45,6 +50,27 @@ public class MsSqlBulkDatabaseWriterTest extends AbstractBulkDatabaseWriterTest 
     protected long writeData(List<CsvData> data) {
         Table table = platform.getTableFromCache(getTestTable(), false);
         return writeData(new MsSqlBulkDatabaseWriter(platform, stagingManager, new CommonsDbcpNativeJdbcExtractor(), 1000, false, null), new TableCsvData(table, data));
+    }
+
+    @Test
+    public void testInsertReorderColumns() throws Exception {
+        if (shouldTestRun(platform)) {
+            String id = getNextId();
+            String[] values = { "string with space in it", "string-with-no-space", "string with space in it",
+                    "string-with-no-space", "2007-01-02 00:00:00.000", "2007-02-03 04:05:06.000", "0", "47", "67.89", "-0.0747663",
+                    encode("string with space in it"), id };
+            List<CsvData> data = new ArrayList<CsvData>();
+            data.add(new CsvData(DataEventType.INSERT, (String[]) ArrayUtils.clone(values)));
+            Table table = (Table) platform.getTableFromCache(getTestTable(), false).clone();
+            Column firstColumn = table.getColumn(0);
+            table.removeColumn(firstColumn);
+            table.addColumn(firstColumn);
+            writeData(new MsSqlBulkDatabaseWriter(platform, stagingManager, new CommonsDbcpNativeJdbcExtractor(), 1000, false, null), 
+                    new TableCsvData(table, data));
+            values = (String[]) ArrayUtils.remove(values, values.length - 1);
+            values = (String[]) ArrayUtils.add(values, 0, id);
+            assertTestTableEquals(id, values);
+        }
     }
 
 }
