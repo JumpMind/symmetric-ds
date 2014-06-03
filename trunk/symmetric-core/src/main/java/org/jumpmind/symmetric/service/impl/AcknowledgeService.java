25b54e6ce87ded9891f59d1eb83ef2a799f76ce3
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-06-03 16:45:47 +0000

    Fixed error message.

--- trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/AcknowledgeService.java
+++ trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/AcknowledgeService.java
@@ -121,8 +121,8 @@ public class AcknowledgeService extends AbstractService implements IAcknowledgeS
 
                 if (status == Status.ER) {
                     log.error(
-                            "The outgoing batch {} failed. {}",
-                            outgoingBatch.getNodeBatchId(), batch.getSqlMessage() != null ? batch.getSqlMessage() : "");
+                            "The outgoing batch {} failed {}",
+                            outgoingBatch.getNodeBatchId(), batch.getSqlMessage() != null ? ". " + batch.getSqlMessage() : "");
                     RouterStats routerStats = engine.getStatisticManager().getRouterStatsByBatch(batch.getBatchId());
                     if (routerStats != null) {
                         log.info("Router stats for batch " + outgoingBatch.getBatchId() + ": " + routerStats.toString());
