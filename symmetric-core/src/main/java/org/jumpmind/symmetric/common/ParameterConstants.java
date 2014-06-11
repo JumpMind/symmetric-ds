27dac6ed8f77494a4db3d4bd208defb20e9e49c6
chenson42 <chenson42@gmail.com>
2014-06-11 22:22:24 +0000

    0001764: Add parameter to disable sync triggers at server startup

--- symmetric-core/src/main/java/org/jumpmind/symmetric/common/ParameterConstants.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/common/ParameterConstants.java
@@ -92,6 +92,7 @@ final public class ParameterConstants {
     public final static String AUTO_SYNC_CONFIGURATION_ON_INCOMING = "auto.sync.configuration.on.incoming";
     public final static String AUTO_CONFIGURE_DATABASE = "auto.config.database";
     public final static String AUTO_SYNC_TRIGGERS = "auto.sync.triggers";
+    public final static String AUTO_SYNC_TRIGGERS_AT_STARTUP = "auto.sync.triggers.at.startup";
     public final static String AUTO_SYNC_TRIGGERS_AFTER_CONFIG_CHANGED = "auto.sync.triggers.after.config.change";
     public final static String AUTO_REFRESH_AFTER_CONFIG_CHANGED = "auto.refresh.after.config.changes.detected";
     public final static String AUTO_CONFIGURE_REG_SVR_SQL_SCRIPT = "auto.config.registration.svr.sql.script";
