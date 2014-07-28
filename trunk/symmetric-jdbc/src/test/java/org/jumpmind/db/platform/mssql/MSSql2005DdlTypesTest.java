9d290d51d5da66d09aae657f4d83fe5ddee48012
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-07-28 16:03:55 +0000

    don't test a sql type that is not supported

--- trunk/symmetric-jdbc/src/test/java/org/jumpmind/db/platform/mssql/MSSql2005DdlTypesTest.java
+++ trunk/symmetric-jdbc/src/test/java/org/jumpmind/db/platform/mssql/MSSql2005DdlTypesTest.java
@@ -39,7 +39,7 @@ public class MSSql2005DdlTypesTest extends AbstractDdlTypesTest {
                 "char", "varchar", "text", 
                 "nchar", "nvarchar", "ntext", 
                 "binary", "varbinary", "image",
-                "timestamp", "uniqueidentifier", "sql_variant", "xml"
+                "timestamp", "uniqueidentifier", "xml"
         };
     }
 
