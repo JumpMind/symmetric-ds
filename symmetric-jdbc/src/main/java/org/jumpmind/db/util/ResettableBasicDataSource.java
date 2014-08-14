3144199531227f9b1989515f011335f92433843b
chenson42 <chenson42@gmail.com>
2014-08-14 12:20:09 +0000

    0001910: Too many connections when network environment is bad. Bug in DBCP.

--- symmetric-jdbc/src/main/java/org/jumpmind/db/util/ResettableBasicDataSource.java
+++ symmetric-jdbc/src/main/java/org/jumpmind/db/util/ResettableBasicDataSource.java
@@ -22,13 +22,19 @@ package org.jumpmind.db.util;
 
 import java.sql.SQLException;
 
+import org.apache.commons.dbcp.AbandonedConfig;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.ConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.SQLNestedException;
+import org.apache.commons.pool.KeyedObjectPoolFactory;
 
 /**
  * A subclass of {@link BasicDataSource} which allows for a data source to be
  * closed (all underlying connections are closed) and then allows new
  * connections to be created.
  */
+@SuppressWarnings("deprecation")
 public class ResettableBasicDataSource extends BasicDataSource {
 
     public ResettableBasicDataSource() {
@@ -47,5 +53,33 @@ public class ResettableBasicDataSource extends BasicDataSource {
         }
 
     }
+    
+    @Override
+    protected void createPoolableConnectionFactory(ConnectionFactory driverConnectionFactory,
+            KeyedObjectPoolFactory statementPoolFactory, AbandonedConfig configuration) throws SQLException {
+        PoolableConnectionFactory connectionFactory = null;
+        try {
+            connectionFactory =
+                new PoolableConnectionFactory(driverConnectionFactory,
+                                              connectionPool,
+                                              statementPoolFactory,
+                                              validationQuery,
+                                              validationQueryTimeout,
+                                              connectionInitSqls,
+                                              defaultReadOnly,
+                                              defaultAutoCommit,
+                                              defaultTransactionIsolation,
+                                              defaultCatalog,
+                                              configuration);
+            validateConnectionFactory(connectionFactory);
+        } catch (Exception e) {
+            try {
+                connectionPool.close();
+            } catch (Exception e1) {
+            }
+            throw new SQLNestedException("Cannot create PoolableConnectionFactory (" + e.getMessage() + ")", e);
+        }
+    }
+
 
 }
