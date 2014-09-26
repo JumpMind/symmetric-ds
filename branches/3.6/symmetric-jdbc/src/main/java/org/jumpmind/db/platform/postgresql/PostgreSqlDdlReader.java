b84f5e0f22289fb1e23cf95c072521b27cc5f01c
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-09-26 11:44:28 +0000

    0001993: Add support for composite data types in postgres

--- branches/3.6/symmetric-jdbc/src/main/java/org/jumpmind/db/platform/postgresql/PostgreSqlDdlReader.java
+++ branches/3.6/symmetric-jdbc/src/main/java/org/jumpmind/db/platform/postgresql/PostgreSqlDdlReader.java
@@ -105,6 +105,7 @@ public class PostgreSqlDdlReader extends AbstractJdbcDdlReader {
     @Override
     protected Integer mapUnknownJdbcTypeForColumn(Map<String, Object> values) {
         String typeName = (String) values.get("TYPE_NAME");
+        Integer type = (Integer) values.get("DATA_TYPE");
         if (typeName != null && typeName.equalsIgnoreCase("ABSTIME")) {
             return Types.TIMESTAMP;
         } else if (typeName != null && typeName.equalsIgnoreCase("TIMESTAMPTZ")) {
@@ -112,6 +113,8 @@ public class PostgreSqlDdlReader extends AbstractJdbcDdlReader {
             return MAPPED_TIMESTAMPTZ;            
         } else if (PostgreSqlDatabasePlatform.isBlobStoredByReference(typeName)) {
             return Types.BLOB;
+        } else if (type != null && type == Types.STRUCT) {
+            return Types.LONGVARCHAR;
         } else {
             return super.mapUnknownJdbcTypeForColumn(values);
         }
