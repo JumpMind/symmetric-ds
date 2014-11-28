1d0d7da03b778a349b81acaf2cf3b9c409a774b9
erilong <erilong@users.noreply.github.com>
2014-11-28 18:43:25 +0000

    0001310: Support sql dialect 1 for firebird

--- symmetric-db/src/main/java/org/jumpmind/db/platform/DatabaseNamesConstants.java
+++ symmetric-db/src/main/java/org/jumpmind/db/platform/DatabaseNamesConstants.java
@@ -32,6 +32,7 @@ final public class DatabaseNamesConstants {
     public final static String GREENPLUM = "greenplum";
     public final static String INFORMIX = "informix";
     public final static String FIREBIRD = "firebird";
+    public final static String FIREBIRD_DIALECT1 = "firebird_dialect1";
     public final static String SQLITE = "sqlite";
     public final static String INTERBASE = "interbase";
     public final static String MSSQL2000 = "mssql2000";
--- symmetric-jdbc/src/main/java/org/jumpmind/db/platform/JdbcDatabasePlatformFactory.java
+++ symmetric-jdbc/src/main/java/org/jumpmind/db/platform/JdbcDatabasePlatformFactory.java
@@ -31,12 +31,14 @@ import java.util.Map;
 import javax.sql.DataSource;
 
 import org.apache.commons.lang.StringUtils;
+import org.h2.util.JdbcUtils;
 import org.jumpmind.db.platform.ase.AseDatabasePlatform;
 import org.jumpmind.db.platform.db2.Db2As400DatabasePlatform;
 import org.jumpmind.db.platform.db2.Db2DatabasePlatform;
 import org.jumpmind.db.platform.db2.Db2zOsDatabasePlatform;
 import org.jumpmind.db.platform.derby.DerbyDatabasePlatform;
 import org.jumpmind.db.platform.firebird.FirebirdDatabasePlatform;
+import org.jumpmind.db.platform.firebird.FirebirdDialect1DatabasePlatform;
 import org.jumpmind.db.platform.greenplum.GreenplumPlatform;
 import org.jumpmind.db.platform.h2.H2DatabasePlatform;
 import org.jumpmind.db.platform.hsqldb.HsqlDbDatabasePlatform;
@@ -84,6 +86,7 @@ public class JdbcDatabasePlatformFactory {
         addPlatform(platforms, "Informix Dynamic Server", InformixDatabasePlatform.class);
         addPlatform(platforms, "Apache Derby", DerbyDatabasePlatform.class);
         addPlatform(platforms, "Firebird", FirebirdDatabasePlatform.class);
+        addPlatform(platforms, DatabaseNamesConstants.FIREBIRD_DIALECT1, FirebirdDialect1DatabasePlatform.class);
         addPlatform(platforms, DatabaseNamesConstants.GREENPLUM, GreenplumPlatform.class);
         addPlatform(platforms, "HsqlDb", HsqlDbDatabasePlatform.class);
         addPlatform(platforms, "HSQL Database Engine2", HsqlDb2DatabasePlatform.class);
@@ -152,6 +155,7 @@ public class JdbcDatabasePlatformFactory {
         try {
             Constructor<? extends IDatabasePlatform> construtor = clazz.getConstructor(DataSource.class, SqlTemplateSettings.class);
             IDatabasePlatform platform = construtor.newInstance(dataSource, settings);
+            log.info("The IDatabasePlatform being used is " + platform.getClass().getCanonicalName());
             platform.getDdlBuilder().setDelimitedIdentifierModeOn(delimitedIdentifierMode);
             return platform;
         } catch (Exception e) {
@@ -237,6 +241,11 @@ public class JdbcDatabasePlatformFactory {
             	nameVersion[0] = DatabaseNamesConstants.DB2AS400;
             }
 
+            if (nameVersion[0].toLowerCase().startsWith(DatabaseNamesConstants.FIREBIRD)) {
+            	if (isFirebirdDialect1(connection)) {
+            		nameVersion[0] = DatabaseNamesConstants.FIREBIRD_DIALECT1;
+            	}
+            }
             log.info("Detected database '" + nameVersion[0] + "', version '" + nameVersion[1] + "', protocol '" + nameVersion[2] + "'");
 
             return nameVersion;
@@ -362,6 +371,30 @@ public class JdbcDatabasePlatformFactory {
             }
         }
         return productVersion;
+    }
+
+    private static boolean isFirebirdDialect1(Connection connection) {
+        boolean isDialect1 = false;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery("select current_time from rdb$database");
+            rs.next();
+        } catch (SQLException ex) {
+            isDialect1 = true;
+            try {
+                JdbcUtils.closeSilently(rs);
+                rs = stmt.executeQuery("select cast(1 as numeric(10,0)) from rdb$database");
+                rs.next();
+            } catch (SQLException e) {
+                log.error("The client sql dialect does not match the database, which is not a supported mode.  You must add ?sql_dialect=1 to the end of the JDBC URL.");
+            }
+        } finally {
+            JdbcUtils.closeSilently(rs);
+            JdbcUtils.closeSilently(stmt);
+        }        
+        return isDialect1;
     }
 
     public static String getDatabaseProductVersion(DataSource dataSource) {
