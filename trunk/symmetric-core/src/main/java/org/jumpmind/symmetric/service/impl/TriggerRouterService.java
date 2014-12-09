e2e910b39f9217be7daae3775d46c6ce61ebadc9
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2014-12-09 12:47:46 +0000

    0002093: Table structure changes, sym_trigger is updated and old table structure is used for new trigger

--- trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TriggerRouterService.java
+++ trunk/symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TriggerRouterService.java
@@ -1110,7 +1110,7 @@ public class TriggerRouterService extends AbstractService implements ITriggerRou
                         List<TriggerHistory> activeTriggerHistories = getActiveTriggerHistories();
                         inactivateTriggers(triggersForCurrentNode, sqlBuffer, activeTriggerHistories);
                         updateOrCreateDatabaseTriggers(triggersForCurrentNode, sqlBuffer, force,
-                                true, activeTriggerHistories);
+                                true, activeTriggerHistories, true);
                         resetTriggerRouterCacheByNodeGroupId();
                     } finally {
                         clusterService.unlock(ClusterConstants.SYNCTRIGGERS);
@@ -1160,7 +1160,7 @@ public class TriggerRouterService extends AbstractService implements ITriggerRou
             Set<Table> tables = tablesByTriggerId.get(history.getTriggerId());
             Trigger trigger = getTriggerFromList(history.getTriggerId(), triggersThatShouldBeActive);
             if (tables == null && trigger != null) {
-                tables = getTablesForTrigger(trigger, triggersThatShouldBeActive);
+                tables = getTablesForTrigger(trigger, triggersThatShouldBeActive, false);
                 tablesByTriggerId.put(trigger.getTriggerId(), tables);
             }
 
@@ -1269,7 +1269,7 @@ public class TriggerRouterService extends AbstractService implements ITriggerRou
                 .select();
     }
 
-    protected Set<Table> getTablesForTrigger(Trigger trigger, List<Trigger> triggers) {
+    protected Set<Table> getTablesForTrigger(Trigger trigger, List<Trigger> triggers, boolean useTableCache) {
         Set<Table> tables = new HashSet<Table>();
         try {
             boolean ignoreCase = this.parameterService
@@ -1333,7 +1333,7 @@ public class TriggerRouterService extends AbstractService implements ITriggerRou
                     } else {
                         Table table = symmetricDialect.getPlatform().getTableFromCache(
                                 catalogName, schemaName,
-                                trigger.getSourceTableName(), false);
+                                trigger.getSourceTableName(), !useTableCache);
                         if (table != null) {
                             tables.add(table);
                         }
@@ -1385,15 +1385,15 @@ public class TriggerRouterService extends AbstractService implements ITriggerRou
     }
 
     protected void updateOrCreateDatabaseTriggers(List<Trigger> triggers, StringBuilder sqlBuffer,
-            boolean force, boolean verifyInDatabase, List<TriggerHistory> activeTriggerHistories) {
+            boolean force, boolean verifyInDatabase, List<TriggerHistory> activeTriggerHistories, boolean useTableCache) {
         for (Trigger trigger : triggers) {
-            updateOrCreateDatabaseTrigger(trigger, triggers, sqlBuffer, force, verifyInDatabase, activeTriggerHistories);
+            updateOrCreateDatabaseTrigger(trigger, triggers, sqlBuffer, force, verifyInDatabase, activeTriggerHistories, useTableCache);
         }
     }
 
     protected void updateOrCreateDatabaseTrigger(Trigger trigger, List<Trigger> triggers,
-            StringBuilder sqlBuffer, boolean force, boolean verifyInDatabase, List<TriggerHistory> activeTriggerHistories) {
-        Set<Table> tables = getTablesForTrigger(trigger, triggers);
+            StringBuilder sqlBuffer, boolean force, boolean verifyInDatabase, List<TriggerHistory> activeTriggerHistories, boolean useTableCache) {
+        Set<Table> tables = getTablesForTrigger(trigger, triggers, useTableCache);
 
         if (tables != null && tables.size() > 0) {
             for (Table table : tables) {
@@ -1439,7 +1439,7 @@ public class TriggerRouterService extends AbstractService implements ITriggerRou
                     }
                 }
                 updateOrCreateDatabaseTrigger(trigger, triggersForCurrentNode, sqlBuffer,
-                    force, verifyInDatabase, allHistories);
+                    force, verifyInDatabase, allHistories, false);
             } else {                
                 for (TriggerHistory triggerHistory : getActiveTriggerHistories(trigger)) {
                     dropTriggers(triggerHistory, sqlBuffer);
