66699b539bfeccb1ae81b84b054bafa3d0b084fb
erilong <erilong@users.noreply.github.com>
2014-12-12 19:34:25 +0000

    0002078: Service wrapper unsatisfied link error

--- symmetric-wrapper/src/main/java/org/jumpmind/symmetric/wrapper/WrapperService.java
+++ symmetric-wrapper/src/main/java/org/jumpmind/symmetric/wrapper/WrapperService.java
@@ -285,6 +285,7 @@ public abstract class WrapperService {
         ArrayList<String> cmd = new ArrayList<String>();
         String quote = getWrapperCommandQuote();
         cmd.add(quote + config.getJavaCommand() + quote);
+        cmd.addAll(config.getOptions());
         cmd.add("-jar");
         cmd.add(quote + config.getWrapperJarPath() + quote);
         cmd.add(arg);
