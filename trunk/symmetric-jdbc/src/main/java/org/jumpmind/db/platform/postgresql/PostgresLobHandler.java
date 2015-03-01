c40cc3b211a50cfa676b084fa059142473100abb
chenson42 <chenson42@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2015-03-01 21:28:28 +0000

    0002216: Replication of rows with NULL in large object column on PostgreSQL throws exception

--- trunk/symmetric-jdbc/src/main/java/org/jumpmind/db/platform/postgresql/PostgresLobHandler.java
+++ trunk/symmetric-jdbc/src/main/java/org/jumpmind/db/platform/postgresql/PostgresLobHandler.java
@@ -20,33 +20,32 @@
  */
 package org.jumpmind.db.platform.postgresql;
 
+import java.sql.Blob;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.jumpmind.db.sql.SymmetricLobHandler;
-import org.springframework.jdbc.support.lob.DefaultLobHandler;
-import org.springframework.jdbc.support.lob.LobHandler;
 
 public class PostgresLobHandler extends SymmetricLobHandler {
 
-    private LobHandler wrappedLobHandler = null;
-
     public PostgresLobHandler() {
         super();
-        DefaultLobHandler wrappedLobHandler = new DefaultLobHandler();
-        wrappedLobHandler.setWrapAsLob(true);
-        this.wrappedLobHandler = wrappedLobHandler;
     }
 
     public boolean needsAutoCommitFalseForBlob(int jdbcTypeCode, String jdbcTypeName) {
         return PostgreSqlDatabasePlatform.isBlobStoredByReference(jdbcTypeName);
     }
 
-    public byte[] getBlobAsBytes(ResultSet rs, int columnIndex, int jdbcTypeCode, String jdbcTypeName)
-            throws SQLException {
+    public byte[] getBlobAsBytes(ResultSet rs, int columnIndex, int jdbcTypeCode,
+            String jdbcTypeName) throws SQLException {
 
         if (PostgreSqlDatabasePlatform.isBlobStoredByReference(jdbcTypeName)) {
-            return wrappedLobHandler.getBlobAsBytes(rs, columnIndex);
+            Blob blob = rs.getBlob(columnIndex);
+            if (blob != null) {
+                return blob.getBytes(1, (int) blob.length());
+            } else {
+                return null;
+            }
         } else {
             return getDefaultHandler().getBlobAsBytes(rs, columnIndex);
         }
