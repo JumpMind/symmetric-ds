5c1cf293e8cc067179f6ed4a6a17f0b93f08c87c
symmetricds <symmetricds@e7b7779c-d383-44e5-97ed-b41cf20c93cf>
2015-03-30 13:27:38 +0000

    Tagged by Jenkins svn-tag plugin. Build:jenkins-SymmetricDS - Release Trunk-18.

---
+++ tags/3.7.11/symmetric-client/src/main/java/org/jumpmind/symmetric/db/mysql/MySqlTriggerTemplate.java
@@ -0,0 +1,124 @@
+/**
+ * Licensed to JumpMind Inc under one or more contributor
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU General Public License, version 3.0 (GPLv3)
+ * (the "License"); you may not use this file except in compliance
+ * with the License.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * version 3.0 (GPLv3) along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jumpmind.symmetric.db.mysql;
+
+import java.util.HashMap;
+
+import org.jumpmind.symmetric.db.AbstractTriggerTemplate;
+import org.jumpmind.symmetric.db.ISymmetricDialect;
+import org.jumpmind.symmetric.util.SymmetricUtils;
+
+public class MySqlTriggerTemplate extends AbstractTriggerTemplate {
+
+    public MySqlTriggerTemplate(ISymmetricDialect symmetricDialect) {
+        super(symmetricDialect);
+        emptyColumnTemplate = "''" ;
+        stringColumnTemplate = "if($(tableAlias).`$(columnName)` is null,'',concat('\"',replace(replace($(tableAlias).`$(columnName)`,'\\\\','\\\\\\\\'),'\"','\\\\\"'),'\"'))\n" ;
+        geometryColumnTemplate = "if($(tableAlias).`$(columnName)` is null,'',concat('\"',replace(replace(astext($(tableAlias).`$(columnName)`),'\\\\','\\\\\\\\'),'\"','\\\\\"'),'\"'))\n" ;
+        numberColumnTemplate = "if($(tableAlias).`$(columnName)` is null,'',concat('\"',cast($(tableAlias).`$(columnName)` as char),'\"'))\n" ;
+        datetimeColumnTemplate = "if($(tableAlias).`$(columnName)` is null,'',concat('\"',cast($(tableAlias).`$(columnName)` as char),'\"'))\n" ;
+        clobColumnTemplate = "if($(tableAlias).`$(columnName)` is null,'',concat('\"',replace(replace($(tableAlias).`$(columnName)`,'\\\\','\\\\\\\\'),'\"','\\\\\"'),'\"'))\n" ;
+        blobColumnTemplate = "if($(tableAlias).`$(columnName)` is null,'',concat('\"',hex($(tableAlias).`$(columnName)`),'\"'))\n" ;
+        booleanColumnTemplate = "if($(tableAlias).`$(columnName)` is null,'',concat('\"',cast($(tableAlias).`$(columnName)` as unsigned),'\"'))\n" ;
+        triggerConcatCharacter = "," ;
+        newTriggerValue = "new" ;
+        oldTriggerValue = "old" ;
+        oldColumnPrefix = "" ;
+        newColumnPrefix = "" ;
+
+        sqlTemplates = new HashMap<String,String>();
+        sqlTemplates.put("insertTriggerTemplate" ,
+"create trigger $(triggerName) after insert on $(schemaName)$(tableName)                                                                                                                                \n" +
+"                                for each row begin                                                                                                                                                     \n" +
+"                                  if $(syncOnInsertCondition) and $(syncOnIncomingBatchCondition) then                                                                                                 \n" +
+"                                    insert into $(defaultCatalog)$(prefixName)_data (table_name, event_type, trigger_hist_id, row_data, channel_id, transaction_id, source_node_id, external_data, create_time)\n" +
+"                                    values(                                                                                                                                                            \n" +
+"                                      '$(targetTableName)',                                                                                                                                            \n" +
+"                                      'I',                                                                                                                                                             \n" +
+"                                      $(triggerHistoryId),                                                                                                                                             \n" +
+"                                      concat($(columns)                                                                                                                                                \n" +
+"                                       ),                                                                                                                                                              \n" +
+"                                      $(channelExpression), $(txIdExpression), @sync_node_disabled,                                                                                                        \n" +
+"                                      $(externalSelect),                                                                                                                                               \n" +
+"                                      CURRENT_TIMESTAMP                                                                                                                                                \n" +
+"                                    );                                                                                                                                                                 \n" +
+"                                  end if;                                                                                                                                                              \n" +
+"                                  $(custom_on_insert_text)                                                                                                                                                \n" +
+"                                end                                                                                                                                                                    " );
+
+        sqlTemplates.put("updateTriggerTemplate" ,
+"create trigger $(triggerName) after update on $(schemaName)$(tableName)                                                                                                                                \n" +
+"                                for each row begin                                                                                                                                                     \n" +
+"                                  DECLARE var_row_data mediumtext character set utf8;                                                                                                                                      \n" +
+"                                  DECLARE var_old_data mediumtext character set utf8;                                                                                                                                     \n" +
+"                                  if $(syncOnUpdateCondition) and $(syncOnIncomingBatchCondition) then                                                                                                 \n" +
+"                                   set var_row_data = concat($(columns));                                                                                                                              \n" +
+"                                   set var_old_data = concat($(oldColumns));                                                                                                                           \n" +
+"                                   if $(dataHasChangedCondition) then                                                                                                                                  \n" +
+"	                                    insert into $(defaultCatalog)$(prefixName)_data (table_name, event_type, trigger_hist_id, pk_data, row_data, old_data, channel_id, transaction_id, source_node_id, external_data, create_time)\n" +
+"	                                    values(                                                                                                                                                           \n" +
+"	                                      '$(targetTableName)',                                                                                                                                           \n" +
+"	                                      'U',                                                                                                                                                            \n" +
+"	                                      $(triggerHistoryId),                                                                                                                                            \n" +
+"	                                      concat($(oldKeys)                                                                                                                                               \n" +
+"	                                       ),                                                                                                                                                             \n" +
+"	                                      var_row_data,                                                                                                                                                   \n" +
+"	                                      var_old_data,                                                                                                                                                   \n" +
+"	                                      $(channelExpression), $(txIdExpression), @sync_node_disabled,                                                                                                       \n" +
+"	                                      $(externalSelect),                                                                                                                                              \n" +
+"	                                      CURRENT_TIMESTAMP                                                                                                                                               \n" +
+"	                                    );                                                                                                                                                                \n" +
+"	                                end if;                                                                                                                                                               \n" +
+"                                  end if;                                                                                                                                                                \n" +
+"                                  $(custom_on_update_text)                                                                                                                                                  \n" +
+"                                end                                                                                                                                                                      " );
+
+        sqlTemplates.put("deleteTriggerTemplate" ,
+"create trigger $(triggerName) after delete on $(schemaName)$(tableName)                                                                                                                                \n" +
+"                                for each row begin                                                                                                                                                     \n" +
+"                                  if $(syncOnDeleteCondition) and $(syncOnIncomingBatchCondition) then                                                                                                 \n" +
+"                                    insert into $(defaultCatalog)$(prefixName)_data (table_name, event_type, trigger_hist_id, pk_data, old_data, channel_id, transaction_id, source_node_id, external_data, create_time)\n" +
+"                                    values(                                                                                                                                                            \n" +
+"                                      '$(targetTableName)',                                                                                                                                            \n" +
+"                                      'D',                                                                                                                                                             \n" +
+"                                      $(triggerHistoryId),                                                                                                                                             \n" +
+"                                      concat($(oldKeys)                                                                                                                                                \n" +
+"                                       ),                                                                                                                                                              \n" +
+"                                       concat($(oldColumns)                                                                                                                                            \n" +
+"                                       ),                                                                                                                                                              \n" +
+"                                      $(channelExpression), $(txIdExpression), @sync_node_disabled,                                                                                                        \n" +
+"                                      $(externalSelect),                                                                                                                                               \n" +
+"                                      CURRENT_TIMESTAMP                                                                                                                                                \n" +
+"                                    );                                                                                                                                                                 \n" +
+"                                  end if;                                                                                                                                                              \n" +
+"                                  $(custom_on_delete_text)                                                                                                                                                \n" +
+"                                end                                                                                                                                                                    " );
+
+        sqlTemplates.put("initialLoadSqlTemplate" ,
+"select concat($(columns)) from $(schemaName)$(tableName) t where $(whereClause)                                                                                                                        " );
+    }
+    
+    @Override
+    protected String castDatetimeColumnToString(String columnName) {
+        return "cast(\n" + SymmetricUtils.quote(symmetricDialect, columnName) + " as char) as \n" + columnName;
+    }
+
+}
---
+++ tags/3.7.11/symmetric-client/src/main/java/org/jumpmind/symmetric/util/TypedPropertiesFactory.java
@@ -0,0 +1,84 @@
+/**
+ * Licensed to JumpMind Inc under one or more contributor
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU General Public License, version 3.0 (GPLv3)
+ * (the "License"); you may not use this file except in compliance
+ * with the License.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * version 3.0 (GPLv3) along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jumpmind.symmetric.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.jumpmind.exception.IoException;
+import org.jumpmind.properties.TypedProperties;
+import org.jumpmind.symmetric.ITypedPropertiesFactory;
+import org.jumpmind.util.AppUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class TypedPropertiesFactory implements ITypedPropertiesFactory {
+
+	protected File propertiesFile;
+	
+	protected Properties properties;
+	
+	public TypedPropertiesFactory() {	
+	}
+	
+	public void init(File propertiesFile, Properties properties) {
+		this.propertiesFile = propertiesFile;
+		this.properties = properties;
+	}
+	
+    public TypedProperties reload() {
+        PropertiesFactoryBean factoryBean = new PropertiesFactoryBean();
+        factoryBean.setIgnoreResourceNotFound(true);
+        factoryBean.setLocalOverride(true);
+        factoryBean.setSingleton(false);
+        factoryBean.setProperties(properties);
+        factoryBean.setLocations(buildLocations(propertiesFile));
+        try {
+            return new TypedProperties(factoryBean.getObject());
+        } catch (IOException e) {
+            throw new IoException(e);
+        }
+    }
+
+    protected Resource[] buildLocations(File propertiesFile) {
+        /*
+         * System properties always override the properties found in
+         * these files. System properties are merged in the parameter
+         * service.
+         */
+        List<Resource> resources = new ArrayList<Resource>();
+        resources.add(new ClassPathResource("/symmetric-default.properties"));
+        resources.add(new ClassPathResource("/symmetric-console-default.properties"));
+        resources.add(new FileSystemResource(AppUtils.getSymHome() + "/conf/symmetric.properties"));
+        resources.add(new ClassPathResource("/symmetric.properties"));
+        resources.add(new ClassPathResource("/symmetric-console-default.properties"));
+        resources.add(new ClassPathResource("/symmetric-override.properties"));
+        if (propertiesFile != null && propertiesFile.exists()) {
+            resources.add(new FileSystemResource(propertiesFile.getAbsolutePath()));
+        }
+        return resources.toArray(new Resource[resources.size()]);
+    }
+
+}
