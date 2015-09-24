cc653dcf1228aa55a2c4de0669ef3af71f555751
Chris Henson <chenson42@gmail.com>
2015-09-24 13:33:22 -0400

    0002399: Oracle bulk loader should respect dataloader.max.rows.before.commit and execute database writer filters

--- symmetric-client/src/main/java/org/jumpmind/symmetric/io/OracleBulkDatabaseWriter.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/io/OracleBulkDatabaseWriter.java
@@ -89,23 +89,26 @@ public class OracleBulkDatabaseWriter extends DefaultDatabaseWriter {
             case INSERT:
                 statistics.get(batch).increment(DataWriterStatisticConstants.STATEMENTCOUNT);
                 statistics.get(batch).increment(DataWriterStatisticConstants.LINENUMBER);
-                Object[] rowData = platform.getObjectValues(batch.getBinaryEncoding(),
-                        getRowData(data, CsvData.ROW_DATA), targetTable.getColumns());
-                for (int i = 0; i < rowData.length; i++) {
+                if (filterBefore(data)) {
+                    Object[] rowData = platform.getObjectValues(batch.getBinaryEncoding(), getRowData(data, CsvData.ROW_DATA),
+                            targetTable.getColumns());
+                    for (int i = 0; i < rowData.length; i++) {
 
-                    List<Object> columnList = null;
-                    if (rowArrays.size() > i) {
-                        columnList = rowArrays.get(i);
-                    } else {
-                        columnList = new ArrayList<Object>();
-                        rowArrays.add(columnList);
-                    }
-                    columnList.add(rowData[i]);
+                        List<Object> columnList = null;
+                        if (rowArrays.size() > i) {
+                            columnList = rowArrays.get(i);
+                        } else {
+                            columnList = new ArrayList<Object>();
+                            rowArrays.add(columnList);
+                        }
+                        columnList.add(rowData[i]);
 
-                    if (columnList.size() >= maxRowsBeforeFlush) {
-                        requiresFlush = true;
+                        if (columnList.size() >= maxRowsBeforeFlush) {
+                            requiresFlush = true;
+                        }
                     }
-                }                
+                    uncommittedCount++;
+                }
                 break;
             case UPDATE:
                 super.write(data);
@@ -121,6 +124,8 @@ public class OracleBulkDatabaseWriter extends DefaultDatabaseWriter {
         if (requiresFlush) {
             flush();
         }
+        
+        checkForEarlyCommit();
     }
 
     protected void flush() {
