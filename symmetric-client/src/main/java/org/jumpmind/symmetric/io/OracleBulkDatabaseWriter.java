aabcafe6135e8f6b4d9452da07d24b17f91d88c7
Chris Henson <chenson42@gmail.com>
2015-09-25 10:46:26 -0400

    0002399: Oracle bulk loader should respect dataloader.max.rows.before.commit and execute database writer filters

--- symmetric-client/src/main/java/org/jumpmind/symmetric/io/OracleBulkDatabaseWriter.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/io/OracleBulkDatabaseWriter.java
@@ -42,6 +42,7 @@ import org.jumpmind.db.sql.JdbcSqlTransaction;
 import org.jumpmind.symmetric.io.data.CsvData;
 import org.jumpmind.symmetric.io.data.DataEventType;
 import org.jumpmind.symmetric.io.data.writer.DataWriterStatisticConstants;
+import org.jumpmind.symmetric.io.data.writer.DatabaseWriterSettings;
 import org.jumpmind.symmetric.io.data.writer.DefaultDatabaseWriter;
 import org.springframework.jdbc.support.nativejdbc.NativeJdbcExtractor;
 
@@ -58,8 +59,8 @@ public class OracleBulkDatabaseWriter extends DefaultDatabaseWriter {
     protected List<List<Object>> rowArrays = new ArrayList<List<Object>>();
 
     public OracleBulkDatabaseWriter(IDatabasePlatform platform, String procedurePrefix,
-            NativeJdbcExtractor jdbcExtractor, int maxRowsBeforeFlush) {
-        super(platform);
+            NativeJdbcExtractor jdbcExtractor, int maxRowsBeforeFlush, DatabaseWriterSettings settings) {
+        super(platform, settings);
         this.procedurePrefix = procedurePrefix;
         this.jdbcExtractor = jdbcExtractor;
         this.maxRowsBeforeFlush = maxRowsBeforeFlush;
