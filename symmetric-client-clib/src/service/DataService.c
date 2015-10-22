9d31394c95ed1b344032efb3ba661df2f8718768
elong <elong@jumpmind.com>
2015-10-22 08:29:57 -0400

    heartbeat

--- symmetric-client-clib/src/service/DataService.c
+++ symmetric-client-clib/src/service/DataService.c
@@ -71,6 +71,56 @@ SymList * SymDataService_selectDataFor(SymDataService *this, SymBatch *batch) {
     return list;
 }
 
+void heartbeat(unsigned short force) {
+    /*
+    Node me = engine.getNodeService().findIdentity();
+
+    IParameterService parameterService = engine.getParameterService();
+    if (parameterService.is(ParameterConstants.HEARTBEAT_ENABLED)) {
+        ISymmetricDialect symmetricDialect = engine.getSymmetricDialect();
+        boolean updateWithBatchStatus = parameterService.is(ParameterConstants.HEARTBEAT_UPDATE_NODE_WITH_BATCH_STATUS, false);
+        int outgoingErrorCount = -1;
+        int outgoingUnsentCount = -1;
+        if (updateWithBatchStatus) {
+            outgoingUnsentCount = engine.getOutgoingBatchService().countOutgoingBatchesUnsent();
+            outgoingErrorCount = engine.getOutgoingBatchService().countOutgoingBatchesInError();
+        }
+        if (!parameterService.getExternalId().equals(me.getExternalId())
+                || !parameterService.getNodeGroupId().equals(me.getNodeGroupId())
+                || (parameterService.getSyncUrl() != null && !parameterService.getSyncUrl().equals(me.getSyncUrl()))
+                || !parameterService.getString(ParameterConstants.SCHEMA_VERSION, "").equals(me.getSchemaVersion())
+                || (engine.getDeploymentType() != null && !engine.getDeploymentType().equals(me.getDeploymentType()))
+                || !Version.version().equals(me.getSymmetricVersion())
+                || !symmetricDialect.getName().equals(me.getDatabaseType())
+                || !symmetricDialect.getVersion().equals(me.getDatabaseVersion())
+                || me.getBatchInErrorCount() != outgoingErrorCount
+                || me.getBatchToSendCount() != outgoingUnsentCount) {
+            log.info("Some attribute(s) of node changed.  Recording changes");
+            me.setDeploymentType(engine.getDeploymentType());
+            me.setSymmetricVersion(Version.version());
+            me.setDatabaseType(symmetricDialect.getName());
+            me.setDatabaseVersion(symmetricDialect.getVersion());
+            me.setBatchInErrorCount(outgoingErrorCount);
+            me.setBatchToSendCount(outgoingUnsentCount);
+            me.setSchemaVersion(parameterService.getString(ParameterConstants.SCHEMA_VERSION));
+            if (parameterService.is(ParameterConstants.AUTO_UPDATE_NODE_VALUES)) {
+                log.info("Updating my node configuration info according to the symmetric properties");
+                me.setExternalId(parameterService.getExternalId());
+                me.setNodeGroupId(parameterService.getNodeGroupId());
+                if (!StringUtils.isBlank(parameterService.getSyncUrl())) {
+                    me.setSyncUrl(parameterService.getSyncUrl());
+                }
+            }
+
+            engine.getNodeService().save(me);
+        }
+
+        log.debug("Updating my node info");
+        engine.getNodeService().updateNodeHostForCurrentNode();
+        log.debug("Done updating my node info");
+    */
+}
+
 void SymDataService_destroy(SymDataService *this) {
     free(this);
 }
