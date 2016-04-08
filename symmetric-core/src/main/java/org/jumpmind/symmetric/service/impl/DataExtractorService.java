00db55d3bd163b6c86c6f747d0182589e1f0183b
elong <elong@jumpmind.com>
2016-04-08 11:19:16 -0400

    0002557: Parameter to allow reload channel to block or not block other channels

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/DataExtractorService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/DataExtractorService.java
@@ -372,8 +372,10 @@ public class DataExtractorService extends AbstractService implements IDataExtrac
 
         // Remove non-load batches so that an initial load finishes before
         // any other batches are loaded.
-        if (!batches.containsBatchesInError() && batches.containsLoadBatches()) {
-            batches.removeNonLoadBatches();
+        if (parameterService.is(ParameterConstants.INITIAL_LOAD_BLOCK_CHANNELS, true)) {
+            if (!batches.containsBatchesInError() && batches.containsLoadBatches()) {
+                batches.removeNonLoadBatches();
+            }
         }
 
         return batches.getBatches();
