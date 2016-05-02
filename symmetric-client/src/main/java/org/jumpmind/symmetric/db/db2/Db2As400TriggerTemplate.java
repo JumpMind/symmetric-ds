b9545a3a0b5e9018047c115338f8fa3db13450cc
elong <elong@jumpmind.com>
2016-05-02 16:51:41 -0400

    0002584: Improve capture of LOBs and wide tables on DB2

--- symmetric-client/src/main/java/org/jumpmind/symmetric/db/db2/Db2As400TriggerTemplate.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/db/db2/Db2As400TriggerTemplate.java
@@ -22,7 +22,7 @@ public class Db2As400TriggerTemplate extends Db2TriggerTemplate {
 "                                        INSERT into $(defaultSchema)$(prefixName)_data                                                                                                                 " +
 "                                            (table_name, event_type, trigger_hist_id, row_data, channel_id, transaction_id, source_node_id, external_data, create_time)                                " +
 "                                        VALUES('$(targetTableName)', 'I', $(triggerHistoryId),                                                                                                         " +
-"                                            $(columns),                                                                                                                                                " +
+"                                            $(oracleToClob)$(columns),                                                                                                                                 " +
 "                                            $(channelExpression), $(txIdExpression), $(sourceNodeExpression),                                                                                          " +
 "                                            $(externalSelect),                                                                                                                                         " +
 "                                            CURRENT_TIMESTAMP);                                                                                                                                        " +
@@ -41,9 +41,9 @@ public class Db2As400TriggerTemplate extends Db2TriggerTemplate {
 "                                            INSERT into $(defaultSchema)$(prefixName)_data                                                                                                             \n"+
 "                                                (table_name, event_type, trigger_hist_id, pk_data, row_data, old_data, channel_id, transaction_id, source_node_id, external_data, create_time)         \n"+
 "                                            VALUES('$(targetTableName)', 'U', $(triggerHistoryId),                                                                                                     \n"+
-"                                                $(oldKeys),                                                                                                                                            \n"+
-"                                                $(columns),                                                                                                                                          \n"+
-"                                                $(oldColumns),                                                                                                                                          \n"+
+"                                                $(oracleToClob)$(oldKeys),                                                                                                                             \n"+
+"                                                $(oracleToClob)$(columns),                                                                                                                             \n"+
+"                                                $(oracleToClob)$(oldColumns),                                                                                                                          \n"+
 "                                                $(channelExpression),                                                                                                                                      \n"+
 "                                                $(txIdExpression),                                                                                                                                     \n"+
 "                                                $(sourceNodeExpression),                                                                                                                               \n"+
@@ -63,8 +63,8 @@ public class Db2As400TriggerTemplate extends Db2TriggerTemplate {
 "                                        INSERT into $(defaultSchema)$(prefixName)_data                                                                                                                 " +
 "                                            (table_name, event_type, trigger_hist_id, pk_data, old_data, channel_id, transaction_id, source_node_id, external_data, create_time)                       " +
 "                                        VALUES ('$(targetTableName)', 'D', $(triggerHistoryId),                                                                                                        " +
-"                                            $(oldKeys),                                                                                                                                                " +
-"                                            $(oldColumns),                                                                                                                                             " +
+"                                            $(oracleToClob)$(oldKeys),                                                                                                                                 " +
+"                                            $(oracleToClob)$(oldColumns),                                                                                                                              " +
 "                                            $(channelExpression),                                                                                                                                          " +
 "                                            $(txIdExpression),                                                                                                                                         " +
 "                                            $(sourceNodeExpression),                                                                                                                                   " +
