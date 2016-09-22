2469f2ac230c6db6562c47472fbfae2479ccc65b
mmichalek <mmichalek@jumpmind.com>
2016-09-22 16:03:02 -0400

    Logging cleanup

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/PushService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/PushService.java
@@ -155,13 +155,12 @@ public class PushService extends AbstractOfflineDetectorService implements IPush
                                 node);
                     }
                     reloadBatchesProcessed = status.getReloadBatchesProcessed();
-                    log.debug("Push requested for {} {}", node, 
-                    		" channel " + nodeCommunication.getQueue());
+                    log.debug("Push requested for node {} channel {}", node, nodeCommunication.getQueue());
                     pushToNode(node, status);
                     if (!status.failed() && status.getBatchesProcessed() > 0 
                             && status.getBatchesProcessed() != lastBatchCount) {
                         log.info(
-                                "Pushed data to {}. {} data and {} batches were processed",
+                                "Pushed data to node {}. {} data and {} batches were processed",
                                 new Object[] { node, status.getDataProcessed(),
                                         status.getBatchesProcessed()});
                     } else if (status.failed()) {
@@ -170,8 +169,7 @@ public class PushService extends AbstractOfflineDetectorService implements IPush
                                 new Object[] { node, status.getDataProcessed(),
                                         status.getBatchesProcessed()});                        
                     }
-                    log.debug("Push completed for {} {}", node, 
-                    		" channel " + nodeCommunication.getQueue());
+                    log.debug("Push completed for {} channel {}", node, nodeCommunication.getQueue());
                     lastBatchCount = status.getBatchesProcessed();
                 } while (status.getReloadBatchesProcessed() > reloadBatchesProcessed && !status.failed());
             } finally {
