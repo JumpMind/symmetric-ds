0aad30ee247b0433da52ee7b8b8b628ebc65c16d
Chris Henson <chenson42@gmail.com>
2017-11-29 21:28:46 -0500

    format code

--- symmetric-core/src/main/java/org/jumpmind/symmetric/load/ConfigurationChangedDatabaseWriterFilter.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/load/ConfigurationChangedDatabaseWriterFilter.java
@@ -160,18 +160,19 @@ public class ConfigurationChangedDatabaseWriterFilter extends DatabaseWriterFilt
             tables.add(table);
         }
         
-        if (data.getDataEventType() == DataEventType.UPDATE && 
-        		!engine.getParameterService().is(ParameterConstants.TRIGGER_CREATE_BEFORE_INITIAL_LOAD)) {
-        	if (matchesTable(table, TableConstants.SYM_NODE_SECURITY)) {
-        	    Map<String,String> newData = data.toColumnNameValuePairs(table.getColumnNames(), CsvData.ROW_DATA);
-        	    String initialLoadEnabled = newData.get("INITIAL_LOAD_ENABLED"); 
-        	    String initialLoadTime = newData.get("INITIAL_LOAD_TIME");
-        	    if (StringUtils.isNotBlank(initialLoadTime) && "0".equals(initialLoadEnabled)) {
-        	    	log.info("Requesting syncTriggers because {} is false and sym_node_security changed to indicate that an initial load has completed",
-        	    			ParameterConstants.TRIGGER_CREATE_BEFORE_INITIAL_LOAD);
+        if (data.getDataEventType() == DataEventType.UPDATE
+                && !engine.getParameterService().is(ParameterConstants.TRIGGER_CREATE_BEFORE_INITIAL_LOAD)) {
+            if (matchesTable(table, TableConstants.SYM_NODE_SECURITY)) {
+                Map<String, String> newData = data.toColumnNameValuePairs(table.getColumnNames(), CsvData.ROW_DATA);
+                String initialLoadEnabled = newData.get("INITIAL_LOAD_ENABLED");
+                String initialLoadTime = newData.get("INITIAL_LOAD_TIME");
+                if (StringUtils.isNotBlank(initialLoadTime) && "0".equals(initialLoadEnabled)) {
+                    log.info(
+                            "Requesting syncTriggers because {} is false and sym_node_security changed to indicate that an initial load has completed",
+                            ParameterConstants.TRIGGER_CREATE_BEFORE_INITIAL_LOAD);
                     context.put(CTX_KEY_RESYNC_NEEDED, true);
-        	    }
-        	}
+                }
+            }
         }
     }
 
