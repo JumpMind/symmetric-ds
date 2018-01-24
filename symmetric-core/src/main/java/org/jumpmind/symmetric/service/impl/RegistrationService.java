001a1f4c8c668dd11b6d74de4df22aefa5bcefe4
maxwellpettit <maxwell.pettit@gmail.com>
2018-01-24 08:14:59 -0500

    0003367: Remote node replacing Master Node User

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/RegistrationService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/RegistrationService.java
@@ -123,7 +123,8 @@ public class RegistrationService extends AbstractService implements IRegistratio
 		node.setNodeGroupId(nodeGroupId);
 		node.setDatabaseType(databaseType);
 		node.setDatabaseVersion(databaseVersion);
-		
+		node.setDeploymentType(Constants.DEPLOYMENT_TYPE_REST);
+
 		node = processRegistration(node, null, null, true);
 		
 		if (node.isSyncEnabled()) {
