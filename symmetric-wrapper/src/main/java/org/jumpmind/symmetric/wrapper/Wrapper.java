22aa3fafd3853818f05ae511035e114b1495575b
elong <elong@jumpmind.com>
2018-04-20 12:40:29 -0400

    0003525: Service wrapper: remove reliance on SYM_HOME environment variable

--- symmetric-wrapper/src/main/java/org/jumpmind/symmetric/wrapper/Wrapper.java
+++ symmetric-wrapper/src/main/java/org/jumpmind/symmetric/wrapper/Wrapper.java
@@ -21,30 +21,113 @@
 package org.jumpmind.symmetric.wrapper;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Wrapper {
 
     public static void main(String[] args) throws Exception {
-    	
-        String dir = System.getenv(Constants.ENV_SYM_HOME);
-        
-        if (dir == null || dir.equals("")) {
-            // Backwards compatible with 3.6 by allowing config file argument to determine home
-            if (args.length > 1) {
-                int index = args[1].lastIndexOf(File.separator);
-                if (index == -1) {
-                    dir = "..";
-                } else {
-                    dir = args[1].substring(0, index + 1) + "..";
-                }
+
+        if (args.length < 1) {
+            printUsage();
+            System.exit(Constants.RC_BAD_USAGE);
+        }
+
+        String appDir = null;
+        String configFile = null;
+        String jarFile = Wrapper.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+
+        if (args.length > 1) {
+            configFile = args[1];
+            appDir = getParentDir(configFile);
+        } else {
+            appDir = getParentDir(jarFile);
+            configFile = findConfigFile(appDir + File.separator + "conf");
+        }
+
+        WrapperService service = WrapperService.getInstance();
+        try {
+            service.loadConfig(appDir, configFile, jarFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("Missing config file " + configFile);
+            System.out.println(e.getMessage());
+            System.exit(Constants.RC_MISSING_CONFIG_FILE);
+        } catch (IOException e) {
+            System.out.println("Cannot read config file " + configFile);
+            System.out.println(e.getMessage());
+            System.exit(Constants.RC_FAIL_READ_CONFIG_FILE);
+        }
+
+        try {
+            if (args[0].equalsIgnoreCase("start")) {
+                service.start();
+            } else if (args[0].equalsIgnoreCase("exec")) {
+                service.execJava(false);
+            } else if (args[0].equalsIgnoreCase("init")) {
+                service.init();
+            } else if (args[0].equalsIgnoreCase("stop")) {
+                service.stop();
+            } else if (args[0].equalsIgnoreCase("restart")) {    
+                service.restart();
+            } else if (args[0].equalsIgnoreCase("install")) {
+                service.install();
+            } else if (args[0].equalsIgnoreCase("uninstall")) {
+                service.uninstall();
+            } else if (args[0].equalsIgnoreCase("status")) {
+                service.status();
+            } else if (args[0].equalsIgnoreCase("console")) {
+                service.console();
             } else {
-                System.out.println("Missing " + Constants.ENV_SYM_HOME + " environment variable.");
-                System.exit(Constants.RC_MISSING_SYM_HOME);
+                System.out.println("ERROR: Invalid argument");
+                printUsage();
+                System.exit(Constants.RC_INVALID_ARGUMENT);
+            }
+        } catch (WrapperException e) {
+            System.out.println("Error " + e.getErrorCode() + ": " + e.getMessage());
+            if (e.getCause() != null) {
+                System.out.println("Exception " + e.getCause().getClass().getSimpleName() + ": "
+                        + e.getCause().getMessage());
+            }
+            if (e.getNativeErrorCode() > 0) {
+                System.out.println("Native error " + e.getErrorCode());    
+            }
+            System.exit(e.getErrorCode());
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    protected static String getParentDir(String filepath) {
+        int index = filepath.lastIndexOf(File.separator);
+        if (index == -1) {
+            return "..";
+        } else {
+            return filepath.substring(0, index + 1) + "..";
+        }
+    }
+
+    protected static String findConfigFile(String dirpath) {
+        File dir = new File(dirpath);
+        if (dir.exists() && dir.isDirectory()) {
+            for (String name : dir.list()) {
+                if (name.endsWith("_service.conf")) {
+                    return dirpath + File.separator + name;
+                }
             }
         }
-        String configFile = dir + File.separator + "conf" + File.separator + "sym_service.conf";
-        String jarFile = dir + File.separator + "lib" + File.separator + Constants.JAR_NAME;
-        
-        WrapperHelper.run(args, dir, configFile, jarFile);
+        return dirpath + File.separator + "wrapper_service.conf"; 
     }
+
+    protected static void printUsage() {
+        System.out.println("Usage: <start|stop|restart|install|remove|console>");
+        System.out.println("   start      - Start service");
+        System.out.println("   stop       - Stop service");
+        System.out.println("   restart    - Restart service");
+        System.out.println("   install    - Install service");
+        System.out.println("   uninstall  - Uninstall service");
+        System.out.println("   status     - Status of service");
+        System.out.println("   console    - Run from console");
+    }
+
 }
