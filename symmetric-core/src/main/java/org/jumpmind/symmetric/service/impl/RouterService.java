5f3056079f145281617b5a5edb36218c5fc989d5
elong <elong@jumpmind.com>
2018-09-20 09:22:17 -0400

    0003730: Common batch mode for bi-directional sync on incoming when using all default routers

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/RouterService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/RouterService.java
@@ -634,9 +634,8 @@ public class RouterService extends AbstractService implements IRouterService {
                                     String incomingTableName = triggerRouter2.getTrigger().getFullyQualifiedSourceTableName();
                                     String targetNodeGroupId = triggerRouter2.getRouter().getNodeGroupLink()
                                             .getTargetNodeGroupId();
-                                    if (incomingTableName
-                                            .equals(outgoingTableName)
-                                            && targetNodeGroupId.equals(nodeGroupId)) {
+                                    if (incomingTableName.equals(outgoingTableName) && targetNodeGroupId.equals(nodeGroupId) 
+                                            && !"default".equals(triggerRouter2.getRouter().getRouterType())) {
                                         producesCommonBatches = false;
                                         break;
                                     }
