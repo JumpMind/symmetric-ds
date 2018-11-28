37bc4262e45cd1f9c39c066d31d110119cd746c3
mmichalek <mmichalek@jumpmind.com>
2018-11-28 10:57:40 -0500

    0003807: Symmetric startup is very slow when there are lots of staging files on a SAN (3.9)

--- symmetric-core/src/main/java/org/jumpmind/symmetric/io/stage/BatchStagingManager.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/io/stage/BatchStagingManager.java
@@ -1,16 +1,19 @@
 package org.jumpmind.symmetric.io.stage;
 
 import static org.jumpmind.symmetric.common.Constants.STAGING_CATEGORY_INCOMING;
+
 import static org.jumpmind.symmetric.common.Constants.STAGING_CATEGORY_OUTGOING;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.jumpmind.symmetric.ISymmetricEngine;
 import org.jumpmind.symmetric.common.ParameterConstants;
+import org.jumpmind.symmetric.io.stage.IStagedResource.State;
 import org.jumpmind.symmetric.model.BatchId;
 import org.jumpmind.symmetric.service.ClusterConstants;
 
@@ -23,7 +26,7 @@ public class BatchStagingManager extends StagingManager {
         this.engine = engine;
     }
     
-    protected Map<String, Long> getBiggestBatchIds(List<BatchId> batches) {
+    protected Map<String, Long> getBiggestBatchIds(Set<BatchId> batches) {
         Map<String,Long> biggest = new HashMap<String,Long>();
         for (BatchId batchId : batches) {
             Long batchNumber = biggest.get(batchId.getNodeId());
@@ -32,7 +35,7 @@ public class BatchStagingManager extends StagingManager {
             }
         }        
         return biggest;
-    }
+    }    
 
     @Override
     public long clean(long ttlInMs) {
@@ -50,17 +53,24 @@ public class BatchStagingManager extends StagingManager {
         } catch (Exception e) {
             // during setup or un-install, it's possible sym_lock table isn't available yet
         }
-
-        try {            
+        try {
             boolean purgeBasedOnTTL = engine.getParameterService().is(ParameterConstants.STREAM_TO_FILE_PURGE_ON_TTL_ENABLED, false);
-            if (purgeBasedOnTTL) {
-                return super.clean(ttlInMs);
-            } else {
-                synchronized (StagingManager.class) {
-                    return purgeStagingBasedOnDatabaseStatus(ttlInMs);
-                }
-            }
-        } finally {
+            boolean recordIncomingBatchesEnabled = engine.getIncomingBatchService().isRecordOkBatchesEnabled();
+            long minTtlInMs = engine.getParameterService().getLong(ParameterConstants.STREAM_TO_FILE_MIN_TIME_TO_LIVE_MS,600000);
+            Set<Long> outgoingBatches = ttlInMs == 0 ? new HashSet<Long>() : new HashSet<Long>(engine.getOutgoingBatchService().getAllBatches());
+            Set<BatchId> incomingBatches =  ttlInMs == 0 ? new HashSet<BatchId>() : new HashSet<BatchId>(engine.getIncomingBatchService().getAllBatches());
+            Map<String, Long> biggestIncomingByNode = getBiggestBatchIds(incomingBatches);
+            
+            StagingPurgeContext context = new StagingPurgeContext();
+            context.putContextValue("purgeBasedOnTTL", purgeBasedOnTTL);
+            context.putContextValue("recordIncomingBatchesEnabled", recordIncomingBatchesEnabled);
+            context.putContextValue("minTtlInMs", minTtlInMs);
+            context.putContextValue("outgoingBatches", outgoingBatches);
+            context.putContextValue("incomingBatches", incomingBatches);
+            context.putContextValue("biggestIncomingByNode", biggestIncomingByNode);
+            
+            return super.clean(ttlInMs, context);            
+        } finally {            
             if (isLockAcquired) {
                 try {
                     engine.getClusterService().unlock(ClusterConstants.STAGE_MANAGEMENT);
@@ -70,73 +80,69 @@ public class BatchStagingManager extends StagingManager {
         }
     }
     
-    protected long purgeStagingBasedOnDatabaseStatus(long ttlInMs) {
-        boolean recordIncomingBatchesEnabled = engine.getIncomingBatchService().isRecordOkBatchesEnabled();
-        long minTtlInMs = engine.getParameterService().getLong(ParameterConstants.STREAM_TO_FILE_MIN_TIME_TO_LIVE_MS,600000);
-        List<Long> outgoingBatches = ttlInMs == 0 ? new ArrayList<Long>() : engine.getOutgoingBatchService().getAllBatches();
-        List<BatchId> incomingBatches =  ttlInMs == 0 ? new ArrayList<BatchId>() :  engine.getIncomingBatchService().getAllBatches();
-        Map<String, Long> biggestIncomingByNode = getBiggestBatchIds(incomingBatches);
-        synchronized (StagingManager.class) {
-            log.trace("Purging staging area");
-            Set<String> keys = getResourceReferences();
-            long purgedFileCount = 0;
-            long purgedFileSize = 0;
-            for (String key : keys) {
-                IStagedResource resource = find(key);
-                String[] path = key.split("/");
-                /*
-                 * resource could have deleted itself between the time the keys
-                 * were cloned and now
-                 */
-                if (resource != null && !resource.isInUse()) {
-                    boolean resourceIsOld = (System.currentTimeMillis() - resource.getLastUpdateTime()) > ttlInMs;
-                    boolean resourceClearsMinTimeHurdle = (System.currentTimeMillis() - resource.getLastUpdateTime()) > minTtlInMs;
-                    if (path[0].equals(STAGING_CATEGORY_OUTGOING)) {
-                        try {
-                            Long batchId = new Long(path[path.length - 1]);
-                            if (!outgoingBatches.contains(batchId) || ttlInMs == 0) {
-                                purgedFileCount++;
-                                purgedFileSize+=resource.getSize();
-                                resource.delete();
-                            }
-                        } catch (NumberFormatException e) {
-                            if (resourceIsOld || ttlInMs == 0) {
-                                purgedFileCount++;
-                                purgedFileSize+=resource.getSize();
-                                resource.delete();
-                            }
-                        }
-                    } else if (path[0].equals(STAGING_CATEGORY_INCOMING)) {
-                        try {
-                            BatchId batchId = new BatchId(new Long(path[path.length - 1]), path[1]);
-                            Long biggestBatchId = biggestIncomingByNode.get(batchId.getNodeId());
-                            if ((recordIncomingBatchesEnabled && !incomingBatches.contains(batchId) && 
-                                    biggestBatchId != null && biggestBatchId > batchId.getBatchId() &&
-                                    resourceClearsMinTimeHurdle)
-                                    || (!recordIncomingBatchesEnabled && resourceIsOld) || ttlInMs == 0) {
-                                purgedFileCount++;
-                                purgedFileSize+=resource.getSize();
-                                resource.delete();
-                            }
-                        } catch (NumberFormatException e) {
-                            if (resourceIsOld || ttlInMs == 0) {
-                                purgedFileCount++;
-                                purgedFileSize+=resource.getSize();
-                                resource.delete();
-                            }
-                        }
-                    }
-                }
-            }
-            if (purgedFileCount > 0) {
-                if (purgedFileSize < 1000) {
-                    log.info("Purged {} from stage, freeing {} bytes of space", purgedFileCount, (int) (purgedFileSize));
-                } else {
-                    log.info("Purged {} from stage, freeing {} kbytes of space", purgedFileCount, (int) (purgedFileSize / 1000));
-                }
-            }
-            return purgedFileCount;
+    @Override
+    protected boolean shouldCleanPath(IStagedResource resource, long ttlInMs, StagingPurgeContext context) {
+        if (context.getBoolean("purgeBasedOnTTL")) {
+            return super.shouldCleanPath(resource, ttlInMs, context);
         }
+        
+        if (resource.isInUse() || resource.getState() != State.DONE) {
+            return false;
+        }
+        
+        String[] path = resource.getPath().split("/");
+        
+        boolean resourceIsOld = (System.currentTimeMillis() - resource.getLastUpdateTime()) > ttlInMs;
+        boolean resourceClearsMinTimeHurdle = (System.currentTimeMillis() - resource.getLastUpdateTime()) > context.getLong("minTtlInMs");
+        
+        if (path[0].equals(STAGING_CATEGORY_OUTGOING)) {
+            return shouldCleanOutgoingPath(resource, ttlInMs, context, path, resourceIsOld);
+        }  else if (path[0].equals(STAGING_CATEGORY_INCOMING)) {
+            return shouldCleanIncomingPath(resource, ttlInMs, context, path, resourceIsOld, resourceClearsMinTimeHurdle);
+        } else {
+            log.warn("Unrecognized path: " + resource.getPath());
+        }        
+        return false;
+    }
+    
+    protected boolean shouldCleanOutgoingPath(IStagedResource resource, long ttlInMs, StagingPurgeContext context, String[] path,
+            boolean resourceIsOld) {
+        Set<Long> outgoingBatches = (Set<Long>) context.getContextValue("outgoingBatches");
+        try {
+            Long batchId = new Long(path[path.length - 1]);
+            if (!outgoingBatches.contains(batchId) || ttlInMs == 0) {
+                return true;
+            }
+        } catch (NumberFormatException e) {
+            if (resourceIsOld || ttlInMs == 0) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    protected boolean shouldCleanIncomingPath(IStagedResource resource, long ttlInMs, StagingPurgeContext context, String[] path, boolean resourceIsOld,
+            boolean resourceClearsMinTimeHurdle) {
+        Set<BatchId> incomingBatches = (Set<BatchId>) context.getContextValue("incomingBatches");
+        Map<String, Long> biggestIncomingByNode = (Map<String, Long>) context.getContextValue("biggestIncomingByNode");
+        boolean recordIncomingBatchesEnabled = context.getBoolean("recordIncomingBatchesEnabled");
+        try {
+            BatchId batchId = new BatchId(new Long(path[path.length - 1]), path[1]);
+            Long biggestBatchId = biggestIncomingByNode.get(batchId.getNodeId());
+            if ((recordIncomingBatchesEnabled && !incomingBatches.contains(batchId) && 
+                    biggestBatchId != null && biggestBatchId > batchId.getBatchId() &&
+                    resourceClearsMinTimeHurdle)
+                    || (!recordIncomingBatchesEnabled && resourceIsOld) || ttlInMs == 0) {
+                return true;
+            }
+        } catch (NumberFormatException e) {
+            if (resourceIsOld || ttlInMs == 0) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 
 }
