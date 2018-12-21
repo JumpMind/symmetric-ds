d4166651c044cb53a13a0e1b0f7d4871c604dcb2
Eric Long <elong@jumpmind.com>
2018-12-21 12:10:05 -0500

    fix formatting

--- symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/KafkaWriter.java
+++ symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/KafkaWriter.java
@@ -1,29 +1,32 @@
+/**
+ * Licensed to JumpMind Inc under one or more contributor
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU General Public License, version 3.0 (GPLv3)
+ * (the "License"); you may not use this file except in compliance
+ * with the License.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * version 3.0 (GPLv3) along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jumpmind.symmetric.io.data.writer;
 
-import org.jumpmind.db.model.Table;
 import org.jumpmind.db.platform.IDatabasePlatform;
-import org.jumpmind.symmetric.io.data.CsvData;
 
 public class KafkaWriter extends DynamicDefaultDatabaseWriter {
 
-	public KafkaWriter(IDatabasePlatform symmetricPlatform, IDatabasePlatform targetPlatform, String prefix,
-			IDatabaseWriterConflictResolver conflictResolver, DatabaseWriterSettings settings) {
-		super(symmetricPlatform, targetPlatform, prefix, conflictResolver, settings);
-	}
+    public KafkaWriter(IDatabasePlatform symmetricPlatform, IDatabasePlatform targetPlatform, String prefix,
+            IDatabaseWriterConflictResolver conflictResolver, DatabaseWriterSettings settings) {
+        super(symmetricPlatform, targetPlatform, prefix, conflictResolver, settings);
+    }
 
-	/*
-	@Override
-	protected Table lookupTableAtTarget(Table sourceTable) {
-		return sourceTable;
-	}
-	
-	
-	@Override
-	protected void logFailureDetails(Throwable e, CsvData data, boolean logLastDmlDetails) {
-	}
-
-	@Override
-	protected void allowInsertIntoAutoIncrementColumns(boolean value, Table table) {
-	}
-	*/
 }
