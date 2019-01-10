c3e84a7074a449a380b918146b10167973c7f0e7
Eric Long <elong@jumpmind.com>
2019-01-10 14:37:48 -0500

    update descriptions, remove manual inserts to node tables

--- symmetric-server/src/main/deploy/samples/insert_sample.sql
+++ symmetric-server/src/main/deploy/samples/insert_sample.sql
@@ -22,17 +22,20 @@
 ------------------------------------------------------------------------------
 -- Sample Data
 ------------------------------------------------------------------------------
+
+-- Items to sell and their prices
 insert into item (item_id, name) values (11000001, 'Yummy Gum');
 insert into item_selling_price (item_id, store_id, price, cost) values (11000001, '001',0.20, 0.10);
 insert into item_selling_price (item_id, store_id, price, cost) values (11000001, '002',0.30, 0.20);
 
+-- Sales transactions and line items
 insert into sale_transaction (tran_id, store_id, workstation, day, seq) 
 values (900, '001', '3', '2012-12-01', 90);
 insert into sale_return_line_item (tran_id, item_id, price, quantity, returned_quantity)
 values (900, 11000001, 0.20, 1, 0);
 
 ------------------------------------------------------------------------------
--- Sample Symmetric Configuration
+-- Clear and load SymmetricDS Configuration
 ------------------------------------------------------------------------------
 
 delete from sym_trigger_router;
@@ -46,20 +49,42 @@ delete from sym_node_identity;
 delete from sym_node_security;
 delete from sym_node;
 
+------------------------------------------------------------------------------
+-- Channels
+------------------------------------------------------------------------------
+
+-- Channel "sale_transaction" for tables related to sales and refunds
 insert into sym_channel 
 (channel_id, processing_order, max_batch_size, enabled, description)
 values('sale_transaction', 1, 100000, 1, 'sale_transactional data from register and back office');
 
+-- Channel "item" for tables related to items for purchase
 insert into sym_channel 
 (channel_id, processing_order, max_batch_size, enabled, description)
 values('item', 1, 100000, 1, 'Item and pricing data');
 
+------------------------------------------------------------------------------
+-- Node Groups
+------------------------------------------------------------------------------
+
 insert into sym_node_group (node_group_id) values ('corp');
 insert into sym_node_group (node_group_id) values ('store');
 
+------------------------------------------------------------------------------
+-- Node Group Links
+------------------------------------------------------------------------------
+
+-- Corp sends changes to Store when Store pulls from Corp
 insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action) values ('corp', 'store', 'W');
+
+-- Store sends changes to Corp when Store pushes to Corp
 insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action) values ('store', 'corp', 'P');
 
+------------------------------------------------------------------------------
+-- Triggers
+------------------------------------------------------------------------------
+
+-- Triggers for tables on "item" channel
 insert into sym_trigger 
 (trigger_id,source_table_name,channel_id,last_update_time,create_time)
 values('item_selling_price','item_selling_price','item',current_timestamp,current_timestamp);
@@ -68,6 +93,7 @@ insert into sym_trigger
 (trigger_id,source_table_name,channel_id,last_update_time,create_time)
 values('item','item','item',current_timestamp,current_timestamp);
 
+-- Triggers for tables on "sale_transaction" channel
 insert into sym_trigger 
 (trigger_id,source_table_name,channel_id,last_update_time,create_time)
 values('sale_transaction','sale_transaction','sale_transaction',current_timestamp,current_timestamp);
@@ -76,6 +102,7 @@ insert into sym_trigger
 (trigger_id,source_table_name,channel_id,last_update_time,create_time)
 values('sale_return_line_item','sale_return_line_item','sale_transaction',current_timestamp,current_timestamp);
 
+-- Triggers with capture disabled, so they are used for initial load only
 insert into sym_trigger 
 (trigger_id,source_table_name,channel_id, sync_on_insert, sync_on_update, sync_on_delete,last_update_time,create_time)
 values('sale_transaction_corp','sale_transaction','sale_transaction',0,0,0,current_timestamp,current_timestamp);
@@ -84,26 +111,41 @@ insert into sym_trigger
 (trigger_id,source_table_name,channel_id, sync_on_insert, sync_on_update, sync_on_delete,last_update_time,create_time)
 values('sale_return_line_item_corp','sale_return_line_item','sale_transaction',0,0,0,current_timestamp,current_timestamp);
 
+------------------------------------------------------------------------------
+-- Routers
+------------------------------------------------------------------------------
+
+-- Default router sends all data from corp to store 
 insert into sym_router 
 (router_id,source_node_group_id,target_node_group_id,router_type,create_time,last_update_time)
 values('corp_2_store', 'corp', 'store', 'default',current_timestamp, current_timestamp);
 
+-- Default router sends all data from store to corp
 insert into sym_router 
 (router_id,source_node_group_id,target_node_group_id,router_type,create_time,last_update_time)
 values('store_2_corp', 'store', 'corp', 'default',current_timestamp, current_timestamp);
 
+-- Column match router will subset data from corp to specific store
 insert into sym_router 
 (router_id,source_node_group_id,target_node_group_id,router_type,router_expression,create_time,last_update_time)
 values('corp_2_one_store', 'corp', 'store', 'column','STORE_ID=:EXTERNAL_ID or OLD_STORE_ID=:EXTERNAL_ID',current_timestamp, current_timestamp);
 
+
+------------------------------------------------------------------------------
+-- Trigger Routers
+------------------------------------------------------------------------------
+
+-- Send all items to all stores
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
 values('item','corp_2_store', 100, current_timestamp, current_timestamp);
 
+-- Send item prices to associated stores
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,initial_load_select,last_update_time,create_time)
 values('item_selling_price','corp_2_one_store',100,'store_id=''$(externalId)''',current_timestamp,current_timestamp);
 
+-- Send all sales transactions to corp
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
 values('sale_transaction','store_2_corp', 200, current_timestamp, current_timestamp);
@@ -112,6 +154,7 @@ insert into sym_trigger_router
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
 values('sale_return_line_item','store_2_corp', 200, current_timestamp, current_timestamp);
 
+-- Send all sales transactions to store during initial load
 insert into sym_trigger_router 
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
 values('sale_transaction_corp','corp_2_store', 200, current_timestamp, current_timestamp);
@@ -120,19 +163,3 @@ insert into sym_trigger_router
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
 values('sale_return_line_item_corp','corp_2_store', 200, current_timestamp, current_timestamp);
 
-insert into sym_node (node_id,node_group_id,external_id,sync_enabled,sync_url,schema_version,symmetric_version,database_type,database_version,batch_to_send_count,batch_in_error_count,created_at_node_id) 
- values ('000','corp','000',1,null,null,null,null,null,0,0,'000');
-insert into sym_node (node_id,node_group_id,external_id,sync_enabled,sync_url,schema_version,symmetric_version,database_type,database_version,batch_to_send_count,batch_in_error_count,created_at_node_id) 
- values ('001','store','001',1,null,null,null,null,null,0,0,'000');
-insert into sym_node (node_id,node_group_id,external_id,sync_enabled,sync_url,schema_version,symmetric_version,database_type,database_version,batch_to_send_count,batch_in_error_count,created_at_node_id) 
- values ('002','store','002',1,null,null,null,null,null,0,0,'000');
-
-
-insert into sym_node_security (node_id,node_password,registration_enabled,registration_time,initial_load_enabled,initial_load_time,created_at_node_id) 
- values ('000','5d1c92bbacbe2edb9e1ca5dbb0e481',0,current_timestamp,0,current_timestamp,'000');
-insert into sym_node_security (node_id,node_password,registration_enabled,registration_time,initial_load_enabled,initial_load_time,created_at_node_id) 
- values ('001','5d1c92bbacbe2edb9e1ca5dbb0e481',1,null,1,null,'000');
-insert into sym_node_security (node_id,node_password,registration_enabled,registration_time,initial_load_enabled,initial_load_time,created_at_node_id) 
- values ('002','5d1c92bbacbe2edb9e1ca5dbb0e481',1,null,1,null,'000');
-
-insert into sym_node_identity values ('000');
