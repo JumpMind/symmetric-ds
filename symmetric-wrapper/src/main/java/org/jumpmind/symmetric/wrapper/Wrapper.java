9646deb74c268a4094e75593163f0c82f174c913
Jared Frees_Home <jaredfrees@gmail.com>
2019-05-14 12:36:02 -0400

    0003242: Running DS as a Windows Service in a folder path that contains whitespace causes error Remove unused import from testing fix

--- symmetric-wrapper/src/main/java/org/jumpmind/symmetric/wrapper/Wrapper.java
+++ symmetric-wrapper/src/main/java/org/jumpmind/symmetric/wrapper/Wrapper.java
@@ -23,7 +23,6 @@ package org.jumpmind.symmetric.wrapper;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URLDecoder;
 
 public class Wrapper {
 
