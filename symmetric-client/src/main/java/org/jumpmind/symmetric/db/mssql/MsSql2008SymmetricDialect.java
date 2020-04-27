d53b2672a64ef466c3cb1560561a9b6e885e6b8f
Eric Long <elong@jumpmind.com>
2020-04-27 08:24:11 -0400

    0004362: SQL Server 2000 gets Invalid object name 'sys.triggers'

--- symmetric-client/src/main/java/org/jumpmind/symmetric/db/mssql/MsSql2008SymmetricDialect.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/db/mssql/MsSql2008SymmetricDialect.java
@@ -1,6 +1,13 @@
 package org.jumpmind.symmetric.db.mssql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.jumpmind.db.platform.IDatabasePlatform;
+import org.jumpmind.db.sql.IConnectionCallback;
+import org.jumpmind.db.sql.JdbcSqlTemplate;
 import org.jumpmind.symmetric.model.Trigger;
 import org.jumpmind.symmetric.service.IParameterService;
 
@@ -19,4 +26,32 @@ public class MsSql2008SymmetricDialect extends MsSqlSymmetricDialect {
         /* gets filled/replaced by trigger template as it will compare by each column */
         return "$(anyColumnChanged)";
     }
+    
+    @Override
+    public boolean doesDdlTriggerExist(final String catalogName, final String schema, final String triggerName) {
+        return ((JdbcSqlTemplate) platform.getSqlTemplate()).execute(new IConnectionCallback<Boolean>() {
+            public Boolean execute(Connection con) throws SQLException {
+                String previousCatalog = con.getCatalog();
+                PreparedStatement stmt = con.prepareStatement("select count(*) from sys.triggers where name = ?");
+                try {
+                    if (catalogName != null) {
+                        con.setCatalog(catalogName);
+                    }
+                    stmt.setString(1, triggerName);
+                    ResultSet rs = stmt.executeQuery();
+                    if (rs.next()) {
+                        int count = rs.getInt(1);
+                        return count > 0;
+                    }
+                } finally {
+                    if (catalogName != null) {
+                        con.setCatalog(previousCatalog);
+                    }
+                    stmt.close();
+                }
+                return Boolean.FALSE;
+            }
+        });
+    }
+
 }
