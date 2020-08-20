1ca8afcb239b6b48b3482ec5ce685825bd9ae001
Eric Long <elong@jumpmind.com>
2020-08-20 11:32:55 -0400

    0004505: Regression NPE in Oracle, Tibero bulk loader and Hbase, and Mongo data loader factory

--- symmetric-client/src/main/java/org/jumpmind/symmetric/io/HbaseDataLoaderFactory.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/io/HbaseDataLoaderFactory.java
@@ -1,12 +1,30 @@
+/**
+ * Licensed to JumpMind Inc under one or more contributor
+ * license agreements.  See the NOTICE file distributed
+ * with this work for additional information regarding
+ * copyright ownership.  JumpMind Inc licenses this file
+ * to you under the GNU General Public License, version 3.0 (GPLv3)
+ * (the "License"); you may not use this file except in compliance
+ * with the License.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * version 3.0 (GPLv3) along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jumpmind.symmetric.io;
 
 import java.util.List;
 
 import org.jumpmind.extension.IBuiltInExtensionPoint;
-import org.jumpmind.symmetric.ISymmetricEngine;
 import org.jumpmind.symmetric.common.ParameterConstants;
 import org.jumpmind.symmetric.db.ISymmetricDialect;
-import org.jumpmind.symmetric.ext.ISymmetricEngineAware;
 import org.jumpmind.symmetric.io.data.IDataWriter;
 import org.jumpmind.symmetric.io.data.writer.Conflict;
 import org.jumpmind.symmetric.io.data.writer.IDatabaseWriterErrorHandler;
@@ -15,10 +33,7 @@ import org.jumpmind.symmetric.io.data.writer.ResolvedData;
 import org.jumpmind.symmetric.io.data.writer.TransformWriter;
 import org.jumpmind.symmetric.load.DefaultDataLoaderFactory;
 
-public class HbaseDataLoaderFactory extends DefaultDataLoaderFactory implements
-    ISymmetricEngineAware, IBuiltInExtensionPoint {
-
-    protected ISymmetricEngine engine;
+public class HbaseDataLoaderFactory extends DefaultDataLoaderFactory implements IBuiltInExtensionPoint {
 
     protected String typeName = "hbase";
 
@@ -31,12 +46,6 @@ public class HbaseDataLoaderFactory extends DefaultDataLoaderFactory implements
     }
 
     @Override
-    public void setSymmetricEngine(ISymmetricEngine engine) {
-        this.engine = engine;
-        this.parameterService = engine.getParameterService();
-    }
-
-    @Override
     public String getTypeName() {
         return typeName;
     }
@@ -44,7 +53,6 @@ public class HbaseDataLoaderFactory extends DefaultDataLoaderFactory implements
     public void setTypeName(String typeName) {
         this.typeName = typeName;
     }
-
 
     @Override
     public IDataWriter getDataWriter(String sourceNodeId, ISymmetricDialect symmetricDialect,
