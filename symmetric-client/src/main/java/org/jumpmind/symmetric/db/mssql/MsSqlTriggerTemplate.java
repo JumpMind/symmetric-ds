4275d1708a22dd720f086ca80c562d5e2231f368
Philip Marzullo <pmarzullo@jumpmind.com>
2020-10-30 13:13:10 -0400

    0004611: SQL Server primary key update should create update event when only one record is updated

--- symmetric-client/src/main/java/org/jumpmind/symmetric/db/mssql/MsSqlTriggerTemplate.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/db/mssql/MsSqlTriggerTemplate.java
@@ -145,6 +145,8 @@ public class MsSqlTriggerTemplate extends AbstractTriggerTemplate {
 "create trigger $(triggerName) on $(schemaName)$(tableName)                                                     \n" +
 "       with execute as "+triggerExecuteAs+" after update as                                                    \n" +
 "begin                                                                                                          \n" +
+"  declare @LOCALROWCOUNT int                                                                                   \n" + 
+"  set @LOCALROWCOUNT=@@ROWCOUNT                                                                                \n" +
 "  declare @NCT int                                                                                             \n" +
 "  set @NCT = @@OPTIONS & 512                                                                                   \n" +
 "  set nocount on                                                                                               \n" +
@@ -160,6 +162,28 @@ public class MsSqlTriggerTemplate extends AbstractTriggerTemplate {
 "  begin                                                                                                        \n" +
 "    if ($(hasPrimaryKeysDefined) $(primaryKeysUpdated) )                                                       \n" +
 "    begin                                                                                                      \n" +
+"      if (@LOCALROWCOUNT = 1)                                                                                  \n" +
+"      begin                                                                                                    \n" +
+"         insert into  " + defaultCatalog + "$(defaultSchema)$(prefixName)_data                                 \n" +
+"            (table_name, event_type, trigger_hist_id, row_data, pk_data, old_data, channel_id,                 \n" +
+"             transaction_id, source_node_id,                                                                   \n" +
+"             external_data, create_time)                                                                       \n" +
+"          select '$(targetTableName)','U', $(triggerHistoryId), $(columns), $(oldKeys), $(oldColumns),         \n" +
+"                   $(channelExpression),                                                                       \n" +
+"               $(txIdExpression),  " + defaultCatalog + "dbo.$(prefixName)_node_disabled(), $(externalSelect), \n" +
+"                current_timestamp                                                                              \n" +
+"       $(if:containsBlobClobColumns)                                                                           \n" +
+"          from inserted                                                                                        \n" +
+"          inner join $(schemaName)$(tableName) $(origTableAlias) on $(tableNewPrimaryKeyJoin)                  \n" +
+"          inner join deleted on 1=1                                                                            \n" +
+"       $(else:containsBlobClobColumns)                                                                         \n" +
+"          from inserted                                                                                        \n" +
+"          inner join deleted on 1=1                                                                            \n" +
+"       $(end:containsBlobClobColumns)                                                                          \n" +
+"          where $(syncOnUpdateCondition) and ($(dataHasChangedCondition))                                      \n" +
+"      end                                                                                                      \n" +
+"      else                                                                                                     \n" +
+"      begin                                                                                                    \n" +
 "        insert into  " + defaultCatalog + "$(defaultSchema)$(prefixName)_data                                  \n" +
 "           (table_name, event_type, trigger_hist_id, pk_data, channel_id, transaction_id,                      \n" +
 "            source_node_id, external_data, create_time)                                                        \n" +
@@ -183,6 +207,7 @@ public class MsSqlTriggerTemplate extends AbstractTriggerTemplate {
 "          from inserted                                                                                        \n" +
 "       $(end:containsBlobClobColumns)                                                                          \n" +
 "          where $(syncOnInsertCondition)                                                                       \n" +
+"      end                                                                                                      \n" +
 "    end                                                                                                        \n" +
 "    else                                                                                                       \n" +
 "    begin                                                                                                      \n" +
@@ -214,6 +239,8 @@ public class MsSqlTriggerTemplate extends AbstractTriggerTemplate {
 "create trigger $(triggerName) on $(schemaName)$(tableName)                                                     \n" +
 "       with execute as "+triggerExecuteAs+" after update as                                                    \n" +
 "begin                                                                                                          \n" +
+"  declare @LOCALROWCOUNT int                                                                                   \n" + 
+"  set @LOCALROWCOUNT=@@ROWCOUNT                                                                                \n" +
 "  declare @NCT int                                                                                             \n" +
 "  set @NCT = @@OPTIONS & 512                                                                                   \n" +
 "  set nocount on                                                                                               \n" +
@@ -229,6 +256,28 @@ public class MsSqlTriggerTemplate extends AbstractTriggerTemplate {
 "  begin                                                                                                        \n" +
 "    if ($(hasPrimaryKeysDefined) $(primaryKeysUpdated) )                                                       \n" +
 "    begin                                                                                                      \n" +
+"      if (@LOCALROWCOUNT = 1)                                                                                  \n" +
+"      begin                                                                                                    \n" +
+"         insert into  " + defaultCatalog + "$(defaultSchema)$(prefixName)_data                                 \n" +
+"            (table_name, event_type, trigger_hist_id, row_data, pk_data, old_data, channel_id,                 \n" +
+"             transaction_id, source_node_id,                                                                   \n" +
+"             external_data, create_time)                                                                       \n" +
+"          select '$(targetTableName)','U', $(triggerHistoryId), $(columns), $(oldKeys), $(oldColumns),         \n" +
+"                   $(channelExpression),                                                                       \n" +
+"               $(txIdExpression),  " + defaultCatalog + "dbo.$(prefixName)_node_disabled(), $(externalSelect), \n" +
+"                current_timestamp                                                                              \n" +
+"       $(if:containsBlobClobColumns)                                                                           \n" +
+"          from inserted                                                                                        \n" +
+"          inner join $(schemaName)$(tableName) $(origTableAlias) on $(tableNewPrimaryKeyJoin)                  \n" +
+"          inner join deleted on 1=1                                                                            \n" +
+"       $(else:containsBlobClobColumns)                                                                         \n" +
+"          from inserted                                                                                        \n" +
+"          inner join deleted on 1=1                                                                            \n" +
+"       $(end:containsBlobClobColumns)                                                                          \n" +
+"          where $(syncOnUpdateCondition) and ($(dataHasChangedCondition))                                      \n" +
+"      end                                                                                                      \n" +
+"      else                                                                                                     \n" +
+"      begin                                                                                                    \n" +
 "        insert into  " + defaultCatalog + "$(defaultSchema)$(prefixName)_data                                  \n" +
 "           (table_name, event_type, trigger_hist_id, pk_data, channel_id, transaction_id,                      \n" +
 "            source_node_id, external_data, create_time)                                                        \n" +
@@ -252,6 +301,7 @@ public class MsSqlTriggerTemplate extends AbstractTriggerTemplate {
 "          from inserted                                                                                        \n" +
 "       $(end:containsBlobClobColumns)                                                                          \n" +
 "          where $(syncOnInsertCondition)                                                                       \n" +
+"      end                                                                                                      \n" +
 "    end                                                                                                        \n" +
 "    else                                                                                                       \n" +
 "    begin                                                                                                      \n" +
