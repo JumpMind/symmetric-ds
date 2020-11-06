b39c1b106f26f21d6e4881332746c158e430f375
Eric Long <elong@jumpmind.com>
2020-11-06 12:40:48 -0500

    0004625: Multi-primary out of sync with initial load on busy system

--- symmetric-server/src/main/java/org/jumpmind/symmetric/web/NodeConcurrencyInterceptor.java
+++ symmetric-server/src/main/java/org/jumpmind/symmetric/web/NodeConcurrencyInterceptor.java
@@ -88,7 +88,15 @@ public class NodeConcurrencyInterceptor implements IInterceptor {
                     ServletUtils.sendError(resp, WebConstants.SC_SERVICE_ERROR);
                 }
             }
-            
+
+            if (configurationService.isMasterToMaster() & nodeService.isDataLoadStarted()) {
+                NodeSecurity identity = nodeService.findNodeSecurity(nodeService.findIdentityNodeId(), true);
+                if (identity != null && nodeId != null && !nodeId.equals(identity.getCreatedAtNodeId())) {
+                    log.debug("Not allowing push from node {} until initial load from {} is complete", nodeId, identity.getCreatedAtNodeId());
+                    ServletUtils.sendError(resp, WebConstants.INITIAL_LOAD_PENDING);
+                }
+            }
+
             NodeSecurity nodeSecurity = nodeService.findNodeSecurity(nodeId, true);            
             if (nodeSecurity != null) {
                 String createdAtNodeId = nodeSecurity.getCreatedAtNodeId();
