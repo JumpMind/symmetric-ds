6e389b9a7c9d3bac035a9942e9a11327d0592af9
Eric Long <elong@jumpmind.com>
2021-05-10 10:56:33 -0400

    remove unused

--- symmetric-util/src/main/java/org/jumpmind/util/AppUtils.java
+++ symmetric-util/src/main/java/org/jumpmind/util/AppUtils.java
@@ -218,43 +218,6 @@ public class AppUtils {
                 .equalsIgnoreCase(System.getProperty(propName, Boolean.toString(defaultValue)));
     }
 
-    /**
-     * Checks to see if a specific port is available.
-     *
-     * @param port
-     *            the port to check for availability
-     */
-    public static boolean isPortAvailable(int port) {
-        if (port < 1 || port > 65535) {
-            throw new IllegalArgumentException("Invalid start port: " + port);
-        }
-
-        ServerSocket ss = null;
-        DatagramSocket ds = null;
-        try {
-            ss = new ServerSocket(port);
-            ss.setReuseAddress(true);
-            ds = new DatagramSocket(port);
-            ds.setReuseAddress(true);
-            return true;
-        } catch (IOException e) {
-        } finally {
-            if (ds != null) {
-                ds.close();
-            }
-
-            if (ss != null) {
-                try {
-                    ss.close();
-                } catch (IOException e) {
-                    /* should not be thrown */
-                }
-            }
-        }
-
-        return false;
-    }
-
     public static void unzip(InputStream in, File toDir) {
         try {
             ZipInputStream is = new ZipInputStream(in);
