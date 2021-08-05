70508f99384d464c03154d447d879d4662bef202
JD <46619169+rudiejd@users.noreply.github.com>
2021-08-05 12:29:43 -0400

    Fix: V14 label tweak (#163)
    * fix: tweak label to use vaadin Html component, not refresh all children every time
    
    * fix: change display style on span in label
    
    * fix: label text changes
    
    * Update azure-pipelines.yml
    
    * fix: don't use floats in the flex span, remove configure method
    
    * fix: indentation and name refactor for label
    
    * fix: formatting
    
    Co-authored-by: philipmarzullo64 <philipmarzullo64@gmail.com>

--- symmetric-sqlexplorer/src/main/java/org/jumpmind/vaadin/ui/common/Label.java
+++ symmetric-sqlexplorer/src/main/java/org/jumpmind/vaadin/ui/common/Label.java
@@ -21,105 +21,88 @@
 package org.jumpmind.vaadin.ui.common;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 
 public class Label extends Span {
-    
+
     private static final long serialVersionUID = 1L;
-    
-    private String text;
-    
-    private Component leftIcon;
-    
-    private Component rightIcon;
+
+    private Html html;
+
+    private Component icon;
 
     public Label() {
         super();
     }
-    
+
     public Label(String text) {
-        setText(text);
+        html = new Html("<div>" + text + "</div>");
+        getElement().getStyle().set("display", "flex");
+        getElement().getStyle().set("margin", "auto");
+        add(html);
     }
-    
+
     public Label(VaadinIcon icon, String text) {
-        setText(text);
-        setLeftIcon(icon);
+        this(new Icon(icon), text);
+
     }
-    
+
     public Label(String text, VaadinIcon icon) {
-        setText(text);
+        this(text, new Icon(icon));
+    }
+
+    public Label(Icon icon, String text) {
+        this(text);
         setRightIcon(icon);
     }
-    
-    public Label(Icon icon, String text) {
-        setText(text);
+
+    public Label(String text, Icon icon) {
+        this(text);
         setLeftIcon(icon);
     }
-    
-    public Label(String text, Icon icon) {
-        setText(text);
-        setRightIcon(icon);
-    }
-    
+
     public Label(Component component) {
         setText(component.getElement().getOuterHTML());
     }
-    
-    public void setLeftIcon(VaadinIcon icon) {
-        leftIcon = new Icon(icon);
-        configureIcon(leftIcon);
-        updateLabel();
-    }
-    
-    public void setRightIcon(VaadinIcon icon) {
-        rightIcon = new Icon(icon);
-        configureIcon(rightIcon);
-        updateLabel();
-    }
-    
-    public void setLeftIcon(Component icon) {
-        leftIcon = icon;
-        configureIcon(leftIcon);
-        updateLabel();
-    }
-    
-    public void setRightIcon(Component icon) {
-        rightIcon = icon;
-        configureIcon(rightIcon);
-        updateLabel();
-    }
-    
-    @Override
+
     public String getText() {
         return getElement().getProperty("innerHTML");
     }
-    
-    @Override
+
     public void setText(String text) {
-        this.text = text;
-        updateLabel();
-    }
-    
-    @Override
-    public void removeAll() {
-        super.removeAll();
-        text = null;
-        leftIcon = null;
-        rightIcon = null;
-    }
-    
-    private void updateLabel() {
-        getElement().setProperty("innerHTML", (leftIcon != null ? leftIcon.getElement().getOuterHTML() + " " : "")
-                + (text != null ? text : "") + (rightIcon != null ? " " + rightIcon.getElement().getOuterHTML() : ""));
-    }
-    
-    private void configureIcon(Component icon) {
-        icon.getElement().getStyle().set("margin-top", "-4px");
-        if (icon != null && icon instanceof Icon) {
-            ((Icon) icon).setSize("1em");
+        if (html != null) {
+            remove(html);
         }
+        html = new Html("<div>" + text + "</div>");
+        add(html);
     }
 
+    public void setRightIcon(VaadinIcon icon) {
+        setRightIcon(new Icon(icon));
+    }
+
+    public void setLeftIcon(VaadinIcon icon) {
+        setLeftIcon(new Icon(icon));
+    }
+
+    public void setLeftIcon(Component icon) {
+        if (this.icon != null) {
+            remove(this.icon);
+        }
+        icon.getElement().getStyle().set("align-self", "flex-start");
+        this.icon = icon;
+        add(icon);
+    }
+
+    public void setRightIcon(Component icon) {
+        if (this.icon != null) {
+            remove(this.icon);
+        }
+        icon.getElement().getStyle().set("align-self", "flex-end");
+        this.icon = icon;
+        add(icon);
+    }
 }
