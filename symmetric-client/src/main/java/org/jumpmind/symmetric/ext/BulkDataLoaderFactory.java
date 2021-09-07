9dda4fb102ff24877642149afd0715c389c835b3
Eric Long <elong@jumpmind.com>
2021-09-07 13:14:55 -0400

    code refactor

--- symmetric-client/src/main/java/org/jumpmind/symmetric/ext/BulkDataLoaderFactory.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/ext/BulkDataLoaderFactory.java
@@ -22,14 +22,7 @@ package org.jumpmind.symmetric.ext;
 
 import java.util.List;
 
-import org.jumpmind.db.platform.DatabaseNamesConstants;
 import org.jumpmind.db.platform.IDatabasePlatform;
-import org.jumpmind.db.platform.mssql.MsSql2000DatabasePlatform;
-import org.jumpmind.db.platform.mysql.MySqlDatabasePlatform;
-import org.jumpmind.db.platform.oracle.OracleDatabasePlatform;
-import org.jumpmind.db.platform.postgresql.PostgreSqlDatabasePlatform;
-import org.jumpmind.db.platform.redshift.RedshiftDatabasePlatform;
-import org.jumpmind.db.platform.tibero.TiberoDatabasePlatform;
 import org.jumpmind.extension.IBuiltInExtensionPoint;
 import org.jumpmind.symmetric.ISymmetricEngine;
 import org.jumpmind.symmetric.common.ParameterConstants;
@@ -57,34 +50,9 @@ public class BulkDataLoaderFactory extends AbstractDataLoaderFactory implements 
             List<IDatabaseWriterFilter> filters, List<IDatabaseWriterErrorHandler> errorHandlers,
             List<? extends Conflict> conflictSettings, List<ResolvedData> resolvedData) {
         IDatabasePlatform platform = engine.getTargetDialect().getPlatform();
-        String platformName = platform.getName();
         if (engine.getParameterService().is(ParameterConstants.JDBC_EXECUTE_BULK_BATCH_OVERRIDE, false)) {
             return new JdbcBatchBulkDatabaseWriter(symmetricDialect.getPlatform(), platform,
                     symmetricDialect.getTablePrefix(), buildParameterDatabaseWriterSettings(conflictSettings));
-        } else if (platform instanceof MySqlDatabasePlatform) {
-            return new MySqlBulkDataLoaderFactory(engine).getDataWriter(sourceNodeId, symmetricDialect, transformWriter,
-                    filters, errorHandlers, conflictSettings, resolvedData);
-        } else if (platform instanceof MsSql2000DatabasePlatform) {
-            return new MsSqlBulkDataLoaderFactory(engine).getDataWriter(sourceNodeId, symmetricDialect, transformWriter,
-                    filters, errorHandlers, conflictSettings, resolvedData);
-        } else if (platform instanceof OracleDatabasePlatform) {
-            return new OracleBulkDataLoaderFactory(engine).getDataWriter(sourceNodeId, symmetricDialect, transformWriter,
-                    filters, errorHandlers, conflictSettings, resolvedData);
-        } else if (platform instanceof TiberoDatabasePlatform) {
-            return new TiberoBulkDataLoaderFactory(engine).getDataWriter(sourceNodeId, symmetricDialect, transformWriter,
-                    filters, errorHandlers, conflictSettings, resolvedData);
-        } else if (platform instanceof PostgreSqlDatabasePlatform) {
-            return new PostgresBulkDataLoaderFactory(engine).getDataWriter(sourceNodeId, symmetricDialect, transformWriter,
-                    filters, errorHandlers, conflictSettings, resolvedData);
-        } else if (platform instanceof RedshiftDatabasePlatform) {
-            return new RedshiftBulkDataLoaderFactory(engine).getDataWriter(sourceNodeId, symmetricDialect, transformWriter,
-                    filters, errorHandlers, conflictSettings, resolvedData);
-        } else if (platformName != null && platformName.startsWith(DatabaseNamesConstants.TERADATA)) {
-            return new TeradataBulkDataLoaderFactory(engine).getDataWriter(sourceNodeId, symmetricDialect, transformWriter,
-                    filters, errorHandlers, conflictSettings, resolvedData);
-        } else if (platformName != null && platformName.startsWith(DatabaseNamesConstants.SNOWFLAKE)) {
-            return new SnowflakeBulkDataLoaderFactory(engine).getDataWriter(sourceNodeId, symmetricDialect, transformWriter,
-                    filters, errorHandlers, conflictSettings, resolvedData);
         } else {
             return new JdbcBatchBulkDatabaseWriter(symmetricDialect.getPlatform(), platform,
                     symmetricDialect.getTablePrefix(), buildParameterDatabaseWriterSettings(conflictSettings));
