a02832c6fb9004618d0c5442d9883ac5bd1d818e
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2022-04-25 10:01:01 -0400

    0003805: Added notification templates

--- symmetric-core/src/main/java/org/jumpmind/symmetric/util/SymmetricUtils.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/util/SymmetricUtils.java
@@ -26,16 +26,22 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.jumpmind.symmetric.ISymmetricEngine;
 import org.jumpmind.symmetric.Version;
 import org.jumpmind.symmetric.common.ParameterConstants;
 import org.jumpmind.symmetric.db.ISymmetricDialect;
+import org.jumpmind.symmetric.model.Monitor;
+import org.jumpmind.symmetric.model.MonitorEvent;
+import org.jumpmind.symmetric.model.Node;
 import org.jumpmind.util.AppUtils;
 import org.jumpmind.util.CollectionUtils;
 import org.jumpmind.util.FormatUtils;
@@ -97,6 +103,47 @@ final public class SymmetricUtils {
                 properties.put(object, value);
             }
         }
+    }
+
+    public static Map<String, String> getReplacementsForMonitorEvent(ISymmetricEngine engine, MonitorEvent event) {
+        Node eventNode = engine.getNodeService().findNode(event.getNodeId());
+        Map<String, String> replacements = new HashMap<String, String>();
+        replacements.put("eventCount", String.valueOf(event.getCount()));
+        replacements.put("eventDetails", event.getDetails());
+        replacements.put("eventHostName", event.getHostName());
+        replacements.put("eventIsNotified", String.valueOf(event.isNotified()));
+        replacements.put("eventIsResolved", String.valueOf(event.isResolved()));
+        replacements.put("eventLastUpdateTime", FormatUtils.formatDateTimeISO(event.getLastUpdateTime()));
+        replacements.put("eventMonitorId", event.getMonitorId());
+        if (eventNode != null) {
+            replacements.put("eventNodeExternalId", eventNode.getExternalId());
+            replacements.put("eventNodeGroupId", eventNode.getNodeGroupId());
+        } else {
+            replacements.put("eventNodeExternalId", "<unknown>");
+            replacements.put("eventNodeGroupId", "<unknown>");
+        }
+        replacements.put("eventNodeId", event.getNodeId());
+        replacements.put("eventSeverityLevel", Monitor.getSeverityLevelNames().get(event.getSeverityLevel()));
+        replacements.put("eventThreshold", String.valueOf(event.getThreshold()));
+        replacements.put("eventTime", FormatUtils.formatDateTimeISO(event.getEventTime()));
+        replacements.put("eventType", event.getType());
+        replacements.put("eventValue", String.valueOf(event.getValue()));
+        return replacements;
+    }
+
+    public static Map<String, String> getReplacementsForMonitorEventList(ISymmetricEngine engine, List<MonitorEvent> events) {
+        Map<String, String> replacements = new HashMap<String, String>();
+        Set<String> nodeIds = new HashSet<String>();
+        Set<String> types = new HashSet<String>();
+        for (MonitorEvent event : events) {
+            nodeIds.add(event.getNodeId());
+            types.add(event.getType());
+        }
+        replacements.put("eventCount", String.valueOf(events.size()));
+        replacements.put("eventNodeCount", String.valueOf(nodeIds.size()));
+        replacements.put("eventNodeIds", String.join(", ", nodeIds));
+        replacements.put("eventTypes", String.join(", ", types));
+        return replacements;
     }
 
     public static void logNotices() {
