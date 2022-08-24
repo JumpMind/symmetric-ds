d8f8b6f535981ac320459fcd6a988c198a4b7e2d
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2022-08-24 10:41:29 -0400

    0005401: Fixed table names being created using incorrect case when the target is load-only

--- symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/DefaultDatabaseWriter.java
+++ symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/DefaultDatabaseWriter.java
@@ -606,7 +606,7 @@ public class DefaultDatabaseWriter extends AbstractDatabaseWriter {
             StringReader reader = new StringReader(xml);
             Database db = DatabaseXmlUtil.read(reader, false);
             if (writerSettings.isCreateTableAlterCaseToMatchDatabaseDefault()) {
-                getPlatform().alterCaseToMatchDatabaseDefaultCase(db);
+                getPlatform(tempNonSymTable).alterCaseToMatchDatabaseDefaultCase(db);
             }
             getPlatform(tempNonSymTable).makePlatformSpecific(db);
             if (writerSettings.isAlterTable()) {
