73ae78199ce6d3889b96707c666c064e7f600b7e
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2022-09-02 11:08:09 -0400

    0003251: Fixed inserts and updates for MySQL unsigned bigints larger than Long.MAX_VALUE

--- symmetric-jdbc/src/main/java/org/jumpmind/db/platform/mysql/MySqlJdbcSqlTemplate.java
+++ symmetric-jdbc/src/main/java/org/jumpmind/db/platform/mysql/MySqlJdbcSqlTemplate.java
@@ -20,12 +20,22 @@
  */
 package org.jumpmind.db.platform.mysql;
 
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
 import javax.sql.DataSource;
 
+import org.jumpmind.db.model.TypeMap;
 import org.jumpmind.db.platform.DatabaseInfo;
 import org.jumpmind.db.sql.JdbcSqlTemplate;
 import org.jumpmind.db.sql.SqlTemplateSettings;
 import org.jumpmind.db.sql.SymmetricLobHandler;
+import org.springframework.jdbc.core.SqlTypeValue;
+import org.springframework.jdbc.core.StatementCreatorUtils;
+import org.springframework.jdbc.support.lob.LobHandler;
 
 public class MySqlJdbcSqlTemplate extends JdbcSqlTemplate {
     public MySqlJdbcSqlTemplate(DataSource dataSource, SqlTemplateSettings settings,
@@ -41,5 +51,35 @@ public class MySqlJdbcSqlTemplate extends JdbcSqlTemplate {
     @Override
     public String getSelectLastInsertIdSql(String sequenceName) {
         return "select last_insert_id()";
+    }
+
+    @Override
+    public void setValues(PreparedStatement ps, Object[] args, int[] argTypes, LobHandler lobHandler) throws SQLException {
+        for (int i = 1; i <= args.length; i++) {
+            Object arg = args[i - 1];
+            int argType = argTypes != null && argTypes.length >= i ? argTypes[i - 1] : SqlTypeValue.TYPE_UNKNOWN;
+            try {
+                if (argType == Types.BLOB && lobHandler != null && arg instanceof byte[]) {
+                    lobHandler.getLobCreator().setBlobAsBytes(ps, i, (byte[]) arg);
+                } else if (argType == Types.BLOB && lobHandler != null && arg instanceof String) {
+                    lobHandler.getLobCreator().setBlobAsBytes(ps, i, arg.toString().getBytes(Charset.defaultCharset()));
+                } else if (argType == Types.CLOB && lobHandler != null) {
+                    lobHandler.getLobCreator().setClobAsString(ps, i, (String) arg);
+                } else if ((argType == Types.DECIMAL || argType == Types.NUMERIC) && arg != null) {
+                    setDecimalValue(ps, i, arg, argType);
+                } else if (argType == Types.TINYINT) {
+                    setTinyIntValue(ps, i, arg, argType);
+                } else if (argType == Types.BIGINT && arg instanceof BigInteger
+                        && ((BigInteger) arg).compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0) {
+                    ps.setString(i, arg.toString());
+                } else {
+                    StatementCreatorUtils.setParameterValue(ps, i, verifyArgType(arg, argType), arg);
+                }
+            } catch (SQLException ex) {
+                String msg = String.format("Parameter arg '%s' type: %s caused exception: %s", arg,
+                        TypeMap.getJdbcTypeName(argType), ex.getMessage());
+                throw new SQLException(msg, ex);
+            }
+        }
     }
 }
