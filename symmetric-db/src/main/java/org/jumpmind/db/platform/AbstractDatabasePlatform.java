65388906b92a37beef372d30bfc6b86e46b4804a
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2022-09-06 13:47:59 -0400

    0004116: Fixed Postgres timestamp to time and time to timestamp conversion

--- symmetric-db/src/main/java/org/jumpmind/db/platform/AbstractDatabasePlatform.java
+++ symmetric-db/src/main/java/org/jumpmind/db/platform/AbstractDatabasePlatform.java
@@ -652,13 +652,10 @@ public abstract class AbstractDatabasePlatform implements IDatabasePlatform {
                     if (useTimestamp) {
                         return parseTimestamp(type, value);
                     } else if (type == Types.TIME) {
-                        if (value.indexOf(".") == 8) {
-                            /*
-                             * Firebird (at least) captures fractional seconds in time fields which need to be parsed by Timestamp.valueOf
-                             */
+                        if (value.indexOf(".") == 8 || value.length() <= 8) {
                             return Timestamp.valueOf("1970-01-01 " + value);
                         } else {
-                            return FormatUtils.parseDate(value, FormatUtils.TIME_PATTERNS);
+                            return Timestamp.valueOf(value);
                         }
                     } else {
                         return FormatUtils.parseDate(value, FormatUtils.TIMESTAMP_PATTERNS);
@@ -942,6 +939,9 @@ public abstract class AbstractDatabasePlatform implements IDatabasePlatform {
 
     public java.util.Date parseTimestamp(int type, String value) {
         try {
+            if (value.indexOf(".") == 8 || value.length() <= 8) {
+                value = "1970-01-01 " + value;
+            }
             return Timestamp.valueOf(value);
         } catch (IllegalArgumentException ex) {
             if (!getDatabaseInfo().isZeroDateAllowed() && value != null && value.startsWith(ZERO_DATE_STRING)) {
