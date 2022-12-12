4a165747bf225524e24e9cfe3ac242c4e89d4e8e
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2022-12-12 12:42:12 -0500

    0005622: Prevented deletes from being ignored when FK children exist but cannot be found

--- symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/DefaultDatabaseWriterConflictResolver.java
+++ symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/DefaultDatabaseWriterConflictResolver.java
@@ -555,7 +555,12 @@ public class DefaultDatabaseWriterConflictResolver extends AbstractDatabaseWrite
             log.info("Child exists foreign key violation on table {} during {} with batch {}.  Attempting to correct.", 
                     targetTable.getName(), data.getDataEventType().toString(), writer.getContext().getBatch().getNodeBatchId());
             
-            return deleteForeignKeyChildren(platform, sqlTemplate, databaseWriter, writer.getTargetTable(), data);
+            if (deleteForeignKeyChildren(platform, sqlTemplate, databaseWriter, writer.getTargetTable(), data)) {
+                return true;
+            } else {
+                throw new RuntimeException("Failed to delete foreign table rows to fix foreign key violation for table '"
+                        + writer.getTargetTable().getFullyQualifiedTableName() + "'");
+            }
         }
         return false;
     }    
