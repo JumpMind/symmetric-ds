0ca024a1415c7a1e877a6033495664c5228b3c89
jakobvanmeter <jakobvanmeter@192.168.42.224>
2023-03-22 10:44:18 -0400

    0005737: Serve Time Offsets Causing an Error in Determining if a Node is Offline

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TransformService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TransformService.java
@@ -282,7 +282,7 @@ public class TransformService extends AbstractService implements ITransformServi
         TransformTableNodeGroupLink transform = new TransformTableNodeGroupLink();
         transform.setSourceTableName(tableName);
         transform.setTargetTableName(tableName);
-        transform.setTransformPoint(TransformPoint.LOAD);
+        transform.setTransformPoint(TransformPoint.EXTRACT);
         transform.addTransformColumn(column);
         transform.setNodeGroupLink(nodeGroupLink);
         transforms.add(transform);
@@ -291,7 +291,7 @@ public class TransformService extends AbstractService implements ITransformServi
             transform = new TransformTableNodeGroupLink();
             transform.setSourceTableName(tableName);
             transform.setTargetTableName(tableName);
-            transform.setTransformPoint(TransformPoint.EXTRACT);
+            transform.setTransformPoint(TransformPoint.LOAD);
             column = new TransformColumn("node_password", "node_password", false);
             column.setTransformType("bsh");
             column.setTransformExpression(String.format(NODE_FILTER_BSH, "onNodeSecuritySave"));
