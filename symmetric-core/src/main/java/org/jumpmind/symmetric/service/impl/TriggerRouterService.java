d7a8ece921126ea9913338420092880dcc636fda
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2023-05-22 11:25:18 -0400

    0005851: sym_monitor_event trigger should capture changes when monitor.events.capture.enabled=true even if auto.sync.configuration=false

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TriggerRouterService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TriggerRouterService.java
@@ -524,7 +524,8 @@ public class TriggerRouterService extends AbstractService implements ITriggerRou
         boolean syncChanges = !TableConstants.getTablesThatDoNotSync(tablePrefix).contains(
                 tableName)
                 && (parameterService.is(ParameterConstants.AUTO_SYNC_CONFIGURATION)
-                        || TableConstants.getTableName(tablePrefix, TableConstants.SYM_NODE_HOST).equals(tableName));
+                        || TableConstants.getTableName(tablePrefix, TableConstants.SYM_NODE_HOST).equals(tableName)
+                        || TableConstants.getTableName(tablePrefix, TableConstants.SYM_MONITOR_EVENT).equals(tableName));
 //      boolean syncOnIncoming = !configurationService.isMasterToMaster() && (parameterService.is(
 //          ParameterConstants.AUTO_SYNC_CONFIGURATION_ON_INCOMING, true)
 //          || tableName.equals(TableConstants.getTableName(tablePrefix,
