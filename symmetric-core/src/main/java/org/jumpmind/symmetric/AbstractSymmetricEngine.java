f877e2a307424fbf1ba32cdf7d53be0c398bafb3
Eric Long <elong@jumpmind.com>
2023-09-07 13:20:04 -0400

    0005975: Upgrading from open source to pro is missing pro tables

--- symmetric-core/src/main/java/org/jumpmind/symmetric/AbstractSymmetricEngine.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/AbstractSymmetricEngine.java
@@ -487,7 +487,8 @@ abstract public class AbstractSymmetricEngine implements ISymmetricEngine {
     protected boolean hasSoftwareVersionChanged() {
         Node identity = nodeService.findIdentity();
         if (identity != null) {
-            return !Version.version().equals(identity.getSymmetricVersion()) || Version.isDevelopment(identity.getSymmetricVersion());
+            return !Version.version().equals(identity.getSymmetricVersion()) || !StringUtils.equals(identity.getDeploymentType(), getDeploymentType()) || 
+                    Version.isDevelopment(identity.getSymmetricVersion());
         }
         return true;
     }
