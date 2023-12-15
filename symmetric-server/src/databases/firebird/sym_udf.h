37790b2947c6b3758baf8e75c5e5e14507576bac
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2023-12-15 15:17:13 -0500

    0006150: Fixed Firebird sym_hex UDF causing an access violation

--- symmetric-server/src/databases/firebird/sym_udf.h
+++ symmetric-server/src/databases/firebird/sym_udf.h
@@ -21,19 +21,11 @@
 #include "stdio.h"
 #include "ibase.h"
 #include "ib_util.h"
+#include "stdlib.h"
+#include "string.h"
 
 static char backslash_chr = '\\';
 static char quote_chr = '"';
-static char *empty_str = "";
-
-typedef struct blob {
-	short	(*blob_get_segment) ();
-	void	*blob_handle;
-	long	blob_number_segments;
-	long	blob_max_segment;
-	long	blob_total_length;
-	void	(*blob_put_segment) ();
-} *BLOBCALLBACK;
 
 char *sym_escape(char *str);
 char *sym_hex(BLOBCALLBACK blob);
