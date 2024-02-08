2a2eddc586c89783bf98c41b598f47ec2a5f5771
jakobvanmeter <jakobvanmeter@192.168.1.4>
2024-02-08 13:40:26 -0500

    Fixed issues with H2 found by multiple tests.

--- symmetric-db/src/main/java/org/jumpmind/db/platform/h2/H2DdlBuilder.java
+++ symmetric-db/src/main/java/org/jumpmind/db/platform/h2/H2DdlBuilder.java
@@ -46,6 +46,7 @@ package org.jumpmind.db.platform.h2;
 import java.sql.Types;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jumpmind.db.alter.AddColumnChange;
@@ -62,6 +63,7 @@ import org.jumpmind.db.model.ColumnTypes;
 import org.jumpmind.db.model.Database;
 import org.jumpmind.db.model.IIndex;
 import org.jumpmind.db.model.ModelException;
+import org.jumpmind.db.model.PlatformColumn;
 import org.jumpmind.db.model.Table;
 import org.jumpmind.db.platform.AbstractDdlBuilder;
 import org.jumpmind.db.platform.DatabaseNamesConstants;
@@ -285,5 +287,19 @@ public class H2DdlBuilder extends AbstractDdlBuilder {
 
     public void setVersion2(boolean isVersion2) {
         this.isVersion2 = isVersion2;
+    }
+    
+    @Override
+    public String getSqlType(Column column) {
+        String type = super.getSqlType(column);
+        if (column.getPlatformColumns() != null) {
+            for (Map.Entry<String, PlatformColumn> platformColumn : column.getPlatformColumns().entrySet()) {
+                if (platformColumn.getValue() != null && platformColumn.getValue().getType() != null &&
+                        platformColumn.getValue().getType().equals("UUID")) {
+                    return "UUID";
+                }
+            }
+        }
+        return type;
     }
 }
