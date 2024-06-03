ef7a5672d3eed224a446b6e214221457400a9bfb
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2024-06-03 12:04:39 -0400

    0006468: Fixed symadmin export-sym-tables subcommand doubling the table prefix when called without the --alters option

--- symmetric-core/src/main/java/org/jumpmind/symmetric/db/AbstractSymmetricDialect.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/db/AbstractSymmetricDialect.java
@@ -518,7 +518,6 @@ abstract public class AbstractSymmetricDialect implements ISymmetricDialect {
 
     public String getCreateSymmetricDDL() {
         Database database = readSymmetricSchemaFromXml();
-        prefixConfigDatabase(database);
         IDdlBuilder builder = platform.getDdlBuilder();
         return builder.createTables(database, true);
     }
